ae52bf9bc6f5fdaa5613236fdad6b6409a016e63
Wesley Beary <wbeary@engineyard.com>
2009-07-10 14:58:29 -0700

    work across more buckets for bench

--- benchs/fog_vs.rb
+++ benchs/fog_vs.rb
@@ -17,44 +17,60 @@ raws = RightAws::S3Interface.new(
 )
 raws.logger.level = 3 # ERROR
 
-TIMES = 100
+TIMES = 10
 
 Benchmark.bmbm(25) do |bench|
   bench.report('fog.put_bucket') do
-    fog.put_bucket('fogbench')
+    TIMES.times do |x|
+      fog.put_bucket("fogbench#{x}")
+    end
   end
   bench.report('raws.create_bucket') do
-    raws.create_bucket('rawsbench')
+    TIMES.times do |x|
+      raws.create_bucket("rawsbench#{x}")
+    end
   end
 
   bench.report('fog.put_object') do
     TIMES.times do |x|
-      file = File.open(File.dirname(__FILE__) + '/../spec/lorem.txt', 'r')
-      fog.put_object('fogbench', "lorem_#{x}", file)
+      TIMES.times do |y|
+        file = File.open(File.dirname(__FILE__) + '/../spec/lorem.txt', 'r')
+        fog.put_object("fogbench#{x}", "lorem_#{y}", file)
+      end
     end
   end
   bench.report('raws.put') do
     TIMES.times do |x|
-      file = File.open(File.dirname(__FILE__) + '/../spec/lorem.txt', 'r')
-      raws.put('rawsbench', "lorem_#{x}", file)
+      TIMES.times do |y|
+        file = File.open(File.dirname(__FILE__) + '/../spec/lorem.txt', 'r')
+        raws.put("rawsbench#{x}", "lorem_#{y}", file)
+      end
     end
   end
 
   bench.report('fog.delete_object') do
     TIMES.times do |x|
-      fog.delete_object('fogbench', "lorem_#{x}")
+      TIMES.times do |y|
+        fog.delete_object("fogbench#{x}", "lorem_#{y}")
+      end
     end
   end
   bench.report('raws.delete') do
     TIMES.times do |x|
-      raws.delete('rawsbench', "lorem_#{x}")
+      TIMES.times do |y|
+        raws.delete("rawsbench#{x}", "lorem_#{y}")
+      end
     end
   end
 
   bench.report('fog.delete_bucket') do
-    fog.delete_bucket('fogbench')
+    TIMES.times do |x|
+      fog.delete_bucket("fogbench#{x}")
+    end
   end
   bench.report('raws.delete_bucket') do
-    raws.delete_bucket('rawsbench')
+    TIMES.times do |x|
+      raws.delete_bucket("rawsbench#{x}")
+    end
   end
 end
