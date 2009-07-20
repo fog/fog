b4986f57573c015248c27f56817728bdba739944
Wesley Beary <wbeary@engineyard.com>
2009-07-19 17:02:25 -0700

    fill in pending spec for describe availability zones

--- spec/aws/ec2/describe_availability_zones_spec.rb
+++ spec/aws/ec2/describe_availability_zones_spec.rb
@@ -10,6 +10,12 @@ describe 'EC2.describe_availability_zones' do
     zone[:zone_state].should be_a(String)
   end
 
-  it "should return proper attributes for specific ip"
+  it "should return proper attribute with params" do
+    actual = ec2.describe_availability_zones(['us-east-1a'])
+    zone = actual.body[:availability_zone_info].first
+    zone[:region_name].should be_a(String)
+    zone[:zone_name].should be_a(String)
+    zone[:zone_state].should be_a(String)
+  end
 
 end
