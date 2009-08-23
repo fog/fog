9003052d42113a983778a2b950866d486d6411b5
Wesley Beary <wbeary@engineyard.com>
2009-08-23 14:38:54 -0700

    fix the connection grabbing function to match with the request specs

--- spec/aws/models/s3/buckets_spec.rb
+++ spec/aws/models/s3/buckets_spec.rb
@@ -2,28 +2,24 @@ require File.dirname(__FILE__) + '/../../../spec_helper'
 
 describe 'S3.buckets' do
 
-  before(:all) do
-    @s3 = Fog::AWS::S3.gen
-  end
-
   it "should return buckets from all" do
-    p @s3.buckets.all
+    p s3.buckets.all
   end
 
   it "should create bucket" do
-    p @bucket = @s3.buckets.create(:name => 'fogbucketstest')
+    p @bucket = s3.buckets.create(:name => 'fogbucketstest')
     p @bucket.delete
   end
 
   it "should get/put request payment" do
-    p @bucket = @s3.buckets.create(:name => 'fogbucketspaymenttest')
+    p @bucket = s3.buckets.create(:name => 'fogbucketspaymenttest')
     p @bucket.payer = 'BucketOwner'
     p @bucket.payer
     p @bucket.delete
   end
 
   it "should push bucket down into objects" do
-    p @bucket = @s3.buckets.create(:name => 'fogbucketsobjectstest')
+    p @bucket = s3.buckets.create(:name => 'fogbucketsobjectstest')
     p @bucket.objects.new(:key => 'object')
     p @bucket.objects.all
     p @bucket.delete
