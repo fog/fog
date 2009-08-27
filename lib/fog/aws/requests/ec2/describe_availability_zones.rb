1d780119ca51d2e00aab95c74e75ea6f12cde564
Wesley Beary <wbeary@engineyard.com>
2009-08-26 21:31:53 -0700

    describe availability zones and regions mocks

--- lib/fog/aws/requests/ec2/describe_availability_zones.rb
+++ lib/fog/aws/requests/ec2/describe_availability_zones.rb
@@ -1,27 +1,64 @@
-module Fog
-  module AWS
-    class EC2
+unless Fog.mocking?
 
-      # Describe all or specified availability zones
-      #
-      # ==== Params
-      # * zone_name<~String> - List of availability zones to describe, defaults to all
-      #
-      # ==== Returns
-      # * response<~Fog::AWS::Response>:
-      #   * body<~Hash>:
-      #     * 'requestId'<~String> - Id of request
-      #     * 'availabilityZoneInfo'<~Array>:
-      #       * 'regionName'<~String> - Name of region
-      #       * 'zoneName'<~String> - Name of zone
-      #       * 'zoneState'<~String> - State of zone
-      def describe_availability_zones(zone_name = [])
-        params = indexed_params('ZoneName', zone_name)
-        request({
-          'Action' => 'DescribeAvailabilityZones'
-        }.merge!(params), Fog::Parsers::AWS::EC2::DescribeAvailabilityZones.new)
+  module Fog
+    module AWS
+      class EC2
+
+        # Describe all or specified availability zones
+        #
+        # ==== Params
+        # * zone_name<~String> - List of availability zones to describe, defaults to all
+        #
+        # ==== Returns
+        # * response<~Fog::AWS::Response>:
+        #   * body<~Hash>:
+        #     * 'requestId'<~String> - Id of request
+        #     * 'availabilityZoneInfo'<~Array>:
+        #       * 'regionName'<~String> - Name of region
+        #       * 'zoneName'<~String> - Name of zone
+        #       * 'zoneState'<~String> - State of zone
+        def describe_availability_zones(zone_name = [])
+          params = indexed_params('ZoneName', zone_name)
+          request({
+            'Action' => 'DescribeAvailabilityZones'
+          }.merge!(params), Fog::Parsers::AWS::EC2::DescribeAvailabilityZones.new)
+        end
+
       end
-
     end
   end
+
+else
+
+  module Fog
+    module AWS
+      class EC2
+
+        def describe_availability_zones(zone_name = [])
+          response = Fog::Response.new
+          zone_name = [*zone_name]
+          zones = {
+            'us-east-1a' => {"zoneName"=>"us-east-1a", "regionName"=>"us-east-1", "zoneState"=>"available"}, 
+            'us-east-1b' => {"zoneName"=>"us-east-1b", "regionName"=>"us-east-1", "zoneState"=>"available"}, 
+            'us-east-1c' => {"zoneName"=>"us-east-1c", "regionName"=>"us-east-1", "zoneState"=>"available"}, 
+            'us-east-1d' => {"zoneName"=>"us-east-1d", "regionName"=>"us-east-1", "zoneState"=>"available"}
+          }
+          if zone_name != []
+            availability_zone_info = zones.reject {|key, value| !zone_name.include?(key)}.values
+          else
+            availability_zone_info = zones.values
+          end
+
+          response.status = 200
+          response.body = {
+            'requestId'             => Fog::AWS::Mock.request_id,
+            'availabilityZoneInfo'  => availability_zone_info
+          }
+          response
+        end
+
+      end
+    end
+  end
+
 end
--- lib/fog/aws/requests/ec2/describe_regions.rb
+++ lib/fog/aws/requests/ec2/describe_regions.rb
@@ -1,26 +1,61 @@
-module Fog
-  module AWS
-    class EC2
+unless Fog.mocking?
 
-      # Describe all or specified regions
-      #
-      # ==== Params
-      # * region_name<~String> - List of regions to describe, defaults to all
-      #
-      # ==== Returns
-      # * response<~Fog::AWS::Response>:
-      #   * body<~Hash>:
-      #     * 'requestId'<~String> - Id of request
-      #     * 'regionInfo'<~Array>:
-      #       * 'regionName'<~String> - Name of region
-      #       * 'regionEndpoint'<~String> - Service endpoint for region
-      def describe_regions(region_name = [])
-        params = indexed_params('RegionName', region_name)
-        request({
-          'Action' => 'DescribeRegions'
-        }.merge!(params), Fog::Parsers::AWS::EC2::DescribeRegions.new)
+  module Fog
+    module AWS
+      class EC2
+
+        # Describe all or specified regions
+        #
+        # ==== Params
+        # * region_name<~String> - List of regions to describe, defaults to all
+        #
+        # ==== Returns
+        # * response<~Fog::AWS::Response>:
+        #   * body<~Hash>:
+        #     * 'requestId'<~String> - Id of request
+        #     * 'regionInfo'<~Array>:
+        #       * 'regionName'<~String> - Name of region
+        #       * 'regionEndpoint'<~String> - Service endpoint for region
+        def describe_regions(region_name = [])
+          params = indexed_params('RegionName', region_name)
+          request({
+            'Action' => 'DescribeRegions'
+          }.merge!(params), Fog::Parsers::AWS::EC2::DescribeRegions.new)
+        end
+
       end
-
     end
   end
+
+else
+
+  module Fog
+    module AWS
+      class EC2
+
+        def describe_regions(region_name = [])
+          response = Fog::Response.new
+          region_name = [*region_name]
+          regions = {
+            'eu-west-1' => {"regionName"=>"eu-west-1", "regionEndpoint"=>"eu-west-1.ec2.amazonaws.com"},
+            'us-east-1' => {"regionName"=>"us-east-1", "regionEndpoint"=>"us-east-1.ec2.amazonaws.com"}
+          }
+          if region_name != []
+            region_info = regions.reject {|key, value| !region_name.include?(key)}.values
+          else
+            region_info = regions.values
+          end
+
+          response.status = 200
+          response.body = {
+            'requestId'   => Fog::AWS::Mock.request_id,
+            'regionInfo'  => region_info
+          }
+          response
+        end
+
+      end
+    end
+  end
+
 end
