0f3cccc5660a94d2689a6bac6d0240d42ce6b591
Wesley Beary <wbeary@engineyard.com>
2009-09-16 20:02:32 -0700

    first pass at security group mocks

--- lib/fog/aws/requests/ec2/authorize_security_group_ingress.rb
+++ lib/fog/aws/requests/ec2/authorize_security_group_ingress.rb
@@ -1,32 +1,68 @@
-module Fog
-  module AWS
-    class EC2
+unless Fog.mocking?
 
-      # Add permissions to a security group
-      #
-      # ==== Parameters
-      # * options<~Hash>:
-      #   * 'GroupName'<~String> - Name of group
-      #   * 'SourceSecurityGroupName'<~String> - Name of security group to authorize
-      #   * 'SourceSecurityGroupOwnerId'<~String> - Name of owner to authorize
-      #   or
-      #   * 'CidrIp' - CIDR range
-      #   * 'FromPort' - Start of port range (or -1 for ICMP wildcard)
-      #   * 'GroupName' - Name of group to modify
-      #   * 'IpProtocol' - Ip protocol, must be in ['tcp', 'udp', 'icmp']
-      #   * 'ToPort' - End of port range (or -1 for ICMP wildcard)
-      #
-      # === Returns
-      # * response<~Fog::AWS::Response>:
-      #   * body<~Hash>:
-      #     * 'requestId'<~String> - Id of request
-      #     * 'return'<~Boolean> - success?
-      def authorize_security_group_ingress(options = {})
-        request({
-          'Action' => 'AuthorizeSecurityGroupIngress'
-        }.merge!(options), Fog::Parsers::AWS::EC2::Basic.new)
+  module Fog
+    module AWS
+      class EC2
+
+        # Add permissions to a security group
+        #
+        # ==== Parameters
+        # * options<~Hash>:
+        #   * 'GroupName'<~String> - Name of group
+        #   * 'SourceSecurityGroupName'<~String> - Name of security group to authorize
+        #   * 'SourceSecurityGroupOwnerId'<~String> - Name of owner to authorize
+        #   or
+        #   * 'CidrIp' - CIDR range
+        #   * 'FromPort' - Start of port range (or -1 for ICMP wildcard)
+        #   * 'GroupName' - Name of group to modify
+        #   * 'IpProtocol' - Ip protocol, must be in ['tcp', 'udp', 'icmp']
+        #   * 'ToPort' - End of port range (or -1 for ICMP wildcard)
+        #
+        # === Returns
+        # * response<~Fog::AWS::Response>:
+        #   * body<~Hash>:
+        #     * 'requestId'<~String> - Id of request
+        #     * 'return'<~Boolean> - success?
+        def authorize_security_group_ingress(options = {})
+          request({
+            'Action' => 'AuthorizeSecurityGroupIngress'
+          }.merge!(options), Fog::Parsers::AWS::EC2::Basic.new)
+        end
+
       end
-
     end
   end
+
+else
+
+  module Fog
+    module AWS
+      class EC2
+
+        # TODO: handle the GroupName/Source/Source case
+        def authorize_security_group_ingress(options = {})
+          response = Fog::Response.new
+          group = Fog::AWS::EC2.data[:security_groups][options['GroupName']]
+
+          group['ipPermissions'] ||= []
+          group['ipPermissions'] << {
+            'groups'      => [],
+            'fromPort'    => options['FromPort'],
+            'ipRanges'    => [{ 'cidrIp' => options['CidrIp'] }],
+            'ipProtocol'  => options['IpProtocol'],
+            'toPort'      => options['ToPort']
+          }
+
+          response.status = 200
+          response.body = {
+            'requestId' => Fog::AWS::Mock.request_id,
+            'return'    => true
+          }
+          response
+        end
+
+      end
+    end
+  end
+
 end
--- lib/fog/aws/requests/ec2/revoke_security_group_ingress.rb
+++ lib/fog/aws/requests/ec2/revoke_security_group_ingress.rb
@@ -1,32 +1,68 @@
-module Fog
-  module AWS
-    class EC2
+unless Fog.mocking?
 
-      # Remove permissions from a security group
-      #
-      # ==== Parameters
-      # * options<~Hash>:
-      #   * 'GroupName'<~String> - Name of group
-      #   * 'SourceSecurityGroupName'<~String> - Name of security group to authorize
-      #   * 'SourceSecurityGroupOwnerId'<~String> - Name of owner to authorize
-      #   or
-      #   * 'CidrIp' - CIDR range
-      #   * 'FromPort' - Start of port range (or -1 for ICMP wildcard)
-      #   * 'GroupName' - Name of group to modify
-      #   * 'IpProtocol' - Ip protocol, must be in ['tcp', 'udp', 'icmp']
-      #   * 'ToPort' - End of port range (or -1 for ICMP wildcard)
-      #
-      # === Returns
-      # * response<~Fog::AWS::Response>:
-      #   * body<~Hash>:
-      #     * 'requestId'<~String> - Id of request
-      #     * 'return'<~Boolean> - success?
-      def revoke_security_group_ingress(options = {})
-        request({
-          'Action' => 'RevokeSecurityGroupIngress'
-        }.merge!(options), Fog::Parsers::AWS::EC2::Basic.new)
+  module Fog
+    module AWS
+      class EC2
+
+        # Remove permissions from a security group
+        #
+        # ==== Parameters
+        # * options<~Hash>:
+        #   * 'GroupName'<~String> - Name of group
+        #   * 'SourceSecurityGroupName'<~String> - Name of security group to authorize
+        #   * 'SourceSecurityGroupOwnerId'<~String> - Name of owner to authorize
+        #   or
+        #   * 'CidrIp' - CIDR range
+        #   * 'FromPort' - Start of port range (or -1 for ICMP wildcard)
+        #   * 'GroupName' - Name of group to modify
+        #   * 'IpProtocol' - Ip protocol, must be in ['tcp', 'udp', 'icmp']
+        #   * 'ToPort' - End of port range (or -1 for ICMP wildcard)
+        #
+        # === Returns
+        # * response<~Fog::AWS::Response>:
+        #   * body<~Hash>:
+        #     * 'requestId'<~String> - Id of request
+        #     * 'return'<~Boolean> - success?
+        def revoke_security_group_ingress(options = {})
+          request({
+            'Action' => 'RevokeSecurityGroupIngress'
+          }.merge!(options), Fog::Parsers::AWS::EC2::Basic.new)
+        end
+
       end
-
     end
   end
+
+else
+
+  module Fog
+    module AWS
+      class EC2
+
+        # TODO: handle the GroupName/Source/Source case
+        def revoke_security_group_ingress(options = {})
+          response = Fog::Response.new
+          group = Fog::AWS::EC2.data[:security_groups][options['GroupName']]
+
+          ingress = group['ipPermissions'].select {|permission|
+            permission['fromPort']    == options['FromPort'] &&
+            permission['ipProtocol']  == options['IpProtocol'] &&
+            permission['toPort']      == options['ToPort'] &&
+            permission['ipRanges'].first['cidrIp'] == options['CidrIp']
+          }.first
+
+          group['ipPermissions'].delete(ingress)
+
+          response.status = 200
+          response.body = {
+            'requestId' => Fog::AWS::Mock.request_id,
+            'return'    => true
+          }
+          response
+        end
+
+      end
+    end
+  end
+
 end
