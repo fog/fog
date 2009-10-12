337148709d5829b014934644a89f619425b6accc
Wesley Beary <wbeary@engineyard.com>
2009-10-12 09:25:47 -0700

    add head_containers for rackspace files

---
+++ lib/fog/rackspace/requests/files/head_containers.rb
@@ -0,0 +1,41 @@
+unless Fog.mocking?
+
+  module Fog
+    module Rackspace
+      class Files
+
+        # List number of containers and total bytes stored
+        #
+        # ==== Returns
+        # * response<~Fog::AWS::Response>:
+        #   * headers<~Hash>:
+        #     * 'X-Account-Container-Count'<~String> - Count of containers
+        #     * 'X-Account-Bytes-Used'<~String> - Bytes used
+        def head_containers
+          response = storage_request(
+            :expects  => 204,
+            :method   => 'HEAD',
+            :path     => '',
+            :query    => 'format=json'
+          )
+          response
+        end
+
+      end
+    end
+  end
+
+else
+
+  module Fog
+    module Rackspace
+      class Servers
+
+        def get_flavors
+        end
+
+      end
+    end
+  end
+
+end
---
+++ spec/rackspace/requests/files/head_containers.rb
@@ -0,0 +1,11 @@
+require File.dirname(__FILE__) + '/../../../spec_helper'
+
+describe 'Rackspace::Files.head_containers' do
+  describe 'success' do
+
+    it "should return proper attributes" do
+      p files.head_containers
+    end
+
+  end
+end
