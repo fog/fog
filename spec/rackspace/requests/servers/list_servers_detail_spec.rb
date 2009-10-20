5249bf4a54e788826e04841b12d4bc8fa579b440
Wesley Beary <wbeary@engineyard.com>
2009-10-19 21:44:25 -0700

    more rackspace specs

--- spec/rackspace/requests/servers/list_servers_detail_spec.rb
+++ spec/rackspace/requests/servers/list_servers_detail_spec.rb
@@ -3,9 +3,32 @@ require File.dirname(__FILE__) + '/../../../spec_helper'
 describe 'Rackspace::Servers.list_servers_detail' do
   describe 'success' do
 
+    before(:each) do
+      # flavor 1 = 256, image 3 = gentoo 2008.0
+      @server_id = servers.create_server(1, 3).body['server']['id']
+    end
+
+    after(:each) do
+      eventually(128) do
+        servers.delete_server(@server_id)
+      end
+    end
+
     it "should return proper attributes" do
-      pending
-      p servers.list_servers_detail
+      actual = servers.list_servers_detail.body['servers'].first
+      actual['addresses'].should be_a(Hash)
+      actual['addresses']['private'].should be_an(Array)
+      actual['addresses']['private'].first.should be_a(String)
+      actual['addresses']['public'].should be_an(Array)
+      actual['addresses']['public'].first.should be_a(String)
+      actual['flavorId'].should be_an(Integer)
+      actual['hostId'].should be_a(String)
+      actual['id'].should be_an(Integer)
+      actual['imageId'].should be_an(Integer)
+      actual['metadata'].should be_a(Hash)
+      actual['name'].should be_a(String)
+      actual['progress'].should be_an(Integer)
+      actual['status'].should be_a(String)
     end
 
   end
--- spec/rackspace/requests/servers/list_servers_spec.rb
+++ spec/rackspace/requests/servers/list_servers_spec.rb
@@ -3,9 +3,21 @@ require File.dirname(__FILE__) + '/../../../spec_helper'
 describe 'Rackspace::Servers.list_servers' do
   describe 'success' do
 
+    before(:each) do
+      # flavor 1 = 256, image 3 = gentoo 2008.0
+      @server_id = servers.create_server(1, 3).body['server']['id']
+    end
+
+    after(:each) do
+      eventually(128) do
+        servers.delete_server(@server_id)
+      end
+    end
+
     it "should return proper attributes" do
-      pending
-      p servers.list_servers
+      actual = servers.list_servers.body['servers'].first
+      actual['id'].should be_an(Integer)
+      actual['name'].should be_a(String)
     end
 
   end
