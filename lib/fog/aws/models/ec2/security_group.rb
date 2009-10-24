5aaf86defa2763b6c888300cf36071e5a3a43e39
Wesley Beary <wbeary@engineyard.com>
2009-10-24 11:20:05 -0700

    nicer model interface to authorizing security group ingresses

--- lib/fog/aws/models/ec2/security_group.rb
+++ lib/fog/aws/models/ec2/security_group.rb
@@ -10,11 +10,22 @@ module Fog
         attribute :ip_permissions,    'ipPermissions'
         attribute :owner_id,          'ownerId'
 
-        def authorize(options = {})
-          options = {
-            'GroupName' => @group_name
-          }.merge!(options)
-          connection.authorize_security_group_ingress(options)
+        def authorize_group_and_owner(group, owner)
+          connection.authorize_security_group_ingress(
+            'GroupName'                   => @group_name
+            'SourceSecurityGroupName'     => group,
+            'SourceSecurityGroupOwnerId'  => owner
+          )
+        end
+
+        def authorize_port_range(range, options = {})
+          connection.authorize_security_group_ingress(
+            'CidrIp'      => options[:cidr_ip] || '0.0.0.0/0',
+            'FromPort'    => range.min,
+            'GroupName'   => @group_name,
+            'ToPort'      => range.max,
+            'IpProtocol'  => options[:ip_protocol] || 'tcp' 
+          )
         end
 
         def destroy
