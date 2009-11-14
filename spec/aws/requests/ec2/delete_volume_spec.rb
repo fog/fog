2b89a9782fb979e0dfae25574c017cb033ab1036
Wesley Beary <wbeary@engineyard.com>
2009-11-13 18:47:52 -0800

    fix typo

--- spec/aws/requests/ec2/delete_volume_spec.rb
+++ spec/aws/requests/ec2/delete_volume_spec.rb
@@ -18,7 +18,7 @@ describe 'EC2.create_volume' do
 
     it "should raise a BadRequest error if volume does not exist" do
       lambda {
-        ec2.release_address('vol-00000000')
+        ec2.delete_volume('vol-00000000')
       }.should raise_error(Excon::Errors::BadRequest)
     end
 
