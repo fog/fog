cf0257a5961087210b9482860de38b8ab01dd2ae
Wesley Beary <wbeary@engineyard.com>
2010-01-04 21:45:37 -0800

    rename slices in slicehost specs to avoid conflicts

--- spec/slicehost/requests/delete_slice_spec.rb
+++ spec/slicehost/requests/delete_slice_spec.rb
@@ -5,7 +5,7 @@ describe 'Slicehost.delete_slice' do
 
     before(:each) do
       # flavor_id 1: 256 ram, image_id 3: Gentoo 2008.0
-      @slice_id = slicehost.create_slice(1, 3, 'fog_slice').body['id']
+      @slice_id = slicehost.create_slice(1, 3, 'fog_delete_slice').body['id']
     end
 
     it "should return proper attributes" do
