6afa66ecb8cce7ba10ea0911f48f9676710b0692
geemus (Wesley Beary) <wbeary@engineyard.com>
2010-02-18 16:57:50 -0800

    add support for describe_reserved_instances to ec2

---
+++ lib/fog/aws/requests/ec2/describe_reserved_instances.rb
@@ -0,0 +1,52 @@
+unless Fog.mocking?
+
+  module Fog
+    module AWS
+      class EC2
+
+        # Describe all or specified reserved instances
+        #
+        # ==== Parameters
+        # * reserved_instances_id<~Array> - List of reserved instance ids to describe, defaults to all
+        #
+        # ==== Returns
+        # * response<~Excon::Response>:
+        #   * body<~Hash>:
+        #     * 'requestId'<~String> - Id of request
+        #     * 'reservedInstancesSet'<~Array>:
+        #       * 'availabilityZone'<~String> - availability zone of the instance
+        #       * 'duration'<~Integer> - duration of reservation, in seconds
+        #       * 'fixedPrice'<~Float> - purchase price of reserved instance
+        #       * 'instanceType'<~String> - type of instance
+        #       * 'instanceCount'<~Integer> - number of reserved instances
+        #       * 'productDescription'<~String> - reserved instance description
+        #       * 'reservedInstancesId'<~String> - id of the instance
+        #       * 'start'<~Time> - start time for reservation
+        #       * 'state'<~String> - state of reserved instance purchase, in .[pending-payment, active, payment-failed, retired]
+        #       * 'usagePrice"<~Float> - usage price of reserved instances, per hour
+        def describe_reserved_instances(reserved_instances_id = [])
+          params = AWS.indexed_param('ReservedInstancesId', reserved_instances_id)
+          request({
+            'Action' => 'DescribeReservedInstances'
+          }.merge!(params), Fog::Parsers::AWS::EC2::DescribeReservedInstances.new)
+        end
+
+      end
+    end
+  end
+
+else
+
+  module Fog
+    module AWS
+      class EC2
+
+        def describe_reserved_instances(reserved_instances_id = {})
+          raise MockNotImplemented.new("Contributions welcome!")
+        end
+
+      end
+    end
+  end
+
+end
