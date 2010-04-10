3aadefe82a6db8db14a4e0f241e637a081796c36
geemus (Wesley Beary) <wbeary@engineyard.com>
2010-04-09 19:21:59 -0700

    [aws] fix state transitions in terminate_instances mock

--- lib/fog/aws/requests/ec2/terminate_instances.rb
+++ lib/fog/aws/requests/ec2/terminate_instances.rb
@@ -47,7 +47,7 @@ module Fog
               instance = @data[:instances][id]
               @data[:deleted_at][id] = Time.now
               # TODO: the codes are mostly educated guessing, not certainty
-              code = case instance['state']
+              code = case instance['instanceState']['name']
               when 'pending'
                 0
               when 'running'
