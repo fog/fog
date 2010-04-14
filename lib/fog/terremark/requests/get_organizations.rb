05f24e4c8ab276ab69994f846f9aff6108e8f92e
Edward Muller <emuller@engineyard.com>
2010-04-15 02:54:38 +0800

    note that terremark said they're going to remove this in the future

--- lib/fog/terremark/requests/get_organizations.rb
+++ lib/fog/terremark/requests/get_organizations.rb
@@ -15,6 +15,7 @@ module Fog
           :expects  => 200,
           :headers  => {
             'Authorization' => "Basic #{Base64.encode64("#{@terremark_username}:#{@terremark_password}").chomp!}",
+            # Terremark said they're going to remove passing in the Content-Type to login in a future release
             'Content-Type'  => "application/vnd.vmware.vcloud.orgList+xml"
           },
           :method   => 'POST',
