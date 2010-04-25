a0ac71b55175d5ffcabfb03b3228cdf0b5a35edf
geemus <geemus@gmail.com>
2010-04-25 11:35:53 -0700

    more detailed deprecation warning

--- lib/fog/deprecation.rb
+++ lib/fog/deprecation.rb
@@ -4,7 +4,7 @@ module Fog
     def deprecate(older, newer)
       class_eval <<-EOS, __FILE__, __LINE__
         def #{older}(*args)
-          Formatador.display_line("[yellow][WARN] fog: #{older} is deprecated, use #{newer} instead[/]")
+          Formatador.display_line("[yellow][WARN] #{self} => ##{older} is deprecated, use ##{newer} instead[/]")
           #{newer}(*args)
         end
       EOS
