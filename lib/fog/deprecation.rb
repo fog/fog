b075de92c2ec6842911baf773ce14dc0496737da
geemus <geemus@gmail.com>
2010-05-03 14:11:11 -0700

    display location of deprecated calls

--- lib/fog/deprecation.rb
+++ lib/fog/deprecation.rb
@@ -4,7 +4,10 @@ module Fog
     def deprecate(older, newer)
       class_eval <<-EOS, __FILE__, __LINE__
         def #{older}(*args)
-          Formatador.display_line("[yellow][WARN] #{self} => ##{older} is deprecated, use ##{newer} instead[/]")
+          location = caller.first
+          warning = "[yellow][WARN] #{self} => ##{older} is deprecated, use ##{newer} instead[/]"
+          warning << " [light_black](" << location << ")[/] "
+          Formatador.display_line(warning)
           #{newer}(*args)
         end
       EOS
