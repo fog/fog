6c08b4cbfaac914ab036155ead6620db023f4bc9
Caleb Tennis <caleb.tennis@gmail.com>
2010-07-26 05:03:25 +0800

    [ec2] Reorder the logic in describing security groups parser.  The way it was before       it was not scanning cidrIp address groups properly, and they weren't showing up.

--- lib/fog/aws/parsers/ec2/describe_security_groups.rb
+++ lib/fog/aws/parsers/ec2/describe_security_groups.rb
@@ -28,6 +28,7 @@ module Fog
             case name
             when 'cidrIp'
               @ip_range[name] = @value
+              puts "got cidr ip #{@value}"
             when 'fromPort', 'toPort'
               @ip_permission[name] = @value.to_i
             when 'groups'
@@ -50,13 +51,13 @@ module Fog
               if @in_groups
                 @ip_permission['groups'] << @group
                 @group = {}
-              elsif @in_ip_permissions
-                @security_group['ipPermissions'] << @ip_permission
-                @ip_permission = { 'groups' => [], 'ipRanges' => []}
               elsif @in_ip_ranges
                 @ip_permission['ipRanges'] << @ip_range
                 @ip_range = {}
-              else
+              elsif @in_ip_permissions
+                @security_group['ipPermissions'] << @ip_permission
+                @ip_permission = { 'groups' => [], 'ipRanges' => []}
+               else
                 @response['securityGroupInfo'] << @security_group
                 @security_group = { 'ipPermissions' => [] }
               end
