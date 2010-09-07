75ea140b7990a6353d5d1e86badb70ae27b4134a
geemus <geemus@gmail.com>
2010-09-07 16:35:19 -0700

    [bluebox] nicer deprecation warning around updated blocks namespacing

--- lib/fog/bluebox.rb
+++ lib/fog/bluebox.rb
@@ -6,5 +6,13 @@ module Fog
     service_path 'fog/bluebox'
     service :blocks
 
+    def self.new(attributes = {})
+      location = caller.first
+      warning = "[yellow][WARN] Fog::Bluebox#new is deprecated, use Fog::Bluebox::Blocks#new instead[/]"
+      warning << " [light_black](" << location << ")[/] "
+      Formatador.display_line(warning)
+      Fog::Bluebox::Blocks.new(attributes)
+    end
+
   end
 end
