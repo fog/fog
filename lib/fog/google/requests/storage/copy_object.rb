48e55bada26098bd1680ff4048f67f889be0dcc2
Ariel Zavala <arielo.zavala@gmail.com>
2010-09-18 15:26:48 -0400

    changed x-amz- to x-goo- in some requests

--- lib/fog/google/requests/storage/copy_object.rb
+++ lib/fog/google/requests/storage/copy_object.rb
@@ -13,11 +13,11 @@ module Fog
         # * target_bucket_name<~String> - Name of bucket to create copy in
         # * target_object_name<~String> - Name for new copy of object
         # * options<~Hash>:
-        #   * 'x-amz-metadata-directive'<~String> - Specifies whether to copy metadata from source or replace with data in request.  Must be in ['COPY', 'REPLACE']
-        #   * 'x-amz-copy_source-if-match'<~String> - Copies object if its etag matches this value
-        #   * 'x-amz-copy_source-if-modified_since'<~Time> - Copies object it it has been modified since this time
-        #   * 'x-amz-copy_source-if-none-match'<~String> - Copies object if its etag does not match this value
-        #   * 'x-amz-copy_source-if-unmodified-since'<~Time> - Copies object it it has not been modified since this time
+        #   * 'x-goog-metadata-directive'<~String> - Specifies whether to copy metadata from source or replace with data in request.  Must be in ['COPY', 'REPLACE']
+        #   * 'x-goog-copy_source-if-match'<~String> - Copies object if its etag matches this value
+        #   * 'x-goog-copy_source-if-modified_since'<~Time> - Copies object it it has been modified since this time
+        #   * 'x-goog-copy_source-if-none-match'<~String> - Copies object if its etag does not match this value
+        #   * 'x-goog-copy_source-if-unmodified-since'<~Time> - Copies object it it has not been modified since this time
         #
         # ==== Returns
         # * response<~Excon::Response>:
@@ -26,7 +26,7 @@ module Fog
         #     * 'LastModified'<~Time> - date object was last modified
         #
         def copy_object(source_bucket_name, source_object_name, target_bucket_name, target_object_name, options = {})
-          headers = { 'x-amz-copy-source' => "/#{source_bucket_name}/#{source_object_name}" }.merge!(options)
+          headers = { 'x-goog-copy-source' => "/#{source_bucket_name}/#{source_object_name}" }.merge!(options)
           request({
             :expects  => 200,
             :headers  => headers,
