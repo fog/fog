ad2a921ea64d6cbadffffa484bcaaa4f693d672d
geemus <geemus@gmail.com>
2010-09-22 12:06:25 -0700

    [aws][compute] add import_key_pair stuff

--- lib/fog/aws/models/compute/key_pair.rb
+++ lib/fog/aws/models/compute/key_pair.rb
@@ -5,11 +5,15 @@ module Fog
     class Compute
 
       class KeyPair < Fog::Model
+        extend Fog::Deprecation
+        deprecate(:material, :private_key)
 
         identity  :name,        :aliases => 'keyName'
 
         attribute :fingerprint, :aliases => 'keyFingerprint'
-        attribute :material,    :aliases => 'keyMaterial'
+        attribute :private_key, :aliases => 'keyMaterial'
+
+        attr_accessor :public_key
 
         def destroy
           requires :name
@@ -21,11 +25,17 @@ module Fog
         def save
           requires :name
 
-          data = connection.create_key_pair(@name).body
+          data = if public_key
+            connection.import_key_pair(name, public_key).body
+          else
+            connection.create_key_pair(name).body
+          end
           new_attributes = data.reject {|key,value| !['keyFingerprint', 'keyMaterial', 'keyName'].include?(key)}
           merge_attributes(new_attributes)
           true
         end
+
+        private
 
       end
 
