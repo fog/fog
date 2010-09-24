f1cb38ab1a4363ab8459d74f6587b97598d9a551
geemus <geemus@gmail.com>
2010-09-24 13:12:01 -0700

    update bin to match latest

--- lib/fog/google/bin.rb
+++ lib/fog/google/bin.rb
@@ -1,41 +1,20 @@
-module Google
+class Google < Fog::Bin
   class << self
 
-    if Fog.credentials[:google_storage_access_key_id] && Fog.credentials[:google_storage_secret_access_key]
-
-      def initialized?
-        true
-      end
-
-      def [](service)
-        @@connections ||= Hash.new do |hash, key|
-          hash[key] = case key
-          when :storage
-            Fog::Google::Storage.new
-          end
+    def [](service)
+      @@connections ||= Hash.new do |hash, key|
+        hash[key] = case key
+        when :storage
+          Fog::Google::Storage.new
         end
-        @@connections[service]
       end
-
-      def services
-        [:storage]
-      end
-
-      for collection in Fog::Google::Storage.collections
-        module_eval <<-EOS, __FILE__, __LINE__
-          def #{collection}
-            self[:storage].#{collection}
-          end
-        EOS
-      end
-
-    else
-
-      def initialized?
-        false
-      end
-
+      @@connections[service]
     end
+
+    def services
+      [:storage]
+    end
+
   end
 
 end
