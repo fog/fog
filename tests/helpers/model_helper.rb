15946def4d0169c3488b4a365986c5184d1e1eb8
geemus <geemus@gmail.com>
2010-09-28 16:01:16 -0700

    update model helper to be more consistent with newer tests

--- tests/helpers/model_helper.rb
+++ tests/helpers/model_helper.rb
@@ -1,47 +1,16 @@
 def tests_models
 
-  after do
-    if @model && !@model.new_record?
-      if @model.respond_to?(:ready?)
-        @model.wait_for { ready? }
-      end
-      @model.destroy
+  tests('model#save') do
+
+    test('does not exist remotely before save') do
+      !@collection.get(@model.identity)
     end
-  end
 
-  tests('collection') do
-
-    test('#all includes persisted models') do
+    test('succeeds') do
       @model.save
-      @collection.all.map {|model| model.identity}.include? @model.identity
     end
 
-    tests('#get') do
-
-      test 'should return a matching model if one exists' do
-        @model.save
-        get = @collection.get(@model.identity)
-        @model.attributes == get.attributes
-      end
-
-      test 'should return nil if no matching model exists' do
-        !@collection.get('0')
-      end
-
-    end
-
-    test('#reload') do
-      @model.save
-      @collection.all
-      reloaded = @collection.reload
-      @collection.attributes == reloaded.attributes
-    end
-
-  end
-
-  tests('model') do
-
-    test('#reload') do
+    test('does exist remotely after save') do
       @model.save
       if @model.respond_to?(:ready?)
         @model.wait_for { ready? }
@@ -50,27 +19,47 @@ def tests_models
       @model.attributes == reloaded.attributes
     end
 
-    tests('#save') do
+  end
 
-      test('does not exist remotely before save') do
-        !@collection.get(@model.identity)
-      end
+  test('model#reload') do
+    if @model.respond_to?(:ready?)
+      @model.wait_for { ready? }
+    end
+    reloaded = @model.reload
+    @model.attributes == reloaded.attributes
+  end
 
-      test('succeeds') do
-        @model.save
-      end
+  test('collection#all includes persisted models') do
+    @model.save
+    @collection.all.map {|model| model.identity}.include? @model.identity
+  end
 
-      test('does exist remotely after save') do
-        @model.save
-        if @model.respond_to?(:ready?)
-          @model.wait_for { ready? }
-        end
-        reloaded = @model.reload
-        @model.attributes == reloaded.attributes
-      end
+  tests('collection#get') do
 
+    test 'should return a matching model if one exists' do
+      @model.save
+      get = @collection.get(@model.identity)
+      @model.attributes == get.attributes
     end
 
+    test 'should return nil if no matching model exists' do
+      !@collection.get('0')
+    end
+
+  end
+
+  test('collection#reload') do
+    @model.save
+    @collection.all
+    reloaded = @collection.reload
+    @collection.attributes == reloaded.attributes
+  end
+
+  test('model#destroy') do
+    if @model.respond_to?(:ready?)
+      @model.wait_for{ ready? }
+    end
+    @model.destroy
   end
 
 end
