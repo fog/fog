068c7162b1ac512b9ca4cbdfe95ac9a61802d136
geemus <geemus@gmail.com>
2010-10-13 13:27:36 -0700

    [aws|storage} more test work

--- tests/aws/requests/storage/object_tests.rb
+++ tests/aws/requests/storage/object_tests.rb
@@ -8,7 +8,21 @@ Shindo.tests('AWS::Storage | object requests', ['aws']) do
       AWS[:storage].put_object(@directory.identity, 'fog_object', lorem_file)
     end
 
-    tests("#get_object")
+    tests("#get_object('#{@directory.identity}', 'fog_object')").returns(lorem_file.read) do
+      AWS[:storage].get_object(@directory.identity, 'fog_object').body
+    end
+
+    tests("#get_object('#{@directory.identity}', 'fog_object', &block)").returns(lorem_file.read) do
+      data = ''
+      AWS[:storage].get_object(@directory.identity, 'fog_object') do |chunk|
+        data << chunk
+      end
+      data
+    end
+
+    tests("#head_object('#{@directory.identity}', 'fog_object')").succeeds do
+      AWS[:storage].head_object(@directory.identity, 'fog_object')
+    end
 
     tests("#delete_object('#{@directory.identity}', 'fog_object')").succeeds do
       AWS[:storage].delete_object(@directory.identity, 'fog_object')
@@ -18,8 +32,24 @@ Shindo.tests('AWS::Storage | object requests', ['aws']) do
 
   tests('failure') do
 
-    tests("#put_object('fognonbucket', 'fog_object', lorem_file)").raises(Excon::Errors::NotFound) do
-      AWS[:storage].put_object('fognonbucket', 'fog_object', lorem_file)
+    tests("#put_object('fognonbucket', 'fog_non_object', lorem_file)").raises(Excon::Errors::NotFound) do
+      AWS[:storage].put_object('fognonbucket', 'fog_non_object', lorem_file)
+    end
+
+    tests("#get_object('fognonbucket', 'fog_non_object')").raises(Excon::Errors::NotFound) do
+      AWS[:storage].get_object('fognonbucket', 'fog_non_object')
+    end
+
+    tests("#get_object('#{@directory.identity}', 'fog_non_object')").raises(Excon::Errors::NotFound) do
+      AWS[:storage].get_object(@directory.identity, 'fog_non_object')
+    end
+
+    tests("#head_object('fognonbucket', 'fog_non_object')").raises(Excon::Errors::NotFound) do
+      AWS[:storage].head_object('fognonbucket', 'fog_non_object')
+    end
+
+    tests("#head_object('#{@directory.identity}', 'fog_non_object')").raises(Excon::Errors::NotFound) do
+      AWS[:storage].head_object(@directory.identity, 'fog_non_object')
     end
 
     tests("#delete_object('fognonbucket', 'fog_non_object')").raises(Excon::Errors::NotFound) do
