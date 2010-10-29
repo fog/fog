85c0c2fbf1d850f326b5fc5cf32a05cb5f067978
geemus <geemus@gmail.com>
2010-10-29 16:38:17 -0700

    [aws&google|storage] cleanup/fixes for bucket acl

--- lib/fog/aws/models/storage/directory.rb
+++ lib/fog/aws/models/storage/directory.rb
@@ -14,6 +14,14 @@ module Fog
 
         attribute :creation_date, :aliases => 'CreationDate'
 
+        def acl=(new_acl)
+          valid_acls = ['private', 'public-read', 'public-read-write', 'authenticated-read']
+          unless valid_acls.include?(new_acl)
+            raise ArgumentError.new("acl must be one of [#{valid_acls.join(', ')}]")
+          end
+          @acl = new_acl
+        end
+
         def destroy
           requires :key
           connection.delete_bucket(key)
@@ -56,6 +64,9 @@ module Fog
         def save
           requires :key
           options = {}
+          if @acl
+            options['x-amz-acl'] = @acl
+          end
           if @location
             options['LocationConstraint'] = @location
           end
--- lib/fog/aws/models/storage/file.rb
+++ lib/fog/aws/models/storage/file.rb
@@ -70,8 +70,11 @@ module Fog
 
         def save(options = {})
           requires :body, :directory, :key
+          if options != {}
+            Formatador.display_line("[yellow][WARN] options param is deprecated, use acl= instead[/] [light_black](#{caller.first})[/]")
+          end
           if @acl
-            options['x-amz-acl'] = @acl
+            options['x-amz-acl'] ||= @acl
           end
           data = connection.put_object(directory.key, @key, @body, options)
           @etag = data.headers['ETag']
