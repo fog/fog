d9ff6e3a717ff704e4fd76c9482ac33403eeb972
joewilliams <joe@der-dieb.local>
2010-11-01 11:52:04 -0700

    fix deprecation warning

--- lib/fog/slicehost.rb
+++ lib/fog/slicehost.rb
@@ -11,7 +11,7 @@ module Fog
 
     def self.new(attributes = {})
       location = caller.first
-      warning = "[yellow][WARN] Fog::Slicehost#new is deprecated, use Fog::Bluebox::Compute#new instead[/]"
+      warning = "[yellow][WARN] Fog::Slicehost#new is deprecated, use Fog::Slicehost::Compute#new instead[/]"
       warning << " [light_black](" << location << ")[/] "
       Formatador.display_line(warning)
       Fog::Slicehost::Compute.new(attributes)
