c0a120a11256120117c20982c07186a6f1df1f2f
Paul Thornthwaite <paul@brightbox.co.uk>
2010-11-11 21:02:20 +0000

    [brightbox] Add check in update requests to ensure some options have been passed in

--- lib/fog/brightbox/requests/compute/update_image.rb
+++ lib/fog/brightbox/requests/compute/update_image.rb
@@ -5,6 +5,7 @@ module Fog
 
         def update_image(identifier, options = {})
           return nil if identifier.nil? || identifier == ""
+          return nil if options.empty? || options.nil?
           request(
             :expects  => [200],
             :method   => 'PUT',
