82e4d245cdfe2ac3fa32ebbea18b76b0158bbf64
mriley <wdperson@hotmail.com>
2010-11-21 09:02:34 -0500

    documentation update for key_pairs and helper

--- tests/helper.rb
+++ tests/helper.rb
@@ -20,6 +20,10 @@ require 'tests/helpers/storage/directories_tests'
 require 'tests/helpers/storage/file_tests'
 require 'tests/helpers/storage/files_tests'
 
+# Use so you can run in mock mode from the command line:
+#
+# FOG_MOCK=true fog
+
 if ENV["FOG_MOCK"] == "true"
   Fog.mock!
 end
