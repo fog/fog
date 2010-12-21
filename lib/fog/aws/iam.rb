a990b73fd156f6e8f595101f343dfed46ab6eeb1
John E. Vincent <lusis.org+github.com@gmail.com>
2010-12-21 14:08:44 -0500

    [aws|iam] get_user implementation

--- lib/fog/aws/iam.rb
+++ lib/fog/aws/iam.rb
@@ -15,6 +15,7 @@ module Fog
       request :delete_group_policy
       request :delete_user
       request :delete_user_policy
+      request :get_user
       request :list_access_keys
       request :list_groups
       request :list_group_policies
---
+++ lib/fog/aws/requests/iam/get_user.rb
@@ -0,0 +1,44 @@
+module Fog
+  module AWS
+    class IAM
+      class Real
+
+        require 'fog/aws/parsers/iam/get_user'
+
+        # Get User
+        # 
+        # ==== Parameters
+        # * options<~Hash>:
+        #   * 'UserName'<~String>: Name of the User. Defaults to current user
+        #
+        # ==== Returns
+        # * response<~Excon::Response>:
+        #   * body<~Hash>:
+        #     * 'User'<~Hash> - User
+        #       * Arn<~String> -
+        #       * UserId<~String> -
+        #       * UserName<~String> -
+        #       * Path<~String> -
+        #
+        # ==== See Also
+        # http://docs.amazonwebservices.com/IAM/latest/APIReference/API_Getuser.html
+        #
+        def get_user(options = {})
+          request({
+            'Action'  => 'GetUser',
+            :parser   => Fog::Parsers::AWS::IAM::GetUser.new
+          }.merge!(options))
+        end
+
+      end
+
+      class Mock
+
+        def get_user(options = {})
+          Fog::Mock.not_implemented
+        end
+
+      end
+    end
+  end
+end
