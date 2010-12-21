6a9b4b0bc609b20e579734d3b2b80d69975479f8
John E. Vincent <lusis.org+github.com@gmail.com>
2010-12-22 03:08:44 +0800

    [aws|iam] get_user implementation

---
+++ lib/fog/aws/parsers/iam/get_user.rb
@@ -0,0 +1,27 @@
+module Fog
+  module Parsers
+    module AWS
+      module IAM
+
+        class GetUser < Fog::Parsers::Base
+        # http://docs.amazonwebservices.com/IAM/latest/APIReference/API_GetUser.html
+
+          def reset
+            @response = { 'User' => {} }
+          end
+
+          def end_element(name)
+            case name
+            when 'Arn', 'UserId', 'UserName', 'Path'
+              @response['User'][name] = @value
+            when 'RequestId'
+              @response[name] = @value
+            end
+          end
+
+        end
+
+      end
+    end
+  end
+end
