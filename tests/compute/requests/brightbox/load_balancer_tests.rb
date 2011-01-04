0a045b0552485df3af06289c8d2767a70e2e3549
geemus <geemus@gmail.com>
2011-01-03 16:21:43 -0800

    [brightbox] skip not yet mocked tests

--- tests/compute/requests/brightbox/load_balancer_tests.rb
+++ tests/compute/requests/brightbox/load_balancer_tests.rb
@@ -2,8 +2,10 @@ Shindo.tests('Brightbox::Compute | load balancer requests', ['brightbox']) do
 
   tests('success') do
 
-    @node = Brightbox[:compute].servers.create(:image_id => Brightbox::Compute::TestSupport::IMAGE_IDENTIFER)
-    node_id = @node.id
+    unless Fog.mocking?
+      @node = Brightbox[:compute].servers.create(:image_id => Brightbox::Compute::TestSupport::IMAGE_IDENTIFER)
+      node_id = @node.id
+    end
 
     creation_args = {
       :nodes => [{
@@ -21,41 +23,51 @@ Shindo.tests('Brightbox::Compute | load balancer requests', ['brightbox']) do
     }
 
     tests("#create_load_balancer(#{creation_args.inspect})").formats(Brightbox::Compute::Formats::Full::LOAD_BALANCER) do
+      pending if Fog.mocking?
       data = Brightbox[:compute].create_load_balancer(creation_args)
       @load_balancer_id = data["id"]
       data
     end
 
-    Brightbox[:compute].load_balancers.get(@load_balancer_id).wait_for { ready? }
+    unless Fog.mocking?
+      Brightbox[:compute].load_balancers.get(@load_balancer_id).wait_for { ready? }
+    end
 
     # tests("#list_load_balancers()").formats(Brightbox::Compute::Formats::Collection::LOAD_BALANCERS) do
     #   Brightbox[:compute].list_load_balancers
     # end
 
     tests("#get_load_balancer('#{@load_balancer_id}')").formats(Brightbox::Compute::Formats::Full::LOAD_BALANCER) do
+      pending if Fog.mocking?
       Brightbox[:compute].get_load_balancer(@load_balancer_id)
     end
 
     tests("#destroy_load_balancer('#{@load_balancer_id}')").formats(Brightbox::Compute::Formats::Full::LOAD_BALANCER) do
+      pending if Fog.mocking?
       Brightbox[:compute].destroy_load_balancer(@load_balancer_id)
     end
 
-    @node.destroy
+    unless Fog.mocking?
+      @node.destroy
+    end
 
   end
 
   tests('failure') do
 
-    tests("#create_load_balancer()").raises(Excon::Errors::BadRequest) do
-      Brightbox[:compute].create_load_balancer()
+    tests("#create_load_balancer").raises(Excon::Errors::BadRequest) do
+      pending if Fog.mocking?
+      Brightbox[:compute].create_load_balancer
     end
 
     tests("#get_load_balancer('lba-00000')").raises(Excon::Errors::NotFound) do
+      pending if Fog.mocking?
       Brightbox[:compute].get_load_balancer('lba-00000')
     end
 
-    tests("#get_load_balancer()").raises(ArgumentError) do
-      Brightbox[:compute].get_load_balancer()
+    tests("#get_load_balancer").raises(ArgumentError) do
+      pending if Fog.mocking?
+      Brightbox[:compute].get_load_balancer
     end
   end
 
