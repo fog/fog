865ac70f615ef2b360367dc96f4cb85610decfb8
geemus <geemus@gmail.com>
2011-01-07 17:07:09 -0800

    [providers] consolidate

--- lib/fog.rb
+++ lib/fog.rb
@@ -8,20 +8,8 @@ module Fog
 
 end
 
-# FIXME: these should go away (force usage of Fog::[Compute, CDN, DNS, Storage]  etc)
-require 'fog/aws'
-require 'fog/bluebox'
-require 'fog/brightbox'
-require 'fog/go_grid'
-require 'fog/google'
-require 'fog/linode'
-require 'fog/local'
-require 'fog/new_servers'
-require 'fog/rackspace'
-require 'fog/slicehost'
-require 'fog/terremark'
-require 'fog/vcloud'
-require 'fog/zerigo'
+# FIXME: this require should go away (force usage of Fog::[Compute, CDN, DNS, Storage]  etc)
+require 'fog/providers'
 
 require 'fog/compute'
 require 'fog/cdn'
