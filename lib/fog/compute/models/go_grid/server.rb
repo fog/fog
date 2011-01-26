a2b28532daa34eb0d3b9e53c7b19143c72b8d18c
Lucas Carlson <lucas@rufy.com>
2011-01-26 02:23:00 -0800

    Various fixes to get the go_grid extension into basic usable shape.

--- lib/fog/compute/models/go_grid/server.rb
+++ lib/fog/compute/models/go_grid/server.rb
@@ -24,13 +24,13 @@ module Fog
 
         def destroy
           requires :id
-          connection.grid_server_destroy(id)
+          connection.grid_server_delete(id)
           true
         end
 
         def image
           requires :image_id
-          connection.grid_image_get(image_id)
+          connection.grid_image_get(:image => image_id)
         end
 
         def ready?
@@ -40,12 +40,48 @@ module Fog
         def save
           raise Fog::Errors::Error.new('Resaving an existing object may create a duplicate') if identity
           requires :name, :image_id, :ip, :memory
-          options['isSandbox'] = sandbox if sandbox
-          options['server.ram'] = memory
-          options['image'] = image_id
+          options = {
+            'isSandbox'   => sandbox,
+            'server.ram'  => memory,
+            'image'       => image_id
+          }
+          options = options.reject {|key, value| value.nil?}
           data = connection.grid_server_add(name, image, ip, options)
+          puts data.body.inspect
           merge_attributes(data.body)
           true
+        end
+
+        def ssh(commands)
+          requires :ip, :identity, :username
+
+          options = {}
+          options[:key_data] = [private_key] if private_key
+          Fog::SSH.new(ip['ip'], username, options).run(commands)
+        end
+
+        def setup(credentials = {})
+          requires :ip, :identity, :public_key, :username
+          Fog::SSH.new(ip['ip'], username, credentials).run([
+            %{mkdir .ssh},
+            %{echo "#{public_key}" >> ~/.ssh/authorized_keys},
+            %{passwd -l root},
+            %{echo "#{attributes.to_json}" >> ~/attributes.json},
+            %{echo "#{metadata.to_json}" >> ~/metadata.json}
+          ])
+        rescue Errno::ECONNREFUSED
+          sleep(1)
+          retry
+        end
+
+        def username
+          @username ||= 'root'
+        end
+
+        private
+
+        def adminPass=(new_admin_pass)
+          @password = new_admin_pass
         end
 
       end
