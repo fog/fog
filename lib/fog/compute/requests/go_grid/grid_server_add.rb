2ba48de69fb5a88a8af4536c9f88812ae2518412
Lucas Carlson <lucas@rufy.com>
2011-01-26 02:23:00 -0800

    Various fixes to get the go_grid extension into basic usable shape.

--- lib/fog/compute/requests/go_grid/grid_server_add.rb
+++ lib/fog/compute/requests/go_grid/grid_server_add.rb
@@ -6,11 +6,11 @@ module Fog
         # Create a new server
         #
         # ==== Parameters
+        # * 'name'<~String>   - name of the server, 20 or fewer characters
         # * 'image'<~String>  - image to use, in grid_image_list
         # * 'ip'<~String> - initial public ip for this server
-        # * 'name'<~String>   - name of the server, 20 or fewer characters
-        # * 'server_ram'<~String> - flavor to use, in common_lookup_list('server.ram')
         # * 'options'<~Hash>:
+        #   * 'server.ram'<~String> - flavor to use, in common_lookup_list('server.ram')
         #   * 'description'<~String>  - description of this server
         #   * 'isSandbox'<~String>    - treat this server as image sandbox? in ['true', 'false']
         #
@@ -18,14 +18,13 @@ module Fog
         # * response<~Excon::Response>:
         #   * body<~Array>:
         # TODO: docs
-        def grid_server_add(image, ip, name, server_ram, options={})
+        def grid_server_add(name, image, ip, options={})
           request(
             :path     => 'grid/server/add',
             :query    => {
               'image'       => image,
               'ip'          => ip,
               'name'        => name,
-              'server.ram'  => server_ram
             }.merge!(options)
           )
         end
