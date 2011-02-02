729da56d61159521c10442811b68a3ec6519220d
phiggins <pete@peterhiggins.org>
2011-02-01 22:20:08 -0800

    Remove copied-and-pasted documentation.

--- lib/fog/aws/requests/sns/list_topics.rb
+++ lib/fog/aws/requests/sns/list_topics.rb
@@ -5,30 +5,6 @@ module Fog
 
         require 'fog/aws/parsers/sns/list_topics'
 
-        # List users
-        #
-        # ==== Parameters
-        # * options<~Hash>:
-        #   * 'Marker'<~String>: used to paginate subsequent requests
-        #   * 'MaxItems'<~Integer>: limit results to this number per page
-        #   * 'PathPrefix'<~String>: prefix for filtering results
-        #
-        # ==== Returns
-        # * response<~Excon::Response>:
-        #   * body<~Hash>:
-        #     * 'Users'<~Array> - Matching groups
-        #       * user<~Hash>:
-        #         * Arn<~String> -
-        #         * Path<~String> -
-        #         * UserId<~String> -
-        #         * UserName<~String> -
-        #     * 'IsTruncated<~Boolean> - Whether or not results were truncated
-        #     * 'Marker'<~String> - appears when IsTruncated is true as the next marker to use
-        #     * 'RequestId'<~String> - Id of the request
-        #
-        # ==== See Also
-        # http://docs.amazonwebservices.com/IAM/latest/APIReference/API_ListUsers.html
-        #
         def list_topics(options = {})
           request({
             'Action'  => 'ListTopics',
