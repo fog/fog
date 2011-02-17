4da8bc58ebcbbf806ca40d92433a31df11a65ed0
geemus <geemus@gmail.com>
2011-02-16 17:31:29 -0800

    finish flattening ecloud in preperation for namespace change

--- lib/fog/vcloud.rb
+++ lib/fog/vcloud.rb
@@ -1,9 +1,1 @@
-require 'fog/core'
-
-require 'builder'
-require 'fog/vcloud/model'
-require 'fog/vcloud/collection'
-require 'fog/vcloud/mock_data_classes'
-# ecloud/vcloud requires at the bottom so that the following will be defined
-
 require 'fog/vcloud/terremark/ecloud'
