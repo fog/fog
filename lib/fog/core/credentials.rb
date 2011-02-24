2006ff3259baad232b192708b09dc12dceb86d21
Darrin Eden <darrin.eden@gmail.com>
2011-02-23 21:54:13 -0800

    dnsimple credentials default

--- lib/fog/core/credentials.rb
+++ lib/fog/core/credentials.rb
@@ -77,6 +77,8 @@ An alternate file may be used by placing its path in the FOG_RC environment vari
   :terremark_password:
   :zerigo_email:
   :zerigo_token:
+  :dnsimple_email:
+  :dnsimple_password:
 #
 # End of Fog Credentials File
 #######################################################
