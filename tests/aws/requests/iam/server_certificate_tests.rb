1fa9274fdf64ebf67c058ca282fa695c11c97e3f
Aaron Suggs <aaron@ktheory.com>
2011-05-07 14:09:33 -0400

    [aws|iam] Added server certificate tests
    Added tests for:
    AWS[:iam].upload_server_certificate
    AWS[:iam].delete_server_certificate

---
+++ tests/aws/requests/iam/server_certificate_tests.rb
@@ -0,0 +1,26 @@
+Shindo.tests('AWS::IAM | server certificate requests', ['aws']) do
+
+  pending if Fog.mocking?
+
+  @key_name = 'fog-test'
+
+  @upload_format = {
+    'Certificate' => {
+    'Arn' => String,
+    'Path' => String,
+    'ServerCertificateId' => String,
+    'ServerCertificateName' => String,
+    'UploadDate' => String
+  },
+    'RequestId' => String
+  }
+  tests('#upload_server_certificate').formats(@upload_format) do
+    public_key  = AWS::IAM::SERVER_CERT_PUBLIC_KEY
+    private_key = AWS::IAM::SERVER_CERT_PRIVATE_KEY
+    AWS[:iam].upload_server_certificate(public_key, private_key, @key_name).body
+  end
+
+  tests('#delete_server_certificate').formats(AWS::IAM::Formats::BASIC) do
+    AWS[:iam].delete_server_certificate(@key_name).body
+  end
+end
