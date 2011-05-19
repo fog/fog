bdc84813f941415c9c00a6668a4876a73ecdd278
anomalousthought <joseph@anomalousthought.com>
2011-05-19 10:05:33 -0400

    Add reset method to other providers in addition to Compute providers.

--- lib/fog/dns/zerigo.rb
+++ lib/fog/dns/zerigo.rb
@@ -36,6 +36,10 @@ module Fog
           end
         end
 
+        def self.reset
+          @data = nil
+        end
+
         def initialize(options={})
           unless options.delete(:provider)
             location = caller.first
