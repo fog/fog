96ec6066e49144f8cbe299c5e6c1b55d9978e116
Henry Addison <hja22@me.com>
2011-05-23 11:18:22 +0100

    renamed argument for all method in cloud watch metric_statistics from filters to conditions as filters has a meaning in fog

--- lib/fog/aws/models/cloud_watch/metric_statistics.rb
+++ lib/fog/aws/models/cloud_watch/metric_statistics.rb
@@ -7,11 +7,11 @@ module Fog
       class MetricStatistics < Fog::Collection
         model Fog::AWS::CloudWatch::MetricStatistic
         
-        def all(filters)
-          metricName = filters['MetricName']
-          namespace = filters['Namespace']
-          dimensions = filters['Dimensions']
-          data = connection.get_metric_statistics(filters).body['GetMetricStatisticsResult']['Datapoints']
+        def all(conditions)
+          metricName = conditions['MetricName']
+          namespace = conditions['Namespace']
+          dimensions = conditions['Dimensions']
+          data = connection.get_metric_statistics(conditions).body['GetMetricStatisticsResult']['Datapoints']
           data.collect! { |datum| datum.merge('MetricName' => metricName, 'Namespace' => namespace, 'Dimensions' => dimensions) }
           load(data) # data is an array of attribute hashes
         end
