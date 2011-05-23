8232576e0d6cafa3cdfe9be1951d48418a074c40
Henry Addison <hja22@me.com>
2011-05-23 16:28:33 +0100

    Added some defaults for StartTime, EndTime and Period to model for MetricStatistics

--- lib/fog/aws/models/cloud_watch/metric_statistics.rb
+++ lib/fog/aws/models/cloud_watch/metric_statistics.rb
@@ -11,7 +11,8 @@ module Fog
           metricName = conditions['MetricName']
           namespace = conditions['Namespace']
           dimensions = conditions['Dimensions']
-          data = connection.get_metric_statistics(conditions).body['GetMetricStatisticsResult']['Datapoints']
+          get_metric_opts = {"StartTime" => (Time.now-3600).iso8601, "EndTime" => Time.now.iso8601, "Period" => 300}.merge(conditions)
+          data = connection.get_metric_statistics(get_metric_opts).body['GetMetricStatisticsResult']['Datapoints']
           data.collect! { |datum| datum.merge('MetricName' => metricName, 'Namespace' => namespace, 'Dimensions' => dimensions) }
           load(data) # data is an array of attribute hashes
         end
