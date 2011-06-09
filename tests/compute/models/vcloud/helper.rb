ef95bbdf32adff6d4295501d31155ab502db8a47
Lincoln Stoll <me@lstoll.net>
2011-06-09 13:54:55 +1000

    [vcloud|compute] fix error messages in test helper

--- tests/compute/models/vcloud/helper.rb
+++ tests/compute/models/vcloud/helper.rb
@@ -2,15 +2,11 @@ class Vcloud
   module Compute
     module TestSupport
       def self.template
-        begin
-          template_name = ENV['VCLOUD_TEMPLATE']
-          raise unless template_name
-          uri = Vcloud.catalogs.first.catalog_items.select {|ci| ci.name == template_name }[0].href
-          raise unless uri
-        rescue
-          raise('Specify VApp template name in VCLOUD_TEMPLATE env var')
-        end
-        uri
+        template_name = ENV['VCLOUD_TEMPLATE']
+        raise "Specify VApp template name in VCLOUD_TEMPLATE env var" unless template_name
+        template_res = Vcloud.catalogs.first.catalog_items.select {|ci| ci.name == template_name }[0]
+        raise "URI Not found for specified template - check template name" unless template_res
+        template_res.href
       end
     end
     module Formats
