10ed24606e32045f39edca4368d6c0ddc9b66727
Rupak Ganguly <rupakg@gmail.com>
2011-06-15 18:00:20 -0400

    Add implementation for #head_object method.

---
+++ lib/fog/storage/requests/hp/head_object.rb
@@ -0,0 +1,24 @@
+module Fog
+  module HP
+    class Storage
+      class Real
+
+        # Get headers for object
+        #
+        # ==== Parameters
+        # * container<~String> - Name of container to look in
+        # * object<~String> - Name of object to look for
+        #
+        def head_object(container, object)
+          response = request({
+            :expects  => 200,
+            :method   => 'HEAD',
+            :path     => "#{URI.escape(container)}/#{URI.escape(object)}"
+          }, false)
+          response
+        end
+
+      end
+    end
+  end
+end
