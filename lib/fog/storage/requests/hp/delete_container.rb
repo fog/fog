0210723e1efffb6ea18d8b0ce09db1bbae437cd2
Rupak Ganguly <rupakg@gmail.com>
2011-06-15 02:15:06 -0400

    Add #get_containers, #get_container, #put_container and #delete_container methods to the storage service.

---
+++ lib/fog/storage/requests/hp/delete_container.rb
@@ -0,0 +1,23 @@
+module Fog
+  module HP
+    class Storage
+      class Real
+
+        # Delete an existing container
+        #
+        # ==== Parameters
+        # * name<~String> - Name of container to delete
+        #
+        def delete_container(name)
+          response = request(
+            :expects  => 204,
+            :method   => 'DELETE',
+            :path     => URI.escape(name)
+          )
+          response
+        end
+
+      end
+    end
+  end
+end
---
+++ lib/fog/storage/requests/hp/put_container.rb
@@ -0,0 +1,23 @@
+module Fog
+  module HP
+    class Storage
+      class Real
+
+        # Create a new container
+        #
+        # ==== Parameters
+        # * name<~String> - Name for container, should be < 256 bytes and must not contain '/'
+        #
+        def put_container(name)
+          response = request(
+            :expects  => [201, 202],
+            :method   => 'PUT',
+            :path     => URI.escape(name)
+          )
+          response
+        end
+
+      end
+    end
+  end
+end
