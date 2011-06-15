127484adbfaf6a0e6a248f90ed7e0fcc53764196
Rupak Ganguly <rupakg@gmail.com>
2011-06-15 17:59:47 -0400

    Add implementation for #get_object method.

---
+++ lib/fog/storage/requests/hp/get_object.rb
@@ -0,0 +1,25 @@
+module Fog
+  module HP
+    class Storage
+      class Real
+
+        # Get details for an object
+        #
+        # ==== Parameters
+        # * container<~String> - Name of container to look in
+        # * object<~String> - Name of object to look for
+        #
+        def get_object(container, object, &block)
+          response = request({
+            :block    => block,
+            :expects  => 200,
+            :method   => 'GET',
+            :path     => "#{URI.escape(container)}/#{URI.escape(object)}"
+          }, false, &block)
+          response
+        end
+
+      end
+    end
+  end
+end
