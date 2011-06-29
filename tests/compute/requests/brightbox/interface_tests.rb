1d89eec55ac9fccf04a3413b592a1a0d1211bbc9
Paul Thornthwaite <paul@brightbox.co.uk>
2011-06-29 09:17:24 +0100

    [compute|brightbox] Fixed deprecated use of Brightbox[:compute] to new Compute[:brightbox]

--- tests/compute/requests/brightbox/interface_tests.rb
+++ tests/compute/requests/brightbox/interface_tests.rb
@@ -3,13 +3,13 @@ Shindo.tests('Fog::Compute[:brightbox] | interface requests', ['brightbox']) do
   tests('success') do
 
     unless Fog.mocking?
-      server = Brightbox[:compute].servers.first
+      server = Fog::Compute[:brightbox].servers.first
       @interface_id = server.interfaces.first["id"]
     end
 
     tests("#get_interface('#{@interface_id}')").formats(Brightbox::Compute::Formats::Full::INTERFACE) do
       pending if Fog.mocking?
-      Brightbox[:compute].get_interface(@interface_id)
+      Fog::Compute[:brightbox].get_interface(@interface_id)
     end
 
   end
@@ -18,12 +18,12 @@ Shindo.tests('Fog::Compute[:brightbox] | interface requests', ['brightbox']) do
 
     tests("#get_interface('int-00000')").raises(Excon::Errors::Forbidden) do
       pending if Fog.mocking?
-      Brightbox[:compute].get_interface('int-00000')
+      Fog::Compute[:brightbox].get_interface('int-00000')
     end
 
     tests("#get_interface()").raises(ArgumentError) do
       pending if Fog.mocking?
-      Brightbox[:compute].get_interface()
+      Fog::Compute[:brightbox].get_interface()
     end
   end
 
