83f1a6adebdb0ed2caf2315eced823bfefd11630
Aaron Suggs <aaron@ktheory.com>
2011-07-13 17:18:58 -0400

    [aws|acs] Create ACS security_group model and collection

---
+++ lib/fog/aws/models/acs/security_group.rb
@@ -0,0 +1,42 @@
+require 'fog/core/model'
+
+module Fog
+  module AWS
+    class ACS
+
+      class SecurityGroup < Fog::Model
+
+        identity :id, :aliases => 'CacheSecurityGroupName'
+        attribute :description, :aliases => 'CacheSecurityGroupDescription'
+        attribute :ec2_security_group, :aliases => 'EC2SecurityGroups', :type => :array
+        attribute :owner_id, :aliases => 'OwnerId'
+
+        def ready?
+          ec2_security_groups.all?{|ingress| ingress['Status'] == 'authorized'}
+        end
+
+        def destroy
+          requires :id
+          connection.delete_cache_security_group(id)
+          true
+        end
+
+        def save
+          requires :id
+          requires :description
+          connection.create_cache_security_group(id, description)
+        end
+
+        def authorize_ec2_group(group_name, group_owner_id=owner_id)
+          connection.authorize_ec2_security_group(id, group_name, group_owner_id)
+        end
+
+        def revoke_ec2_group(group_name, group_owner_id=owner_id)
+          connection.revoke_ec2_security_group(id, group_name, group_owner_id)
+        end
+
+      end
+
+    end
+  end
+end
---
+++ lib/fog/aws/models/acs/security_groups.rb
@@ -0,0 +1,26 @@
+require 'fog/core/collection'
+require 'fog/aws/models/acs/security_group'
+
+module Fog
+  module AWS
+    class ACS
+
+      class SecurityGroups < Fog::Collection
+        model Fog::AWS::ACS::SecurityGroup
+
+        def all
+          data = connection.describe_cache_security_groups.body['CacheSecurityGroups']
+          load(data)
+        end
+
+        def get(identity)
+          data = connection.describe_cache_security_groups('CacheSecurityGroupName' => identity).body['CacheSecurityGroups'].first
+          new(data)
+        rescue Fog::AWS::ACS::NotFound
+          nil
+        end
+      end
+
+    end
+  end
+end
