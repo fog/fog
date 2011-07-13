e9553e6fc7db8115075052c19965c4a504dda4d5
Rupak Ganguly <rupakg@gmail.com>
2011-07-13 18:09:13 -0400

    Add a options hash so that headers can be passed in to set acls.

--- lib/fog/storage/requests/hp/put_container.rb
+++ lib/fog/storage/requests/hp/put_container.rb
@@ -8,9 +8,10 @@ module Fog
         # ==== Parameters
         # * name<~String> - Name for container, should be < 256 bytes and must not contain '/'
         #
-        def put_container(name)
+        def put_container(name, options = {})
           response = request(
             :expects  => [201, 202],
+            :headers  => options,
             :method   => 'PUT',
             :path     => URI.escape(name)
           )
