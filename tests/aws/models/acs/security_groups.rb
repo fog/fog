580d49e8ff4c2a0d6782c790083167a8dfa8dd34
Aaron Suggs <aaron@ktheory.com>
2011-07-14 10:45:52 -0400

    [aws|acs] Improve security group tests

--- tests/aws/models/acs/security_groups.rb
+++ tests/aws/models/acs/security_groups.rb
@@ -5,9 +5,28 @@ Shindo.tests('AWS::ACS | security groups', ['aws', 'acs']) do
   pending if Fog.mocking?
 
   model_tests(AWS[:acs].security_groups, {:id => group_name, :description => description}, false) do
-    # TODO:
-    # test authorize_ec2_group
-    # test revoke_ec2_group
+
+    # An EC2 group to authorize
+    ec2_group = Fog::Compute.new(:provider => 'AWS').security_groups.create(:name => 'fog-test-acs', :description => 'fog test')
+
+    # Reload to get the instance owner_id
+    @instance.reload
+
+    tests('#authorize_ec2_group') do
+      @instance.authorize_ec2_group(ec2_group.name)
+      returns('authorizing') { @instance.ec2_groups.detect{|g| g['EC2SecurityGroupName'] == ec2_group.name}['Status'] }
+      returns(false, 'not ready') { @instance.ready? }
+    end
+
+    @instance.wait_for { ready? }
+
+    tests('#revoke_ec2_group') do
+      @instance.revoke_ec2_group(ec2_group.name)
+      returns('revoking') { @instance.ec2_groups.detect{|g| g['EC2SecurityGroupName'] == ec2_group.name}['Status'] }
+      returns(false, 'not ready') { @instance.ready? }
+    end
+
+    ec2_group.destroy
   end
 
   collection_tests(AWS[:acs].security_groups, {:id => group_name, :description => description}, false)
