05a2384ffef815ce63dde4ccee339a65447282c2
geemus <geemus@gmail.com>
2011-07-21 16:46:43 -0500

    [aws|autoscaling] mark unimplemented/unsupported tests as pending

--- tests/aws/models/auto_scaling/instances_tests.rb
+++ tests/aws/models/auto_scaling/instances_tests.rb
@@ -1,5 +1,6 @@
 Shindo.tests('AWS::AutoScaling | instances', ['aws', 'auto_scaling_m']) do
 
-  collection_tests(AWS[:auto_scaling].instances, {}, false)
+  pending # FIXME: instance#save is not defined
+  #collection_tests(AWS[:auto_scaling].instances, {}, false)
 
 end
