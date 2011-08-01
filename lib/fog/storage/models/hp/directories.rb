1215f0872549eff736721e4b7e3c5b88ce97cb50
Rupak Ganguly <rupakg@gmail.com>
2011-08-01 15:04:20 -0400

    Retrieve acl headers if present and set the acl string on a directory.

--- lib/fog/storage/models/hp/directories.rb
+++ lib/fog/storage/models/hp/directories.rb
@@ -15,12 +15,23 @@ module Fog
         end
 
         def get(key, options = {})
+          read_header = nil
+          write_header = nil
           data = connection.get_container(key, options)
           directory = new(:key => key)
           for key, value in data.headers
             if ['X-Container-Bytes-Used', 'X-Container-Object-Count'].include?(key)
               directory.merge_attributes(key => value)
             end
+            if key == 'X-Container-Read'
+              read_header = value
+            elsif key == 'X-Container-Write'
+              write_header = value
+            end
+          end
+          # set the acl on the directory based on the headers
+          if !(read_header.nil? && write_header.nil?)
+            directory.acl = connection.header_to_acl(read_header, write_header)
           end
           directory.files.merge_attributes(options)
           directory.files.instance_variable_set(:@loaded, true)
