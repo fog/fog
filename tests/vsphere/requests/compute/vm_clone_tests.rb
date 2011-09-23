af7ebb2ad8309f0aa09b25847dc87b7d6a568d86
geemus <geemus@gmail.com>
2011-09-22 19:54:11 -0500

    [vsphere|compute] mark test requiring guid pending, as require can not be found

--- tests/vsphere/requests/compute/vm_clone_tests.rb
+++ tests/vsphere/requests/compute/vm_clone_tests.rb
@@ -1,5 +1,5 @@
 Shindo.tests("Fog::Compute[:servers] | vm_clone request") do
-  require 'guid'
+  #require 'guid'
   template = "50323f93-6835-1178-8b8f-9e2109890e1a"
   compute = Fog::Compute[:vsphere]
 
@@ -17,6 +17,7 @@ Shindo.tests("Fog::Compute[:servers] | vm_clone request") do
       compute.vm_clone('instance_uuid' => '123', 'name' => 'jefftest')
     end
     raises(Fog::Compute::Vsphere::NotFound, 'it should raise Fog::Compute::Vsphere::NotFound when the UUID is not a string') do
+      pending # require 'guid'
       compute.vm_clone('instance_uuid' => Guid.from_s(template), 'name' => 'jefftestfoo')
     end
   end
