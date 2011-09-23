7ac7bbe3afde96d10d478c50c4f23516d5eb8978
geemus <geemus@gmail.com>
2011-09-23 16:31:18 -0500

    [core] make sure credentials tests properly reset after completion

--- tests/core/credential_tests.rb
+++ tests/core/credential_tests.rb
@@ -1,16 +1,12 @@
 Shindo.tests do
   before do
-    @old_home = ENV['HOME']
-    @old_rc   = ENV['FOG_RC']
-    @old_credential = ENV['FOG_CREDENTIAL']
+    @old_credentials = Fog.credentials
     Fog.instance_variable_set('@credential_path', nil) # kill memoization
     Fog.instance_variable_set('@credential', nil) # kill memoization
   end
 
   after do
-    ENV['HOME'] = @old_home
-    ENV['FOG_RC'] = @old_rc
-    ENV['FOG_CREDENTIAL'] = @old_credential
+    Fog.credentials = @old_credentials
   end
 
   tests('credential') do
