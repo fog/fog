7ac96b96e378af472e190c25635b1f498b63a4b7
Peter Meier <peter.meier@immerda.ch>
2011-09-27 11:26:54 +0200

    [core] fix attribute squashing with : in key
    It can happen, that the squashing options conatins a : within its key,
    for example: :squash => :'ovf:Item' . However this fails if we don't
    quote the used symbol.
    
    Note that :a == :'a', hence we don't change anything on how things work.

--- lib/fog/core/attributes.rb
+++ lib/fog/core/attributes.rb
@@ -71,8 +71,8 @@ module Fog
             class_eval <<-EOS, __FILE__, __LINE__
               def #{name}=(new_data)
                 if new_data.is_a?(Hash)
-                  if new_data.has_key?(:#{squash})
-                    attributes[:#{name}] = new_data[:#{squash}]
+                  if new_data.has_key?(:'#{squash}')
+                    attributes[:#{name}] = new_data[:'#{squash}']
                   elsif new_data.has_key?("#{squash}")
                     attributes[:#{name}] = new_data["#{squash}"]
                   else
