6fb4247c0707dbc755ab44f8e646898e80c51cd1
Paul Thornthwaite <paul@brightbox.co.uk>
2011-09-30 15:06:22 +0100

    [brightbox] Updated default Ubuntu image

--- lib/fog/brightbox/models/compute/server.rb
+++ lib/fog/brightbox/models/compute/server.rb
@@ -36,7 +36,7 @@ module Fog
         attribute :server_type
 
         def initialize(attributes={})
-          self.image_id   ||= 'img-2ab98' # Ubuntu Lucid 10.04 server (i686)
+          self.image_id   ||= 'img-4gqhs' # Ubuntu Lucid 10.04 server (i686)
           super
         end
 
