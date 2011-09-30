57f9b309aa39ecb4e19b5fe575db776f2f0294a5
Michael Zeng <michaelzeng@swipely.com>
2011-09-30 13:55:43 -0400

    fixing describe_alarms and describe_alarms_for_metric requests

--- lib/fog/aws/requests/cloud_watch/describe_alarms.rb
+++ lib/fog/aws/requests/cloud_watch/describe_alarms.rb
@@ -3,18 +3,18 @@ module Fog
     class CloudWatch
       class Real     
       
-      	require 'fog/aws/parsers/cloud_watch/describe_alarms_for_metric'
+      	require 'fog/aws/parsers/cloud_watch/describe_alarms'
       
-    	# Retrieves all alarms for a single metric
+    	# Retrieves alarms with the specified names
         # ==== Options
-        # * Dimensions<~Array>: a list of dimensions to filter against
-        #     Name : The name of the dimension
-        #     Value : The value to filter against
-        # * MetricName<~String>: The name of the metric
-        # * Namespace<~String>: The namespace of the metric
-        # * Period<~Integer>: The period in seconds over which the statistic is applied
-        # * Statistics<~String>: The statistic for the metric
-        # * Unit<~String> The unit for the metric
+        # * ActionPrefix<~String>: The action name prefix
+        # * AlarmNamePrefix<~String>: The alarm name prefix. 
+        # 				AlarmNames cannot be specified if this parameter is specified
+        # * AlarmNames<~Array>: An array of alarm names to retrieve information for. 
+        # * MaxRecords<~Integer>: The maximum number of alarm descriptions to retrieve
+        # * NextToken<~String>: The token returned by a previous call to indicate that there is more data available
+        # * NextToken<~String> The token returned by a previous call to indicate that there is more data available
+        # * StateValue<~String>: The state value to be used in matching alarms
         #
         # ==== Returns
         # * response<~Excon::Response>:
@@ -24,18 +24,16 @@ module Fog
         #
       
       
-        def describe_alarms_for_metric(options)
-          if dimensions = options.delete('Dimensions')
-            options.merge!(AWS.indexed_param('Dimensions.member.%d.Name', dimensions.collect {|dimension| dimension['Name']}))
-            options.merge!(AWS.indexed_param('Dimensions.member.%d.Value', dimensions.collect {|dimension| dimension['Value']}))
+        def describe_alarms(options={})
+          if alarm_names = options.delete('AlarmNames')
+            options.merge!(AWS.indexed_param('AlarmNames.member.%d', [*alarm_names]))
           end
           request({
               'Action'    => 'DescribeAlarms',
-              :parser     => Fog::Parsers::AWS::CloudWatch::DescribeAlarmsForMetric.new
+              :parser     => Fog::Parsers::AWS::CloudWatch::DescribeAlarms.new
             }.merge(options))
         end
       end     
     end
   end
 end
-
