d96b5ff7b95b29c38e5abd60705529c5dc2f3937
Thom May <thom@digital-science.com>
2011-11-16 10:18:55 +0000

    Split [AWS|STS] tests into separate files per #609

--- tests/aws/requests/sts/session_token_tests.rb
+++ tests/aws/requests/sts/session_token_tests.rb
@@ -13,22 +13,4 @@ Shindo.tests('AWS::STS | session tokens', ['aws']) do
 		Fog::AWS[:sts].get_session_token.body
 	end
 	
-	@policy = {"Statement" => [{"Effect" => "Allow", "Action" => "*", "Resource" => "*"}]}
-
-	@federation_format = {
-		'SessionToken' => String,
-		'SecretAccessKey' => String,
-		'Expiration' => String,
-		'AccessKeyId' => String,
-		'Arn' => String,
-		'FederatedUserId' => String,
-		'PackedPolicySize' => String,
-		'RequestId'	=> String
-	}
-
-	tests("#get_federation_token('test@fog.io', #{@policy.inspect})").formats(@federation_format) do
-		pending if Fog.mocking?
-		Fog::AWS[:sts].get_federation_token("test@fog.io", @policy).body
-	end
-	
 end
