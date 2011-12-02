e62b69fb1969ffcb8f921ee7842ad36913cd8dbf
geemus <geemus@gmail.com>
2011-12-02 10:00:41 -0600

    [vcloud|compute] stopgap fix for test data files
    stopgap for #638

--- tests/vcloud/models/compute/conn_helper.rb
+++ tests/vcloud/models/compute/conn_helper.rb
@@ -4,8 +4,14 @@ module Fog
     end
 
     def request(params, &block)
+      path = File.expand_path(File.join(File.dirname(__FILE__),'..','..','data',params[:path].gsub(/^\//,'').gsub('/','_+_')))
+      if File.exists?(path)
+        body = File.read(path)
+      else
+        ''
+      end
       Excon::Response.new(
-        :body => File.read(File.expand_path(File.join(File.dirname(__FILE__),'..','..','data',params[:path].gsub(/^\//,'').gsub('/','_+_')))),
+        :body   => body,
         :status => 200,
         :header => '')
     end
