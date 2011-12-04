473513e68a5735d820db68fac1c1a3ff0f3a6f9b
geemus <geemus@gmail.com>
2011-12-04 13:42:19 -0600

    [vcloud|compute] properly namespace vcloud test to prevent breaking others
    closes #638

--- tests/vcloud/models/compute/conn_helper.rb
+++ tests/vcloud/models/compute/conn_helper.rb
@@ -1,19 +1,20 @@
 module Fog
-  class Connection
-    def initialize(url, persistent=false, params={})
-    end
-
-    def request(params, &block)
-      path = File.expand_path(File.join(File.dirname(__FILE__),'..','..','data',params[:path].gsub(/^\//,'').gsub('/','_+_')))
-      if File.exists?(path)
-        body = File.read(path)
-      else
-        ''
+  module Vcloud
+    class Compute < Fog::Service
+      class Real
+        def request(params, &block)
+          path = File.expand_path(File.join(File.dirname(__FILE__),'..','..','data',params[:path].gsub(/^\//,'').gsub('/','_+_')))
+          if File.exists?(path)
+            body = File.read(path)
+          else
+            ''
+          end
+          Excon::Response.new(
+            :body   => body,
+            :status => 200,
+            :header => '')
+        end
       end
-      Excon::Response.new(
-        :body   => body,
-        :status => 200,
-        :header => '')
     end
   end
 end
