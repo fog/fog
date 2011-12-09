815a05d23d318371400233c666cf72dabcb0c92d
Rupak Ganguly <rupakg@gmail.com>
2011-12-09 13:32:15 -0500

    Revise implementation for returning private and public ip addresses for a given server, along with mocking support.

--- lib/fog/hp/requests/compute/list_server_private_addresses.rb
+++ lib/fog/hp/requests/compute/list_server_private_addresses.rb
@@ -7,28 +7,44 @@ module Fog
         #
         # ==== Parameters
         # * server_id<~Integer> - Id of server to list addresses for
+        # * network_name<~String> - The name of the network name i.e. public, private or custom name
         #
         # ==== Returns
         # * response<~Excon::Response>:
         #   * body<~Hash>:
-        #     * 'private'<~Array> - Public ip addresses
-        def list_server_private_addresses(server_id)
-          request(
-            :expects  => [200, 203],
+        #     * 'private'<~Array> - Private ip addresses
+        def list_server_private_addresses(server_id, network_name)
+          response = request(
+            :expects  => 200,
             :method   => 'GET',
-            :path     => "servers/#{server_id}/ips/private.json"
+            :path     => "servers/#{server_id}/ips/#{network_name}.json"
           )
+          # return the first address
+          private_address = []
+          data = response.body["#{network_name}"][0]
+          if data
+            private_address << data
+          end
+
+          response.body = { 'private' => private_address }
+          response
         end
 
       end
 
       class Mock
 
-        def list_server_private_addresses(server_id)
+        def list_server_private_addresses(server_id, network_name)
           response = Excon::Response.new
           if server = list_servers_detail.body['servers'].detect {|_| _['id'] == server_id}
-            response.status = [200, 203][rand(1)]
-            response.body = { 'private' => server['addresses']['private'] }
+            private_address = []
+            data = server['addresses']["#{network_name}"][0]
+            if data
+              private_address << data
+            end
+
+            response.status = 200
+            response.body = { 'private' => private_address }
             response
           else
             raise Fog::Compute::HP::NotFound
--- lib/fog/hp/requests/compute/list_server_public_addresses.rb
+++ lib/fog/hp/requests/compute/list_server_public_addresses.rb
@@ -7,28 +7,45 @@ module Fog
         #
         # ==== Parameters
         # * server_id<~Integer> - Id of server to list addresses for
+        # * network_name<~String> - The name of the network name i.e. public, private or custom name
         #
         # ==== Returns
         # * response<~Excon::Response>:
         #   * body<~Hash>:
         #     * 'public'<~Array> - Public ip addresses
-        def list_server_public_addresses(server_id)
-          request(
-            :expects  => [200, 203],
+        def list_server_public_addresses(server_id, network_name)
+          response = request(
+            :expects  => 200,
             :method   => 'GET',
-            :path     => "servers/#{server_id}/ips/public.json"
+            :path     => "servers/#{server_id}/ips/#{network_name}.json"
           )
+          # return everything except the first address
+          data = response.body["#{network_name}"]
+          if data
+            data.delete_at(0)
+            public_address = data
+          end
+
+          response.body = { 'public' => public_address }
+          response
         end
 
       end
 
       class Mock
 
-        def list_server_public_addresses(server_id)
+        def list_server_public_addresses(server_id, network_name)
           response = Excon::Response.new
           if server = list_servers_detail.body['servers'].detect {|_| _['id'] == server_id}
-            response.status = [200, 203][rand(1)]
-            response.body = { 'public' => server['addresses']['public'] }
+            # return everything except the first address
+            data = server['addresses']["#{network_name}"]
+            if data
+              data.delete_at(0)
+              public_address = data
+            end
+
+            response.status = 200
+            response.body = { 'public' => public_address }
             response
           else
             raise Fog::Compute::HP::NotFound
