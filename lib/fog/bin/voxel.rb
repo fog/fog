c551e87d29a39734e73c9cd47034fe4681b0fe87
Kashif Rasul <kashif@nomad-labs.com>
2012-01-18 10:16:06 +0100

    One more typo fix.

--- lib/fog/bin/voxel.rb
+++ lib/fog/bin/voxel.rb
@@ -14,7 +14,7 @@ class Voxel < Fog::Bin
       @@connections ||= Hash.new do |hash, key|
         hash[key] = case key
         when :compute
-          Fog::Logger.warning("Voxel[:comupte] is not recommended, use Compute[:voxel]] for portability")
+          Fog::Logger.warning("Voxel[:compute] is not recommended, use Compute[:voxel] for portability")
           Fog::Compute.new(:provider => 'Voxel')
         else
           raise ArgumentError, "Unrecognized service: #{key.inspect}"
