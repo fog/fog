5f13ad4bcd18a923f6bf2d9f64cf1e6a7c86e8f4
Rupak Ganguly <rupakg@gmail.com>
2012-01-19 14:49:40 -0500

    Update inline docs as per real responses.

--- lib/fog/hp/requests/cdn/head_container.rb
+++ lib/fog/hp/requests/cdn/head_container.rb
@@ -11,12 +11,10 @@ module Fog
         # ==== Returns
         # * response<~Excon::Response>:
         #   * headers<~Hash>:
-        #     * 'X-CDN-Enabled'<~Boolean> - cdn status for container
-        #     * 'X-CDN-URI'<~String> - cdn url for this container
-        #     * 'X-TTL'<~String> - integer seconds before data expires, defaults to 86400 (1 day)
+        #     * 'X-Cdn-Enabled'<~Boolean> - cdn status for container
+        #     * 'X-Cdn-Uri'<~String> - cdn url for this container
+        #     * 'X-Ttl'<~String> - integer seconds before data expires, defaults to 86400 (1 day)
         #     * 'X-Log-Retention'<~Boolean> - ?
-        #     * 'X-User-Agent-ACL'<~String> - ?
-        #     * 'X-Referrer-ACL'<~String> - ?
         def head_container(container)
           response = request(
             :expects  => 204,
