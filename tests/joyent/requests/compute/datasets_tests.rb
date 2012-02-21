9ef1c52d807b05b9594ae024b9f5635c7f3681b8
Kevin Chan <kevin@yinkei.com>
2012-02-21 16:31:22 +0800

    Fixes dataset tests

--- tests/joyent/requests/compute/datasets_tests.rb
+++ tests/joyent/requests/compute/datasets_tests.rb
@@ -9,30 +9,34 @@ Shindo.tests("Fog::Compute[:joyent] | dataset requests", ["joyent"]) do
     "urn" => String,
     "default" => Fog::Boolean,
     "type" => String,
-    "created" => String
+    "created" => Time,
   }
 
   if Fog.mock?
     Fog::Compute[:joyent].data[:datasets] = {
-      "7456f2b0-67ac-11e0-b5ec-832e6cf079d5" => {
-        "name" => "nodejs",
-        "version" => "1.1.3",
+      "33904834-1f01-49d3-bed3-b642e158c375" => {
+        "id" => "33904834-1f01-49d3-bed3-b642e158c375",
+        "urn" => "sdc:sdc:zeus-simple-lb-200mbps:1.1.1",
+        "name" => "zeus-simple-lb-200mbps",
         "os" => "smartos",
-        "id" => "7456f2b0-67ac-11e0-b5ec-832e6cf079d5",
-        "urn" => "sdc:sdc:nodejs:1.1.3",
-        "default" => true,
         "type" => "smartmachine",
-        "created" => Time.parse("2011-04-15T22:04:12+00:00")
-      },
-      "febaa412-6417-11e0-bc56-535d219f2590" => {
-        "name" => "smartos",
-        "version" => "1.3.12",
-        "os" => "smartos",
-        "id" => "febaa412-6417-11e0-bc56-535d219f2590",
-        "urn" => "sdc:sdc:smartos:1.3.23",
+        "description" => "Zeus Simple Load Balancer 200 Mbps SmartMachine",
         "default" => false,
+        "requirements" => {},
+        "version" => "1.1.1",
+        "created" => Time.parse("2011-09-15T07:39:13+00:00")
+      },
+      "3fcf35d2-dd79-11e0-bdcd-b3c7ac8aeea6" => {
+        "id" => "3fcf35d2-dd79-11e0-bdcd-b3c7ac8aeea6",
+        "urn" => "sdc:sdc:mysql:1.4.1",
+        "name" => "mysql",
+        "os" => "smartos",
         "type" => "smartmachine",
-        "created" => Time.parse("2011-04-11T08:45:00+00:00")
+        "description" => "MySQL SmartMachine",
+        "default" => false,
+        "requirements" => {},
+        "version" => "1.4.1",
+        "created" => Time.parse("2011-09-15T05:01:34+00:00")
       }
     }
   end
