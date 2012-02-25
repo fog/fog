469a2152de9bcb991b629087001c0f7432efe189
Aaron Suggs <aaron@ktheory.com>
2012-02-25 11:44:46 -0500

    [aws|dns] add test for Record#modify

--- tests/aws/models/dns/record_tests.rb
+++ tests/aws/models/dns/record_tests.rb
@@ -6,7 +6,14 @@ Shindo.tests("Fog::Dns[:aws] | record", ['aws', 'dns']) do
   end
 
   params = { :name => @zone.domain, :type => 'A', :ttl => 3600, :value => ['1.2.3.4'] }
-  model_tests(@zone.records, params, false)
+
+  model_tests(@zone.records, params, false) do
+    tests("#modify") do
+      new_value = ['5.5.5.5']
+      returns(true) { @instance.modify(:value => new_value) }
+      returns(new_value) { @instance.value }
+    end
+  end
 
   tests("zones#destroy").succeeds do
     @zone.destroy
