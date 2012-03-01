cfda94295ce07c788fce90e6536e153a3d1feba4
TWer-cloud <leishi@thoughtworks.com>
2012-03-01 18:21:39 +0800

    [shilei] add group for list node parser

--- lib/fog/cloudstack/parsers/compute/list_virtual_machines.rb
+++ lib/fog/cloudstack/parsers/compute/list_virtual_machines.rb
@@ -9,7 +9,7 @@ module Fog
             @context = []
             @contexts = ['nic', 'securitygroup']
             @instance = {}
-            @response = { 'virtualMachines' => [] }
+            @response = {'virtualMachines' => []}
           end
 
           def start_element(name, attrs = [])
@@ -21,35 +21,37 @@ module Fog
 
           def end_element(name)
             case name
-            when 'templateid'
-              @instance['imageId'] = value
-            when 'serviceofferingid'
-              @instance['instanceType'] = value
-            when 'ipaddress'
-              @instance['ipAddress'] = value
-            when 'state'
-              @instance['instanceState'] = value
-            when 'name'
-              case @context.last
-              when 'nic'
-              when 'securitygroup'
-              else
-                @instance['name'] = value
-              end
-            when 'displayname'
-              @instance['displayName'] = value
-            when 'id'
-              case @context.last
-              when 'nic'
-              when 'securitygroup'
-              else
-                @instance['instanceId'] = value
-              end
-            when *@contexts
-              @context.pop
-            when 'virtualmachine'
-              @response['virtualMachines'] << @instance
-              @instance = {}
+              when 'templateid'
+                @instance['imageId'] = value
+              when 'serviceofferingid'
+                @instance['instanceType'] = value
+              when 'ipaddress'
+                @instance['ipAddress'] = value
+              when 'state'
+                @instance['instanceState'] = value
+              when 'name'
+                case @context.last
+                  when 'nic'
+                  when 'securitygroup'
+                  else
+                    @instance['name'] = value
+                end
+              when 'displayname'
+                @instance['displayName'] = value
+              when 'id'
+                case @context.last
+                  when 'nic'
+                  when 'securitygroup'
+                  else
+                    @instance['instanceId'] = value
+                end
+              when *@contexts
+                @context.pop
+              when 'virtualmachine'
+                @response['virtualMachines'] << @instance
+                @instance = {}
+              when 'group'
+                @instance['groupName'] = value
             end
           end
         end
