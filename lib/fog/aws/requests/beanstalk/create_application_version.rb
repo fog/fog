f4b4ebb73a4cce6e14e4aec5d261beb06deea7b3
George Scott <gscott@rumbleware.com>
2012-03-14 22:48:01 -0700

    Flatten SourceBundle hash, update docs.

--- lib/fog/aws/requests/beanstalk/create_application_version.rb
+++ lib/fog/aws/requests/beanstalk/create_application_version.rb
@@ -5,11 +5,16 @@ module Fog
 
         require 'fog/aws/parsers/beanstalk/create_application_version'
 
-        # Launches an environment for the specified application using the specified configuration.
+        # Creates an application version for the specified application.
         #
         # ==== Options
-        # * ApplicationName<~String>: If specified, AWS Elastic Beanstalk restricts the returned descriptions
-        #   to include only those that are associated with this application.
+        # * ApplicationName<~String>: The name of the application. If no application is found with this name,
+        #   and AutoCreateApplication is false, returns an InvalidParameterValue error.
+        # * AutoCreateApplication<~Boolean>: If true, create the application if it doesn't exist.
+        # * Description<~String>: Describes this version.
+        # * SourceBundle<~Hash>: The Amazon S3 bucket and key that identify the location of the source bundle
+        #     for this version.  Use keys 'S3Bucket' and 'S3Key' to describe location.
+        # * VersionLabel<~String>: A label identifying this version.
         #
         # ==== Returns
         # * response<~Excon::Response>:
@@ -18,6 +23,13 @@ module Fog
         # http://docs.amazonwebservices.com/elasticbeanstalk/latest/api/API_CreateApplicationVersion.html
         #
         def create_application_version(options={})
+          if source_bundle = options.delete('SourceBundle')
+            # flatten hash
+            options.merge!({
+                               'SourceBundle.S3Bucket' => source_bundle['S3Bucket'],
+                               'SourceBundle.S3Key' => source_bundle['S3Key']
+                           })
+          end
           request({
                       'Operation'    => 'CreateApplicationVersion',
                       :parser     => Fog::Parsers::AWS::ElasticBeanstalk::CreateApplicationVersion.new
