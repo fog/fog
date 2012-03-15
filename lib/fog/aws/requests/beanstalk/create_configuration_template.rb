92d2dd147deb922ef4095c129280f931fde15598
George Scott <gscott@rumbleware.com>
2012-03-15 16:04:51 -0700

    Added template collection/model and tests.

--- lib/fog/aws/requests/beanstalk/create_configuration_template.rb
+++ lib/fog/aws/requests/beanstalk/create_configuration_template.rb
@@ -9,9 +9,19 @@ module Fog
         # deploy different versions of the application with the same configuration settings.
         #
         # ==== Options
-        # * ApplicationName<~String>: If specified, AWS Elastic Beanstalk restricts the returned descriptions
-        #   to include only those that are associated with this application.
-        # * VersionLabel<~String>:
+        # * ApplicationName<~String>: The name of the application to associate with this configuration template.
+        #   If no application is found with this name, AWS Elastic Beanstalk returns an InvalidParameterValue error.
+        # * Description<~String>: Describes this configuration.
+        # * EnvironmentId<~String>: The ID of the environment used with this configuration template.
+        # * OptionSettings<~Hash>: If specified, AWS Elastic Beanstalk sets the specified configuration option
+        #     to the requested value. The new value overrides the value obtained from the solution stack or the
+        #     source configuration template.
+        # * SolutionStackName<~String>: The name of the solution stack used by this configuration. The solution
+        #     stack specifies the operating system, architecture, and application server for a configuration template.
+        #     It determines the set of configuration options as well as the possible and default values.
+        # * SourceConfiguration<~String>: If specified, AWS Elastic Beanstalk uses the configuration values from the
+        #     specified configuration template to create a new configuration.
+        # * TemplateName<~String>: The name of the configuration template.
         #
         # ==== Returns
         # * response<~Excon::Response>:
