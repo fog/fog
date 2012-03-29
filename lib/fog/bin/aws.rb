12f539c5773b93ba36efb20c029604c1b935ec68
George Scott <gscott@rumbleware.com>
2012-03-28 19:06:05 -0700

    [aws|beanstalk] Added missing :beanstalk case.

--- lib/fog/bin/aws.rb
+++ lib/fog/bin/aws.rb
@@ -54,6 +54,8 @@ class AWS < Fog::Bin
         hash[key] = case key
         when :auto_scaling
           Fog::AWS::AutoScaling.new
+        when :beanstalk
+          Fog::AWS::ElasticBeanstalk.new
         when :cdn
           Fog::Logger.warning("AWS[:cdn] is not recommended, use CDN[:aws] for portability")
           Fog::CDN.new(:provider => 'AWS')
