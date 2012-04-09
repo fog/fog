685ee5d61a64b082515543f4802fac3c3695dbf7
Amos Benari <abenari@redhat.com>
2012-04-09 17:05:06 +0300

    [ovirt] fixed list storage domain test

--- tests/ovirt/requests/compute/list_storage_domains_tests.rb
+++ tests/ovirt/requests/compute/list_storage_domains_tests.rb
@@ -7,7 +7,7 @@ Shindo.tests('Fog::Compute[:ovirt] | storage_domains request', ['ovirt']) do
     response = compute.storage_domains
     tests("The response data format ...") do
       test("it should be a kind of Array") { response.kind_of? Array }
-      test("be a kind of Hash") { response.first.kind_of? Hash }
+      test("be a kind of OVIRT::StorageDomain") { response.first.kind_of? OVIRT::StorageDomain }
     end
   end
 end
