c4ceb5ca08d4d79b8b71e9ccbb3a5eafacf66054
jxiao <jxiao@vmware.com>
2012-04-11 00:16:27 -0700

    Add fog interface to update vNic network

---
+++ lib/fog/vsphere/requests/compute/vm_update_network.rb
@@ -0,0 +1,82 @@
+module Fog
+  module Compute
+    class Vsphere
+      class Real
+
+        def vm_update_network(options = {})
+          raise ArgumentError, "Must pass parameter: vm_moid or instance_uuid" unless (options['vm_moid'] || options['instance_uuid'])
+          raise ArgumentError, "Cannot specify both vm_moid and instance_uuid" if (options['vm_moid'] && options['instance_uuid'])
+          raise ArgumentError, "Must pass parameter: network adapter name" unless options['adapter_name']
+          raise ArgumentError, "Must pass parameter: portgroup name" unless options['portgroup_name']
+
+          if options['vm_moid']
+            vm_mob_ref = get_vm_mob_ref_by_moid(options['vm_moid'])
+            raise Fog::Compute::Vsphere::NotFound, "VirtualMachine #{options['vm_moid']} not found." unless vm_mob_ref
+          end
+
+          if options['instance_uuid']
+            search_filter = { :uuid => options['instance_uuid'], 'vmSearch' => true, 'instanceUuid' => true }
+            vm_mob_ref = @connection.searchIndex.FindAllByUuid(search_filter).first
+            raise Fog::Compute::Vsphere::NotFound, "VirtualMachine #{options['instance_uuid']} not found." unless vm_mob_ref
+          end
+
+          devices = vm_mob_ref.config.hardware.device
+          nic = devices.select { |device| device.kind_of?(RbVmomi::VIM::VirtualEthernetCard) &&
+             device.deviceInfo.label == options['adapter_name'] }.first
+          raise Fog::Compute::Vsphere::NotFound, "network adapter:#{options['adapter_name']} not found." unless nic
+
+          dc_mob_ref = get_parent_dc_by_vm_mob(vm_mob_ref)
+          network_mob_ref = dc_mob_ref.network.select { |network| network.name == options['portgroup_name'] }.first
+          raise Fog::Compute::Vsphere::NotFound, "portgroup:#{options['portgroup']} not found." unless network_mob_ref
+
+          if network_mob_ref.class == RbVmomi::VIM::DistributedVirtualPortgroup
+            port = RbVmomi::VIM::DistributedVirtualSwitchPortConnection.new
+            port.switchUuid = network_mob_ref.config.distributedVirtualSwitch.uuid
+            port.portgroupKey = network_mob_ref.config.key
+
+            backing_info = RbVmomi::VIM::VirtualEthernetCardDistributedVirtualPortBackingInfo.new
+            backing_info.port = port
+          else
+            backing_info = RbVmomi::VIM::VirtualEthernetCardNetworkBackingInfo.new
+            backing_info.deviceName = options['portgroup_name']
+            backing_info.network = network_mob_ref
+          end
+
+          nic.backing = backing_info
+          network_config_spec = RbVmomi::VIM::VirtualDeviceConfigSpec.new
+          network_config_spec.device = nic
+          network_config_spec.operation = RbVmomi::VIM::VirtualDeviceConfigSpecOperation("edit")
+
+          config = RbVmomi::VIM::VirtualMachineConfigSpec.new
+          config.deviceChange = []
+          config.deviceChange << network_config_spec
+
+          task = vm_mob_ref.ReconfigVM_Task(:spec => config)
+          task.wait_for_completion
+
+          {
+              'vm_ref'        => vm_mob_ref,
+              'vm_attributes' => convert_vm_mob_ref_to_attr_hash(vm_mob_ref),
+              'task_state' => task.info.state
+          }
+
+        end
+
+      end
+
+      class Mock
+        def vm_update_network(options = {})
+          raise ArgumentError, "Must pass parameter: vm_moid or instance_uuid" unless (options['vm_moid'] || options['instance_uuid'])
+          raise ArgumentError, "Cannot specify both vm_moid and instance_uuid" if (options['vm_moid'] && options['instance_uuid'])
+          raise ArgumentError, "Must pass parameter: network adapter name" unless options['adapter_name']
+          raise ArgumentError, "Must pass parameter: portgroup name" unless options['portgroup_name']
+          {
+              'vm_ref'   => 'vm-123',
+              'task_ref' => 'task-1234',
+          }
+        end
+
+      end
+    end
+  end
+end
---
+++ tests/vsphere/requests/compute/vm_update_network_tests.rb
@@ -0,0 +1,51 @@
+Shindo.tests("Fog::Compute[:vsphere] | vm_update_network request", 'vsphere') do
+
+  require 'rubygems'
+  require 'rbvmomi'
+  require 'Fog'
+  require 'guid'
+
+  compute = Fog::Compute[:vsphere]
+
+  response = nil
+  response_linked = nil
+  vm_moid = nil
+  vm_uuid = nil
+
+  class ConstClass
+    DATACENTER = 'DatacenterCF'
+    TEMPLATE = "/Datacenters/#{DATACENTER}/vm/testvm" #path of a vm which need update network
+    MOB_TYPE = 'VirtualMachine' # type refereed to an effective VirtualMachine management object
+    ADAPTER_NAME = 'Network adapter 1' #vNic name
+    PORTGROUP_NAME = 'dvPortGroup' #portgroup name either dvportgroup or vss portgroup
+  end
+
+  tests("Get vm management object id by path | The return value should") do
+    response = compute.get_vm_mob_ref_by_path('path' => ConstClass::TEMPLATE)
+    test("be a kind of mob of VirtualMachine") { response.class.to_s == ConstClass::MOB_TYPE}
+  end
+
+  tests("Get vm management object by id | The return value should") do
+    response = compute.get_vm_mob_ref_by_path('path' => ConstClass::TEMPLATE)
+    vm_moid = response._ref.to_s
+    response = compute.get_vm_mob_ref_by_moid(vm_moid)
+    test("be a kind of mob of VirtualMachine") { response.class.to_s == ConstClass::MOB_TYPE}
+    test("equal to input moid or not") { response._ref.to_s == vm_moid}
+  end
+
+  tests("Config vNic network | The return value should") do
+    response = compute.get_vm_mob_ref_by_path('path' => ConstClass::TEMPLATE)
+    vm_uuid = response.config.instanceUuid
+    response = compute.vm_update_network(
+      'instance_uuid' => vm_uuid,
+      'adapter_name' => ConstClass::ADAPTER_NAME,
+      'portgroup_name' => ConstClass::PORTGROUP_NAME
+    )
+    test("be a kind of Hash") { response.kind_of? Hash }
+    %w{ vm_ref task_state}.each do |key|
+      test("have a #{key} key") { response.has_key? key }
+    end
+    test("reconfig network success or not")  { response.fetch('task_state').to_s == 'success'}
+  end
+
+end
