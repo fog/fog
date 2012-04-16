8a8b7e81b93323bd9a291a69d9f7ac0d9ce17af9
Amos Benari <abenari@redhat.com>
2012-04-16 12:01:00 +0300

    use constant for GB.

--- lib/fog/ovirt/requests/compute/add_volume.rb
+++ lib/fog/ovirt/requests/compute/add_volume.rb
@@ -2,10 +2,10 @@ module Fog
   module Compute
     class Ovirt
       class Real
-
+        DISK_SIZE_TO_GB = 1073741824
         def add_volume(id, options = {})
           raise ArgumentError, "instance id is a required parameter" unless id
-          options[:size]=options[:size_gb].to_i*1073741824 if options[:size_gb]
+          options[:size]=options[:size_gb].to_i*DISK_SIZE_TO_GB if options[:size_gb]
           client.add_volume(id, options)
         end
 
