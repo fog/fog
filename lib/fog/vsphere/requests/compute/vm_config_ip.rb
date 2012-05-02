c73c22a472e9f25d7498f6a69cfcb89d1a8d5774
Cindia-blue <lxinhui@vmware.com>
2012-05-02 14:24:58 +0800

    ip config

---
+++ lib/fog/vsphere/requests/compute/vm_config_ip.rb
@@ -0,0 +1,29 @@
+module Fog
+  module Compute
+    class Vsphere
+
+      module Shared
+        private
+
+
+      end
+
+      class Real
+        include Shared
+
+        def vm_config_ip(options = {})
+          raise ArgumentError, "config_json is a required parameter" unless options.has_key? 'config_json'
+          raise ArgumentError, "vm_moid is a required parameter" unless options.has_key? 'vm_moid'
+          vm_mob_ref = get_vm_mob_ref_by_moid(options['vm_moid'])
+          ovs = []
+          ovs[0] = RbVmomi::VIM.OptionValue(:key=>"machine.id", :value=> options['config_json'])
+          ip_config_spec = RbVmomi::VIM.VirtualMachineConfigSpec(:extraConfig => ovs)
+          task = vm_mob_ref.ReconfigVM_Task(:spec => ip_config_spec )
+          task.wait_for_completion
+          { 'task_state' => task.info.state }
+        end
+
+      end
+    end
+  end
+end
