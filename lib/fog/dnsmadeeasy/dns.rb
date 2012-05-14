c5ee16dedf8c4772bda093e644fab5320a18dee7
James Herdman <james.herdman@me.com>
2012-05-14 15:28:03 -0400

    Remove unused variable

--- lib/fog/dnsmadeeasy/dns.rb
+++ lib/fog/dnsmadeeasy/dns.rb
@@ -127,7 +127,7 @@ module Fog
 
         def signature(params)
           string_to_sign = params[:headers]['x-dnsme-requestDate']
-          signed_string = OpenSSL::HMAC.hexdigest('sha1', @dnsmadeeasy_secret_key, string_to_sign)
+          OpenSSL::HMAC.hexdigest('sha1', @dnsmadeeasy_secret_key, string_to_sign)
         end
       end
     end
