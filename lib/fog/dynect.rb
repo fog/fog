b0a7473f5b82c4fc6c582b7c8f08d217af6f68c2
James Herdman <james.herdman@me.com>
2012-05-14 14:10:36 -0400

    Remove unnecessary full path usage with require
    'fog/core' is already in the load path. No need to go to these extents.

--- lib/fog/dynect.rb
+++ lib/fog/dynect.rb
@@ -1,6 +1,6 @@
 require 'nokogiri'
 
-require(File.expand_path(File.join(File.dirname(__FILE__), 'core')))
+require 'fog/core'
 require 'fog/core/parser'
 
 module Fog
--- lib/fog/libvirt.rb
+++ lib/fog/libvirt.rb
@@ -1,4 +1,4 @@
-require(File.expand_path(File.join(File.dirname(__FILE__), 'core')))
+require 'fog/core'
 
 module Fog
   module Libvirt
