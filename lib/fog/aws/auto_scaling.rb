d7539ea27a0fd0baa8c05dc354ddce99318be94c
James Herdman <james.herdman@me.com>
2012-05-14 15:36:49 -0400

    Remove unused variable

--- lib/fog/aws/auto_scaling.rb
+++ lib/fog/aws/auto_scaling.rb
@@ -145,7 +145,6 @@ module Fog
 
         def self.data
           @data ||= Hash.new do |hash, region|
-            owner_id = Fog::AWS::Mock.owner_id
             hash[region] = Hash.new do |region_hash, key|
               region_hash[key] = {
                 :adjustment_types => [
