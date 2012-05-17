0f87c56e4d55d482dddac46393f1e5b0e174da33
Josh Lane <lane.joshlane@gmail.com>
2012-05-17 13:38:22 -0700

    remove FOG_PROVIDER env override

--- tests/compute/models/servers_tests.rb
+++ tests/compute/models/servers_tests.rb
@@ -1,6 +1,5 @@
 
 for provider, config in compute_providers
-  next if ENV['FOG_PROVIDER'] && provider.to_s != ENV['FOG_PROVIDER']
 
   Shindo.tests("Fog::Compute[:#{provider}] | servers", [provider.to_s]) do
 
