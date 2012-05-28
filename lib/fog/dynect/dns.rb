4bb008da1ffe46127cdc944518690208ea90fc0b
Dylan Egan <dylanegan@gmail.com>
2012-05-28 15:59:34 +0200

    Default to false for persistent connections. You can't pass in false. This now behaves like other connections in fog. :v:

--- lib/fog/dynect/dns.rb
+++ lib/fog/dynect/dns.rb
@@ -68,7 +68,7 @@ module Fog
           @host       = "api2.dynect.net"
           @port       = options[:port]        || 443
           @path       = options[:path]        || '/REST'
-          @persistent = options[:persistent]  || true
+          @persistent = options[:persistent]  || false
           @scheme     = options[:scheme]      || 'https'
           @version    = options[:version]     || '2.3.1'
           @connection = Fog::Connection.new("#{@scheme}://#{@host}:#{@port}", @persistent, @connection_options)
