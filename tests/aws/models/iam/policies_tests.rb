30a1a8f190170667e241506422a76bf4251200c6
Rodrigo Estebanez <restebanez@mdsol.com>
2012-06-05 15:08:23 +0200

    @users -> @user. Clean up the @user after the policies and access_keys test

--- tests/aws/models/iam/policies_tests.rb
+++ tests/aws/models/iam/policies_tests.rb
@@ -4,45 +4,47 @@ Shindo.tests("Fog::Compute[:iam] | policies", ['aws','iam']) do
   iam = Fog::AWS[:iam]
   
   @username = 'fake_user'
-  @users = iam.users.create(:id => @username)
+  @user = iam.users.create(:id => @username)
   @policy_document = {"Statement"=>[{"Action"=>["sqs:*"], "Effect"=>"Allow", "Resource"=>"*"}]}
   @policy_name = 'fake-sqs-policy'
   
   tests('#all', 'there is no policies').succeeds do
-    @users.policies.empty?
+    @user.policies.empty?
   end
   
   
   tests('#create','a policy').succeeds do
-    policy = @users.policies.create(id: @policy_name, document: @policy_document)
+    policy = @user.policies.create(id: @policy_name, document: @policy_document)
     policy.id == @policy_name
     policy.username == @username
     #policy.document == @policy_document # FIXME, the format isn't right
   end
   
-  @users.policies.create(id: 'another-policy', document: {})
+  @user.policies.create(id: 'another-policy', document: {})
   
   tests('#all','there are two policies').succeeds do
-    @users.policies.size == 2
+    @user.policies.size == 2
   end
   
   tests('#get') do
     tests('a valid policy').succeeds do
-      policy = @users.policies.get(@policy_name)
-      policy.id == @policy_name
+      policy = @user.policies.get(@policy_name)
+      policy.id == @polic_name
       policy.username == @username
       #policy.document == @policy_document # FIXME, the format isn't right    
     end
     
     tests('an invalid policy').succeeds do
-      @users.policies.get('non-existing') == nil
+      @user.policies.get('non-existing') == nil
     end
   end
   
   tests('#destroy').succeeds do
-    @users.policies.get(@policy_name).destroy
+    @user.policies.get(@policy_name).destroy
   end
   
+  # clean up
+  @user.destroy
 
   
 end
