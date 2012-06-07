368a0999a8ef95aafd74e58f40e16bc3a2b9e598
Eugene Howe <ehowe@engineyard.com>
2012-06-07 08:38:28 -0400

    removed ecloudv2 provider

--- lib/fog/ecloudv2/requests/compute/admin_disable_support_access.rb
+++
@@ -1,9 +0,0 @@
-module Fog
-  module Compute
-    class Ecloudv2
-      class Real
-        basic_request :admin_disable_support_access, 204, 'POST'
-      end
-    end
-  end
-end
--- lib/fog/ecloudv2/requests/compute/admin_enable_support_access.rb
+++
@@ -1,9 +0,0 @@
-module Fog
-  module Compute
-    class Ecloudv2
-      class Real
-        basic_request :admin_enable_support_access, 204, 'POST'
-      end
-    end
-  end
-end
