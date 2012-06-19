2c37b04860e7f3f1ff05bea21d05ac247f600403
Rupak Ganguly <rupakg@gmail.com>
2012-06-19 15:22:12 -0400

    Update call to include response_block rather than passing a block, to conform to excon deprecation message.
    Fix code when block is not passed.

--- lib/fog/hp/requests/storage/get_object.rb
+++ lib/fog/hp/requests/storage/get_object.rb
@@ -10,12 +10,21 @@ module Fog
         # * object<~String> - Name of object to look for
         #
         def get_object(container, object, &block)
-          response = request({
-            :block    => block,
-            :expects  => 200,
-            :method   => 'GET',
-            :path     => "#{Fog::HP.escape(container)}/#{Fog::HP.escape(object)}"
-          }, false, &block)
+          if block_given?
+            response = request(
+              :response_block  => block,
+              :expects  => 200,
+              :method   => 'GET',
+              :path     => "#{Fog::HP.escape(container)}/#{Fog::HP.escape(object)}"
+            )
+          else
+            response = request({
+              :block  => block,
+              :expects  => 200,
+              :method   => 'GET',
+              :path     => "#{Fog::HP.escape(container)}/#{Fog::HP.escape(object)}"
+            }, false, &block)
+          end
           response
         end
 
