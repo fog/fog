a36a6c92396304f76c5b45a25a63c99f721e29a4
Brad Gignac <brad.gignac@rackspace.com>
2012-06-21 17:47:52 -0400

    [rackspace|databases] Add write support for Rackspace Cloud Databases.

--- lib/fog/rackspace/models/databases/database.rb
+++ lib/fog/rackspace/models/databases/database.rb
@@ -5,6 +5,27 @@ module Fog
     class Databases
       class Database < Fog::Model
         identity :name
+
+        attribute :character_set
+        attribute :collate
+
+        def save
+          requires :identity, :instance
+          connection.create_database(instance.identity, identity, :character_set => character_set, :collate => collate)
+          true
+        end
+
+        def destroy
+          requires :identity, :instance
+          connection.delete_database(instance.identity, identity)
+          true
+        end
+
+        private
+
+        def instance
+          collection.instance
+        end
       end
     end
   end
---
+++ lib/fog/rackspace/requests/databases/create_instance.rb
@@ -0,0 +1,29 @@
+module Fog
+  module Rackspace
+    class Databases
+      class Real
+        def create_instance(name, flavor_id, volume_size, options = {})
+          data = {
+            'instance' => {
+              'name' => name,
+              'flavorRef' => flavor_id,
+              'volume' => {
+                'size' => volume_size
+              }
+            }
+          }
+
+          # Add databases
+          # Add users
+
+          request(
+            :body => Fog::JSON.encode(data),
+            :expects => 200,
+            :method => 'POST',
+            :path => 'instances'
+          )
+        end
+      end
+    end
+  end
+end
