aeb13fc0e710c99fec6f4f984713cf059b0483f6
Brian Hartsock <brian.hartsock@gmail.com>
2012-06-26 22:53:35 -0400

    [rackspace|lb] moved to Fog::JSON.encode instead of MultiJson.encode

--- lib/fog/rackspace/requests/load_balancers/set_ssl_termination.rb
+++ lib/fog/rackspace/requests/load_balancers/set_ssl_termination.rb
@@ -19,7 +19,7 @@ module Fog
             data['secureTrafficOnly'] - options[:secure_traffic_only]
           end
           request(
-            :body     => MultiJson.encode(data),
+            :body     => Fog::JSON.encode(data),
             :expects => [200, 202],
             :path => "loadbalancers/#{load_balancer_id}/ssltermination",
             :method => 'PUT'
