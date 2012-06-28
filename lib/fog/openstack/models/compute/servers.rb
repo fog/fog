17439a60f7442ab4afaa9e595ecab31841bfdfce
Ferran Rodenas <frodenas@gmail.com>
2012-06-28 02:35:06 +0200

    Change find_by methods

--- lib/fog/openstack/models/compute/servers.rb
+++ lib/fog/openstack/models/compute/servers.rb
@@ -29,12 +29,32 @@ module Fog
           nil
         end
 
-        def method_missing(method_sym, *arguments, &block)
-          if method_sym.to_s =~ /^find_by_(.*)$/
-            load(connection.list_servers_detail({$1 => arguments.first}).body['servers'])
-          else
-            super
-          end
+        def find_by_name(name)
+          find_by_attribute('name', name)
+        end
+
+        def find_by_status(status)
+          find_by_attribute('status', status)
+        end
+
+        def find_by_image(image)
+          find_by_attribute('image', image)
+        end
+
+        def find_by_flavor(flavor)
+          find_by_attribute('flavor', flavor)
+        end
+
+        def find_by_changes_since(isotime)
+          find_by_attribute('changes-since', isotime)
+        end
+
+        def find_by_reservation_id(reservation_id)
+          find_by_attribute('reservation_id', reservation_id)
+        end
+
+        def find_by_attribute(attribute, value)
+          load(connection.list_servers_detail({attribute => value}).body['servers'])
         end
 
       end
