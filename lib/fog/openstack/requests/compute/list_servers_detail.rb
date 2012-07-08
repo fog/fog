f142f91b7d407f31dc2ce347d9d49403966ad542
Ferran Rodenas <frodenas@gmail.com>
2012-07-08 23:28:10 +0200

    Add comment with available filters at list servers detail

--- lib/fog/openstack/requests/compute/list_servers_detail.rb
+++ lib/fog/openstack/requests/compute/list_servers_detail.rb
@@ -3,6 +3,7 @@ module Fog
     class OpenStack
       class Real
 
+        # Available filters: name, status, image, flavor, changes_since, reservation_id
         def list_servers_detail(filters = {})
           params = Hash.new
           filters[:all_tenants] ? params['all_tenants'] = 'True' : params = filters
