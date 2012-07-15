b5a96c90e99a145cc8a3806875ea3b0dd7098e65
Sean Handley <sean.handley@gmail.com>
2012-07-15 17:27:12 +0200

    Contrary to the documentation, this actually returns a 200 status rather than a 204.

--- lib/fog/serverlove/requests/compute/start_server.rb
+++ lib/fog/serverlove/requests/compute/start_server.rb
@@ -4,7 +4,7 @@ module Fog
       class Real
 
         def start_server(server_id)
-          request(:method => "post", :path => "/servers/#{server_id}/start", :expects => 204)
+          request(:method => "post", :path => "/servers/#{server_id}/start", :expects => 200)
         end
 
       end
