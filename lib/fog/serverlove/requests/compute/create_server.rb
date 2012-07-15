2f4ac6433ee80c6f6123a0b339b8a1a05f26fb4d
Sean Handley <sean.handley@gmail.com>
2012-07-15 12:24:36 +0200

    CRUDs + tests for servers.

---
+++ lib/fog/serverlove/requests/compute/create_server.rb
@@ -0,0 +1,33 @@
+module Fog
+  module Compute
+    class Serverlove
+      class Real
+
+        def create_server(options)
+          return nil if options.empty? || options.nil?
+          request(:method => "post", :path => "/servers/create", :expects => 200, :options => options)
+        end
+
+      end
+      
+      class Mock
+
+        def create_server(options = {})
+          response = Excon::Response.new
+          response.status = 200
+
+          data = {
+            'name'         => options['name'] || 'Test',
+            'cpu'          => options['cpu'] || 1000,
+            'persistent'   => options['persistent'] || false,
+            'vnc:password' => options['vnc:password'] || 'T35tServER!'
+          }
+
+          response.body = data
+          response
+        end
+
+      end
+    end
+  end
+end
--- tests/serverlove/requests/compute/server_tests.rb
+++ tests/serverlove/requests/compute/server_tests.rb
@@ -1,9 +1,37 @@
 Shindo.tests('Fog::Compute[:serverlove] | server requests', ['serverlove']) do
   
+  @server_format = {
+    'server'            => String,
+    'name'              => String,
+    'user'              => String,
+    'status'            => String,
+    'started'           => String,
+    'cpu'               => Integer,
+    'persistent'        => Fog::Nullable::String,
+    'vnc:password'      => String
+  }
+  
   tests('success') do
+    
+    attributes = { 'name' => 'Test', 'cpu' => '1000' }
+
+    tests("#create_server").formats(@server_format) do
+      @server = Fog::Compute[:serverlove].create_server(attributes).body
+    end
 
     tests("#list_servers").succeeds do
       Fog::Compute[:serverlove].servers
+    end
+    
+    tests("#update_server").returns(true) do
+      @server['name'] = "Diff"
+      Fog::Compute[:serverlove].update_server(@server['server'], { name: @server['name']})
+      Fog::Compute[:serverlove].servers.get(@server['server']).name == "Diff"
+    end
+
+
+    tests("#destroy_server").succeeds do
+      Fog::Compute[:serverlove].destroy_server(@server['server'])
     end
 
   end
