9ee9d04f6a6d2b36293fb45bf3ce2f41362712f1
Benton Roberts <broberts@mdsol.com>
2012-07-17 13:07:20 -0400

    [aws|elasticache] Remove erroneous whitespace trimming

--- lib/fog/aws/requests/elasticache/create_cache_cluster.rb
+++ lib/fog/aws/requests/elasticache/create_cache_cluster.rb
@@ -43,7 +43,7 @@ module Fog
             'AutoMinorVersionUpgrade'     => options[:auto_minor_version_upgrade],
             'CacheParameterGroupName'     => options[:parameter_group_name],
             'EngineVersion'               => options[:engine_version],
-            'NotificationTopicArn'        => (options[:notification_topic_arn]).strip,
+            'NotificationTopicArn'        => options[:notification_topic_arn],
             'Port'                        => options[:port],
             'PreferredAvailabilityZone'   => options[:preferred_availablility_zone],
             'PreferredMaintenanceWindow'  => options[:preferred_maintenance_window],
