69fd1561ac74482a726af77af7a3d7aa509faa83
Aaron Suggs <aaron@ktheory.com>
2012-08-03 18:13:01 -0400

    [aws|rds] Fix server tests
    (5.5 is the default mysql version)

--- tests/aws/models/rds/server_tests.rb
+++ tests/aws/models/rds/server_tests.rb
@@ -30,7 +30,7 @@ Shindo.tests("AWS::RDS | server", ['aws', 'rds']) do
       pending if Fog.mocking?
 
       orig_parameter_group = @instance.db_parameter_groups.first['DBParameterGroupName']
-      parameter_group = Fog::AWS[:rds].parameter_groups.create(:id => uniq_id, :family => 'mysql5.1', :description => 'fog-test')
+      parameter_group = Fog::AWS[:rds].parameter_groups.create(:id => uniq_id, :family => 'mysql5.5', :description => 'fog-test')
 
       orig_security_groups = @instance.db_security_groups.map{|h| h['DBSecurityGroupName']}
       security_group = Fog::AWS[:rds].security_groups.create(:id => uniq_id, :description => 'fog-test')
