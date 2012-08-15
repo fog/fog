9272ac88edfe6924dc6d3f72dd655080c7d8247b
Paul Thornthwaite <paul@brightbox.co.uk>
2012-08-15 16:17:28 +0100

    Add missing providers to the all_providers list for testing
    This allows their tags to be removed from the Shindo tests if unavailable

--- tests/helper.rb
+++ tests/helper.rb
@@ -12,7 +12,7 @@ def array_differences(array_a, array_b)
 end
 
 # check to see which credentials are available and add others to the skipped tags list
-all_providers = ['aws', 'bare_metal_cloud', 'bluebox', 'brightbox', 'cloudstack', 'dnsimple', 'dnsmadeeasy', 'dynect', 'ecloud', 'glesys', 'gogrid', 'google', 'hp', 'linode', 'local', 'ninefold', 'openstack', 'rackspace', 'stormondemand', 'voxel', 'xenserver', 'zerigo']
+all_providers = ['aws', 'bare_metal_cloud', 'bluebox', 'brightbox', 'clodo', 'cloudstack', 'dnsimple', 'dnsmadeeasy', 'dynect', 'ecloud', 'glesys', 'gogrid', 'google', 'hp', 'ibm', 'joyent', 'linode', 'local', 'ninefold', 'openstack', 'ovirt', 'rackspace', 'stormondemand', 'vcloud', 'voxel', 'vsphere', 'xenserver', 'zerigo']
 available_providers = Fog.available_providers.map {|provider| provider.downcase}
 for provider in (all_providers - available_providers)
   Formatador.display_line("[yellow]Skipping tests for [bold]#{provider}[/] [yellow]due to lacking credentials (add some to '~/.fog' to run them)[/]")
