ee244ff30224c9344537360f0ce67768ab747d14
Cindia-blue <lxinhui@vmware.com>
2012-09-10 10:55:03 +0000

    add folder_create into vm_folder

--- lib/fog/vsphere/requests/compute/vm_folder.rb
+++ lib/fog/vsphere/requests/compute/vm_folder.rb
@@ -18,6 +18,20 @@ module Fog
           folder_mob
         end
 
+        def folder_create(dc_mob, folder_path)
+          # this function will parse the give folder-path and traverse on the inventory tree of vc
+          # will create the folder if no find
+          raise ArgumentError, "folder_path is a required parameter" unless folder_path
+          path_elements = folder_path.split('/')
+          folder_mob = dc_mob.vmFolder
+          path_elements.each do |f|
+            Fog::Logger.deprecation("fog: folder name=#{f}")
+            folder = folder_mob.traverse(f, RbVmomi::VIM::Folder, true)
+            folder_mob = folder
+          end
+          folder_mob
+        end
+
         def folder_delete(dc_mob, folder_path)
           raise ArgumentError, "folder_path is a required parameter" unless folder_path
           rets = {'task_state' => 'false'}
