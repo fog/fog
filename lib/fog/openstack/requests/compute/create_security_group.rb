f8aeb7a17b59d6a2c66a7847959fcd006c598775
Tim Potter <tpot@hp.com>
2012-09-28 12:32:01 +1000

    Return hash not array for mock OpenStack create_security_group.

--- lib/fog/openstack/requests/compute/create_security_group.rb
+++ lib/fog/openstack/requests/compute/create_security_group.rb
@@ -31,13 +31,13 @@ module Fog
             "Content-Length" => "139",
             "Date" => Date.new}
           response.body = {
-            "security_group" => [{
+            "security_group" => {
               "rules" => [],
               "tenant_id" => "d5183375ab0343f3a0b4b05f547aefc2",
               "id" => 999,
               "name" => name,
               "description" => description
-            }]
+            }
           }
           response
         end
