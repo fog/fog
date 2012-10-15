9925afb151d36d40e1c4b9ffcfeaaf5cd00c66a8
lxinhui@vmware.com <lxinhui@vmware.com>
2012-10-15 15:08:19 +0800

    resubmit the logger definition

--- lib/fog/core/logger.rb
+++ lib/fog/core/logger.rb
@@ -2,9 +2,12 @@ module Fog
   class Logger
 
     @channels = {
-      :deprecation  => ::STDOUT,
-      :warning      => ::STDOUT
+        :deprecation  => ::STDOUT,
+        :debug      => ::STDOUT,
+        :warning      => ::STDOUT
     }
+
+    @open_or_not=true
 
     def self.[](channel)
       @channels[channel]
@@ -14,26 +17,34 @@ module Fog
       @channels[channel] = value
     end
 
+    def self.open()
+      @open_or_not=true
+    end
+
+    def self.close()
+      @open_or_not=false
+    end
+
     def self.debug(message)
-      self.write(:debug, "[light_black][DEBUG] #{message}[/]\n")
+      self.write(:debug, "[light_black][DEBUG] #{message}[/]\n") if @open_or_not
     end
 
     def self.deprecation(message)
-      self.write(:deprecation, "[yellow][DEPRECATION] #{message}[/]\n")
+      self.write(:deprecation, "[yellow][DEPRECATION] #{message}[/]\n") if @open_or_not
     end
 
     def self.warning(message)
-      self.write(:warning, "[yellow][WARNING] #{message}[/]\n")
+      self.write(:warning, "[yellow][WARNING] #{message}[/]\n") if @open_or_not
     end
 
     def self.write(key, value)
       if channel = @channels[key]
         value.gsub(Formatador::INDENT_REGEX, '')
         message = if channel.tty?
-          value.gsub(Formatador::PARSE_REGEX) { "\e[#{Formatador::STYLES[$1.to_sym]}m" }.gsub(Formatador::INDENT_REGEX, '')
-        else
-          value.gsub(Formatador::PARSE_REGEX, '').gsub(Formatador::INDENT_REGEX, '')
-        end
+                    value.gsub(Formatador::PARSE_REGEX) { "\e[#{Formatador::STYLES[$1.to_sym]}m" }.gsub(Formatador::INDENT_REGEX, '')
+                  else
+                    value.gsub(Formatador::PARSE_REGEX, '').gsub(Formatador::INDENT_REGEX, '')
+                  end
         channel.write(message)
       end
       nil
