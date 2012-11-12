97914976287bb22ebe7218d77add4d960bb7638d
geemus <geemus@gmail.com>
2012-11-12 10:50:57 -0600

    [aws|compute] fix one more offeringType mock format test failure

--- tests/aws/requests/compute/instance_tests.rb
+++ tests/aws/requests/compute/instance_tests.rb
@@ -116,6 +116,7 @@ Shindo.tests('Fog::Compute[:aws] | instance requests', ['aws']) do
       'fixedPrice'          => Float,
       'usagePrice'          => Float,
       'instanceCount'       => Integer,
+      'offeringType'        => String,
       'productDescription'  => String,
       'state'               => String,
       'tagSet'              => [{
