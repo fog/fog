d4feba66df202b15f98b0025cc13b6d227b4ceef
Rupak Ganguly <rupakg@gmail.com>
2012-11-14 15:09:48 -0500

    Update volume model and collection to handle bootable volumes as well.

--- lib/fog/hp/models/block_storage/volumes.rb
+++ lib/fog/hp/models/block_storage/volumes.rb
@@ -9,15 +9,22 @@ module Fog
 
         model Fog::BlockStorage::HP::Volume
 
-        def all
-          data = connection.list_volumes.body['volumes']
+        def all(options={})
+          if @bootable = options[:only_bootable]
+            data = connection.list_bootable_volumes.body['volumes']
+          else
+            data = connection.list_volumes.body['volumes']
+          end
           load(data)
         end
 
         def get(volume_id)
-          if volume = connection.get_volume_details(volume_id).body['volume']
-            new(volume)
+          if @bootable
+            volume = connection.get_bootable_volume_details(volume_id).body['volume']
+          else
+            volume = connection.get_volume_details(volume_id).body['volume']
           end
+          new(volume)
         rescue Fog::BlockStorage::HP::NotFound
           nil
         end
