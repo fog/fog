bc4143ad9f72c2815541c68e35682af1355be983
Kyle Rames <kyle.rames@rackspace.com>
2012-12-20 14:00:13 -0600

    updating Compute[:rackspace] to use v1 provider

--- tests/rackspace/requests/compute/server_tests.rb
+++ tests/rackspace/requests/compute/server_tests.rb
@@ -14,6 +14,8 @@ Shindo.tests('Fog::Compute[:rackspace] | server requests', ['rackspace']) do
     'progress'  => Integer,
     'status'    => String
   }
+  
+  @service = Fog::Compute.new(:provider => :rackspace, :version => :v1)  
 
   tests('success') do
 
@@ -21,49 +23,49 @@ Shindo.tests('Fog::Compute[:rackspace] | server requests', ['rackspace']) do
 
     tests('#create_server(1, 19)').formats(@server_format.merge('adminPass' => String)) do
       # 1 => 256MB, 19 => Gentoo
-      data = Fog::Compute[:rackspace].create_server(1, 19).body['server']
+      data =  @service.create_server(1, 19).body['server']
       @server_id = data['id']
       data
     end
 
-    Fog::Compute[:rackspace].servers.get(@server_id).wait_for { ready? }
+     @service.servers.get(@server_id).wait_for { ready? }
 
     tests("#get_server_details(#{@server_id})").formats(@server_format) do
-      Fog::Compute[:rackspace].get_server_details(@server_id).body['server']
+       @service.get_server_details(@server_id).body['server']
     end
 
     tests('#list_servers').formats({'servers' => [Rackspace::Compute::Formats::SUMMARY]}) do
-      Fog::Compute[:rackspace].list_servers.body
+       @service.list_servers.body
     end
 
     tests('#list_servers_detail').formats({'servers' => [@server_format]}) do
-      Fog::Compute[:rackspace].list_servers_detail.body
+       @service.list_servers_detail.body
     end
 
-    Fog::Compute[:rackspace].servers.get(@server_id).wait_for { ready? }
+     @service.servers.get(@server_id).wait_for { ready? }
 
     tests("#update_server(#{@server_id}, :name => 'fogupdatedserver', :adminPass => 'fogupdatedserver')").succeeds do
-      Fog::Compute[:rackspace].update_server(@server_id, :name => 'fogupdatedserver', :adminPass => 'fogupdatedserver')
+       @service.update_server(@server_id, :name => 'fogupdatedserver', :adminPass => 'fogupdatedserver')
     end
 
-    Fog::Compute[:rackspace].servers.get(@server_id).wait_for { ready? }
+     @service.servers.get(@server_id).wait_for { ready? }
 
     tests("#reboot_server(#{@server_id}, 'HARD')").succeeds do
       pending if Fog.mocking?
-      Fog::Compute[:rackspace].reboot_server(@server_id, 'HARD')
+       @service.reboot_server(@server_id, 'HARD')
     end
 
-    Fog::Compute[:rackspace].servers.get(@server_id).wait_for { ready? }
+     @service.servers.get(@server_id).wait_for { ready? }
 
     tests("#reboot_server(#{@server_id}, 'SOFT')").succeeds do
       pending if Fog.mocking?
-      Fog::Compute[:rackspace].reboot_server(@server_id, 'SOFT')
+       @service.reboot_server(@server_id, 'SOFT')
     end
 
-    Fog::Compute[:rackspace].servers.get(@server_id).wait_for { ready? }
+     @service.servers.get(@server_id).wait_for { ready? }
 
     tests("#delete_server(#{@server_id})").succeeds do
-      Fog::Compute[:rackspace].delete_server(@server_id)
+       @service.delete_server(@server_id)
     end
 
   end
@@ -71,20 +73,20 @@ Shindo.tests('Fog::Compute[:rackspace] | server requests', ['rackspace']) do
   tests('failure') do
 
     tests('#delete_server(0)').raises(Fog::Compute::Rackspace::NotFound) do
-      Fog::Compute[:rackspace].delete_server(0)
+       @service.delete_server(0)
     end
 
     tests('#get_server_details(0)').raises(Fog::Compute::Rackspace::NotFound) do
-      Fog::Compute[:rackspace].get_server_details(0)
+       @service.get_server_details(0)
     end
 
     tests("#update_server(0, :name => 'fogupdatedserver', :adminPass => 'fogupdatedserver')").raises(Fog::Compute::Rackspace::NotFound) do
-      Fog::Compute[:rackspace].update_server(0, :name => 'fogupdatedserver', :adminPass => 'fogupdatedserver')
+       @service.update_server(0, :name => 'fogupdatedserver', :adminPass => 'fogupdatedserver')
     end
 
     tests('#reboot_server(0)').raises(Fog::Compute::Rackspace::NotFound) do
       pending if Fog.mocking?
-      Fog::Compute[:rackspace].reboot_server(0)
+       @service.reboot_server(0)
     end
 
   end
