5c188509d0db952e88c45710276b7f33e072da2e
Danny Garcia <dannygarcia.me@gmail.com>
2012-12-27 21:23:05 -0500

    [docs] changed rdoc formatting to yard

--- lib/fog/aws/requests/storage/abort_multipart_upload.rb
+++ lib/fog/aws/requests/storage/abort_multipart_upload.rb
@@ -3,16 +3,14 @@ module Fog
     class AWS
       class Real
 
-        # Abort a multipart upload
-        #
-        # ==== Parameters
-        # * bucket_name<~String> - Name of bucket to abort multipart upload on
-        # * object_name<~String> - Name of object to abort multipart upload on
-        # * upload_id<~String> - Id of upload to add part to
-        #
-        # ==== See Also
-        # http://docs.amazonwebservices.com/AmazonS3/latest/API/mpUploadAbort.html
-        #
+        # Abort a multipart upload
+        #
+        # @param [String] bucket_name Name of bucket to abort multipart upload on
+        # @param [String] object_name Name of object to abort multipart upload on
+        # @param [String] upload_id Id of upload to add part to
+        #
+        # @see http://docs.amazonwebservices.com/AmazonS3/latest/API/mpUploadAbort.html
+        #..
         def abort_multipart_upload(bucket_name, object_name, upload_id)
           request({
             :expects    => 204,
