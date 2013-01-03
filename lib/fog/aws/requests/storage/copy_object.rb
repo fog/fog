1b5795eebc98ac5eff1192e658d43d58577e9c33
Danny Garcia <dannygarcia.me@gmail.com>
2013-01-03 00:14:37 -0500

    change copy_object docs to YARD

--- lib/fog/aws/requests/storage/copy_object.rb
+++ lib/fog/aws/requests/storage/copy_object.rb
@@ -5,29 +5,28 @@ module Fog
 
         require 'fog/aws/parsers/storage/copy_object'
 
+        #
         # Copy an object from one S3 bucket to another
         #
-        # ==== Parameters
-        # * source_bucket_name<~String> - Name of source bucket
-        # * source_object_name<~String> - Name of source object
-        # * target_bucket_name<~String> - Name of bucket to create copy in
-        # * target_object_name<~String> - Name for new copy of object
-        # * options<~Hash>:
-        #   * 'x-amz-metadata-directive'<~String> - Specifies whether to copy metadata from source or replace with data in request.  Must be in ['COPY', 'REPLACE']
-        #   * 'x-amz-copy_source-if-match'<~String> - Copies object if its etag matches this value
-        #   * 'x-amz-copy_source-if-modified_since'<~Time> - Copies object it it has been modified since this time
-        #   * 'x-amz-copy_source-if-none-match'<~String> - Copies object if its etag does not match this value
-        #   * 'x-amz-copy_source-if-unmodified-since'<~Time> - Copies object it it has not been modified since this time
-        #   * 'x-amz-storage-class'<~String> - Default is 'STANDARD', set to 'REDUCED_REDUNDANCY' for non-critical, reproducable data
+        # @param  [String] source_bucket_name Name of source bucket
+        # @param  [String] source_object_name Name of source object
+        # @param  [String] target_bucket_name Name of bucket to create copy in
+        # @param  [String] target_object_name Name for new copy of object
+        # @param  [Hash]   options
+        # @option [String] :x-amz-metadata-directive Specifies whether to copy metadata from source or replace with data in request.  Must be in ['COPY', 'REPLACE']
+        # @option [String] :x-amz-copy_source-if-match Copies object if its etag matches this value
+        # @option [Time]   :x-amz-copy_source-if-modified_since Copies object it it has been modified since this time
+        # @option [String] :x-amz-copy_source-if-none-match Copies object if its etag does not match this value
+        # @option [Time]   :x-amz-copy_source-if-unmodified-since Copies object it it has not been modified since this time
+        # @option [String] :x-amz-storage-class Default is 'STANDARD', set to 'REDUCED_REDUNDANCY' for non-critical, reproducable data
         #
-        # ==== Returns
-        # * response<~Excon::Response>:
-        #   * body<~Hash>:
-        #     * 'ETag'<~String> - etag of new object
-        #     * 'LastModified'<~Time> - date object was last modified
+        # @return Excon::Response
+        #   *body [Hash]
+        #     * :ETag [String] etag of new object
+        #     * :LastModified [Time] date object was last modified
         #
-        # ==== See Also
-        # http://docs.amazonwebservices.com/AmazonS3/latest/API/RESTObjectCOPY.html
+        # @see http://docs.amazonwebservices.com/AmazonS3/latest/API/RESTObjectCOPY.html
+        #
 
         def copy_object(source_bucket_name, source_object_name, target_bucket_name, target_object_name, options = {})
           headers = { 'x-amz-copy-source' => "/#{source_bucket_name}/#{CGI.escape(source_object_name)}" }.merge!(options)
