0485245d19a28f994757dda6fcbfbcd3dc13eaf9
Danny Garcia <dannygarcia.me@gmail.com>
2013-01-03 00:14:37 -0500

    change copy_object docs to YARD

--- lib/fog/aws/requests/storage/copy_object.rb
+++ lib/fog/aws/requests/storage/copy_object.rb
@@ -5,8 +5,10 @@ module Fog
 
         require 'fog/aws/parsers/storage/copy_object'
 
+        #
         # Copy an object from one S3 bucket to another
         #
+<<<<<<< HEAD
         # @param source_bucket_name [String] Name of source bucket
         # @param source_object_name [String] Name of source object
         # @param target_bucket_name [String] Name of bucket to create copy in
@@ -28,6 +30,8 @@ module Fog
         #
         # @see http://docs.amazonwebservices.com/AmazonS3/latest/API/RESTObjectCOPY.html
         # 
+=======
+>>>>>>> change copy_object docs to YARD
         def copy_object(source_bucket_name, source_object_name, target_bucket_name, target_object_name, options = {})
           headers = { 'x-amz-copy-source' => "/#{source_bucket_name}/#{CGI.escape(source_object_name)}" }.merge!(options)
           request({
