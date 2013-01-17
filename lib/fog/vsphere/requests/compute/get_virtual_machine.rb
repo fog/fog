b57c064308a13cedcdfa66e4d1781b596ff7de7d
Carlos Sanchez <csanchez@maestrodev.com>
2013-01-17 17:10:19 +0100

    [vsphere] vsphere fixes
    Correct use of Fog.mock? instead of ENV
    Use find_raw_datacenter to avoid the need of extra permissions
    Find datastores in subfolders
    nil pointer when finding vm

--- lib/fog/vsphere/requests/compute/get_virtual_machine.rb
+++ lib/fog/vsphere/requests/compute/get_virtual_machine.rb
@@ -1,7 +1,16 @@
 module Fog
   module Compute
     class Vsphere
+      module Shared
+        private
+        def is_uuid?(id)
+          !(id =~ /[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}/).nil?
+        end
+      end
+
       class Real
+        include Shared
+
         def get_virtual_machine(id, datacenter_name = nil)
           convert_vm_mob_ref_to_attr_hash(get_vm_ref(id, datacenter_name))
         end
@@ -9,23 +18,24 @@ module Fog
         protected
 
         def get_vm_ref(id, dc = nil)
-          vm = case id
-                 # UUID based
-                 when /[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}/
-                   @connection.searchIndex.FindByUuid :uuid => id, :vmSearch => true, :instanceUuid => true, :datacenter => dc
-                 else
-                   # try to find based on VM name
-                   if dc
-                     get_raw_datacenter(dc).find_vm(id)
-                   else
-                     raw_datacenters.map { |d| d.find_vm(id) }.compact.first
-                   end
-               end
+          raw_datacenter = find_raw_datacenter(dc) if dc
+          vm = case is_uuid?(id)
+          when true
+            # UUID based
+            params = {:uuid => id, :vmSearch => true, :instanceUuid => true}
+            params[:datacenter] = raw_datacenter if dc
+            @connection.searchIndex.FindByUuid(params)
+          else
+            # try to find based on VM name
+            dc ? raw_datacenter.find_vm(id) : raw_datacenters.map{ |d| d.find_vm(id) }.compact.first
+          end
           vm ? vm : raise(Fog::Compute::Vsphere::NotFound, "#{id} was not found")
         end
       end
 
       class Mock
+        include Shared
+
         def get_virtual_machine(id, datacenter_name = nil)
           case id
           when "5032c8a5-9c5e-ba7a-3804-832a03e16381", 'vm-715'
