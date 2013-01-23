4e41e00fed64a7ea6e18da2ef76709be6e07f6cf
Paul Thornthwaite <paul@brightbox.co.uk>
2013-01-23 15:08:24 +0000

    [Brightbox] Switches to new format helper
    Using #data_matches_schema which correctly fails when a nullable
    resource is absent.

--- tests/brightbox/requests/compute/firewall_policy_tests.rb
+++ tests/brightbox/requests/compute/firewall_policy_tests.rb
@@ -10,17 +10,17 @@ Shindo.tests('Fog::Compute[:brightbox] | firewall policy requests', ['brightbox'
     tests("#create_firewall_policy(#{create_options.inspect})") do
       result = Fog::Compute[:brightbox].create_firewall_policy(create_options)
       @firewall_policy_id = result["id"]
-      formats(Brightbox::Compute::Formats::Full::FIREWALL_POLICY, false) { result }
+      data_matches_schema(Brightbox::Compute::Formats::Full::FIREWALL_POLICY, {:allow_extra_keys => true}) { result }
     end
 
     tests("#list_firewall_policies()") do
-      formats(Brightbox::Compute::Formats::Collection::FIREWALL_POLICIES, false) do
+      data_matches_schema(Brightbox::Compute::Formats::Collection::FIREWALL_POLICIES, {:allow_extra_keys => true}) do
         Fog::Compute[:brightbox].list_firewall_policies
       end
     end
 
     tests("#get_firewall_policy('#{@firewall_policy_id}')") do
-      formats(Brightbox::Compute::Formats::Full::FIREWALL_POLICY, false) do
+      data_matches_schema(Brightbox::Compute::Formats::Full::FIREWALL_POLICY, {:allow_extra_keys => true}) do
         Fog::Compute[:brightbox].get_firewall_policy(@firewall_policy_id)
       end
     end
@@ -28,13 +28,13 @@ Shindo.tests('Fog::Compute[:brightbox] | firewall policy requests', ['brightbox'
     update_options = {:name => "Fog test policy B"}
     tests("#update_firewall_policy('#{@firewall_policy_id}', #{update_options.inspect})") do
       result = Fog::Compute[:brightbox].update_firewall_policy(@firewall_policy_id, update_options)
-      formats(Brightbox::Compute::Formats::Full::FIREWALL_POLICY, false) { result }
+      data_matches_schema(Brightbox::Compute::Formats::Full::FIREWALL_POLICY, {:allow_extra_keys => true}) { result }
       returns("Fog test policy B") { result["name"] }
     end
 
     tests("#destroy_firewall_policy('#{@firewall_policy_id}')") do
       result = Fog::Compute[:brightbox].destroy_firewall_policy(@firewall_policy_id)
-      formats(Brightbox::Compute::Formats::Full::FIREWALL_POLICY, false) { result }
+      data_matches_schema(Brightbox::Compute::Formats::Full::FIREWALL_POLICY, {:allow_extra_keys => true}) { result }
     end
   end
 end
