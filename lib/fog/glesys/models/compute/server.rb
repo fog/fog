96e2fbe31abca151de17efcd25edf36f22044eb7
Simon Gate <simon@smgt.me>
2013-01-27 14:56:28 +0100

    [Glesys] Wrap ssh method to use rootpassword if present

--- lib/fog/glesys/models/compute/server.rb
+++ lib/fog/glesys/models/compute/server.rb
@@ -99,6 +99,13 @@ module Fog
           Fog::SSH.new(public_ip_address, username, credentials).run(commands)
         end
 
+        def ssh(command, options={}, &block)
+          if options[:password].nil? && !rootpassword.nil?
+            options[:password] = rootpassword
+          end
+          super(command, options, &block)
+        end
+
         def public_ip_address(options = {})
 
           type = options[:type] || nil
