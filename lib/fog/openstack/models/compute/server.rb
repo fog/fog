fog/fog

commit 21499d8908e8af9df4e130a936f029ff98bf4604

[OpenStack|compute] default metadata to empy hash
Calling metadata.each in the `save` method will make a spurious request to
the nova endpoint, unless metadata has been initialized to an empty hash.

lib/fog/openstack/models/compute/server.rb
@@ -27,6 +27,7 @@ module Fog
 
         def initialize(attributes={})
           @connection = attributes[:connection]
+          attributes[:metadata] = {}
           super
         end
 

commit 60fefe7b80f495b368dfb7a8a4771a8f47b38f62

OpenStack Compute: Fix server model metadata
Updates the OpenStack compute server model so that it stores/handles
metadata correctly on Server create again.

This was a regression from 21499d8 (and should properly handle the
concern there too).

Includes an update to the test that fails without these fixes.

lib/fog/openstack/models/compute/server.rb
@@ -46,7 +46,6 @@ module Fog
         def initialize(attributes={})
           # Old 'connection' is renamed as service and should be used instead
           prepare_service_value(attributes)
-          attributes[:metadata] = {}
 
           self.security_groups = attributes.delete(:security_groups)
           self.min_count = attributes.delete(:min_count)
@@ -66,9 +65,10 @@ module Fog
         end
 
         def metadata=(new_metadata={})
+          return unless new_metadata
           metas = []
           new_metadata.each_pair {|k,v| metas << {"key" => k, "value" => v} }
-          metadata.load(metas)
+          @metadata = metadata.load(metas)
         end
 
         def user_data=(ascii_userdata)
@@ -229,10 +229,7 @@ module Fog
         def save
           raise Fog::Errors::Error.new('Resaving an existing object may create a duplicate') if persisted?
           requires :flavor_ref, :image_ref, :name
-          meta_hash = {}
-          metadata.each { |meta| meta_hash.store(meta.key, meta.value) }
           options = {
-            'metadata'    => meta_hash,
             'personality' => personality,
             'accessIPv4' => accessIPv4,
             'accessIPv6' => accessIPv6,
@@ -244,6 +241,7 @@ module Fog
             'max_count'   => @max_count,
             'os:scheduler_hints' => @os_scheduler_hints
           }
+          options['metadata'] = metadata.to_hash unless @metadata.nil?
           options = options.reject {|key, value| value.nil?}
           data = service.create_server(name, image_ref, flavor_ref, options)
           merge_attributes(data.body['server'])

tests/openstack/models/compute/server_tests.rb
@@ -12,6 +12,7 @@ Shindo.tests("Fog::Compute[:openstack] | server", ['openstack']) do
         image  = fog.images.first.id
 
         server = fog.servers.new(:name       => 'test server',
+                                 :metadata => {"foo" => "bar"},
                                  :flavor_ref => flavor,
                                  :image_ref  => image)
 
@@ -20,8 +21,9 @@ Shindo.tests("Fog::Compute[:openstack] | server", ['openstack']) do
         server.save
 
         found_groups = server.security_groups
-
         returns(1) { found_groups.length }
+
+        returns(1) { server.metadata.length }
 
         group = found_groups.first
         returns('my_group') { group.name }