9a09b76159373a9b0d9ff76537f90e7a7f9bd724
Sergio Rubio <rubiojr@frameos.org>
2013-02-26 21:11:45 +0100

    [digitalocean|compute] string fixes

--- tests/digitalocean/requests/compute/destroy_server_tests.rb
+++ tests/digitalocean/requests/compute/destroy_server_tests.rb
@@ -1,14 +1,12 @@
-Shindo.tests('Fog::Compute[:digitalocean] | reboot_server request', ['digitalocean', 'compute']) do
+Shindo.tests('Fog::Compute[:digitalocean] | destroy_server request', ['digitalocean', 'compute']) do
 
   service = Fog::Compute[:digitalocean]
-  data = fog_test_server
+  server = fog_test_server
 
   tests('success') do
 
-    test('#reboot_server') do
-      sleep 120
-      data.body['status'] == 'OK' and \
-        (service.destroy_server(data.body['droplet']['id']).body['status'] == 'OK')
+    test('#destroy_server') do
+      service.destroy_server(server.id).body['status'] == 'OK'
     end
 
   end
