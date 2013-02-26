12923ffc4d09272ff863cb576a11d9cf9bd807b1
Sergio Rubio <rubiojr@frameos.org>
2013-02-26 14:51:28 +0100

    [openstack|network] create_network provider extensions
    Implements provider extensions when creating networks.
    
    See:
    
    http://docs.openstack.org/trunk/openstack-network/admin/content/provider_attributes.html
    
    Complements #1581 (Added missing Network model attributes)

--- lib/fog/openstack/requests/network/create_network.rb
+++ lib/fog/openstack/requests/network/create_network.rb
@@ -6,9 +6,31 @@ module Fog
         def create_network(options = {})
           data = { 'network' => {} }
 
-          vanilla_options = [:name, :shared, :admin_state_up, :tenant_id]
+          vanilla_options = [
+            :name, 
+            :shared, 
+            :admin_state_up, 
+            :tenant_id, 
+            :router_external, 
+            :provider_network_type,
+            :provider_segmentation_id, 
+            :provider_physical_network
+          ]
+
+          # Advanced Features through API Extensions
+          #
+          # @see http://docs.openstack.org/trunk/openstack-network/admin/content/provider_attributes.html
+          aliases = {
+            :provider_network_type     => 'provider:network_type',
+            # Not applicable to the "local" or "gre" network types
+            :provider_physical_network => 'provider:physical_network',
+            :provider_segmentation_id  => 'provider:segmentation_id',
+            :router_external           => 'router:external'
+          }
+
           vanilla_options.reject{ |o| options[o].nil? }.each do |key|
-            data['network'][key] = options[key]
+            aliased_key = aliases[key] || key
+            data['network'][aliased_key] = options[key]
           end
 
           request(
