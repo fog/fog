9f9629412582dfef215fcf46df9f67cb090128bb
Pete Fritchman <petef@databits.net>
2013-02-27 14:58:27 -0500

    pass the optional availability_zone attribute when creating a new Compute::AWS::Subnet

--- tests/aws/models/compute/subnet_tests.rb
+++ tests/aws/models/compute/subnet_tests.rb
@@ -1,5 +1,10 @@
 Shindo.tests("Fog::Compute[:aws] | subnet", ['aws']) do
-  @vpc=Fog::Compute[:aws].vpcs.create('cidr_block' => '10.0.10.0/24')
-  model_tests(Fog::Compute[:aws].subnets, {:vpc_id => @vpc.id, :cidr_block => '10.0.10.0/28'}, true)
+  @vpc = Fog::Compute[:aws].vpcs.create('cidr_block' => '10.0.10.0/24')
+  @availability_zones = Fog::Compute[:aws].describe_availability_zones('state' => 'available').body['availabilityZoneInfo'].collect{ |az| az['zoneName'] }
+  model_tests(Fog::Compute[:aws].subnets, {:vpc_id => @vpc.id, :cidr_block => '10.0.10.0/28', :availability_zone => @availability_zones.last}, true) do
+    test("availability_zone") do
+      @instance.availability_zone == @availability_zones.last
+    end
+  end
   @vpc.destroy
 end
