385ba4506eebeec80dc77098287f2d7a823a9ccb
Pete Fritchman <petef@databits.net>
2013-02-28 11:31:39 -0500

    better test for VPC subnet creation with an AZ specified

--- tests/aws/models/compute/subnet_tests.rb
+++ tests/aws/models/compute/subnet_tests.rb
@@ -1,9 +1,13 @@
 Shindo.tests("Fog::Compute[:aws] | subnet", ['aws']) do
   @vpc = Fog::Compute[:aws].vpcs.create('cidr_block' => '10.0.10.0/24')
   @availability_zones = Fog::Compute[:aws].describe_availability_zones('state' => 'available').body['availabilityZoneInfo'].collect{ |az| az['zoneName'] }
-  model_tests(Fog::Compute[:aws].subnets, {:vpc_id => @vpc.id, :cidr_block => '10.0.10.0/28', :availability_zone => @availability_zones.last}, true) do
-    test("availability_zone") do
-      @instance.availability_zone == @availability_zones.last
+
+  # Try making a subnet in each AZ.
+  @availability_zones.each do |az|
+    model_tests(Fog::Compute[:aws].subnets, {:vpc_id => @vpc.id, :cidr_block => '10.0.10.0/28', :availability_zone => az}, true) do
+      test("availability_zone") do
+        @instance.availability_zone.equal? az
+      end
     end
   end
   @vpc.destroy
