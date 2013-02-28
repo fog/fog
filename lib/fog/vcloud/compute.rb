5a3332236153390694ab30950330b5784a1f4d41
Chirag Jog <chirag.jog@gmail.com>
2013-02-28 21:40:33 +0530

    Fix the base encoding issue with Ruby 1.8.7 Compliance

--- lib/fog/vcloud/compute.rb
+++ lib/fog/vcloud/compute.rb
@@ -301,7 +301,7 @@ module Fog
 
         # Use this to set the Authorization header for login
         def authorization_header
-          "Basic #{Base64.strict_encode64("#{@username}:#{@password}")}"
+          "Basic #{Base64.encode64("#{@username}:#{@password}").delete("\n")}"
         end
 
         # Actually do the request
