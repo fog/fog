4be353001603079e35464ed084dd71bf72785ec2
Mike Hagedorn <mike@silverchairsolutions.com>
2013-03-01 17:13:52 -0600

    cleaned up dns stuff

--- tests/hp/requests/dns/list_domains_tests.rb
+++
@@ -1,9 +0,0 @@
-Shindo.tests("HP::DNS | list domains tests", ['hp', 'dns', 'domain']) do
-  tests('success') do
-
-  end
-  tests('failure') do
-
-  end
-
-end
--- tests/hp/requests/dns/list_records_tests.rb
+++
@@ -1,9 +0,0 @@
-Shindo.tests("HP::DNS | list records tests", ['hp', 'dns', 'records']) do
-  tests('success') do
-
-  end
-  tests('failure') do
-
-  end
-
-end
