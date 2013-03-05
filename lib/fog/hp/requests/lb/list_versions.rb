029e4c6f44729f7a9b9a76663217534ffb67f1e2
Mike Hagedorn <mike@silverchairsolutions.com>
2013-03-05 09:29:38 -0600

    versioning logic

--- lib/fog/hp/requests/lb/list_versions.rb
+++ lib/fog/hp/requests/lb/list_versions.rb
@@ -4,12 +4,24 @@ module Fog
       class LB
         class Real
           def list_versions
-
+            request(
+              :expects => [200, 204],
+              :method  => 'GET',
+              :path    => "",
+              :version => true
+            )
           end
         end
         class Mock
           def list_versions
-
+            response      = Excon::Response.new
+            response.status = 200
+            response.body = {
+              "versions" => [
+                { "id" => "v1.1","links" => [{ "href" => "http://api-docs.hpcloud.com", "rel" => "self" }], "status" => "CURRENT", "updated" => "2012-12-18T18:30:02.25Z" }
+              ]
+            }
+            response
           end
         end
       end
