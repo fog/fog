a4986dc8c427d7005e5fae02fe7fa2dc47a28d8a
Mike Hagedorn <mike@silverchairsolutions.com>
2013-03-06 17:35:05 -0600

    delete node/lb tests

--- lib/fog/hp/requests/lb/delete_load_balancer_node.rb
+++ lib/fog/hp/requests/lb/delete_load_balancer_node.rb
@@ -17,9 +17,21 @@ module Fog
 
         def delete_load_balancer_node(instance_id, node_id)
           response = Excon::Response.new
-
-
+          response = Excon::Response.new
+          if load_b = get_load_balancer(instance_id).body
+            if node = find_node(load_b, node_id)
+              response.status = 202
+            else
+              raise Fog::HP::LB::NotFound
+            end
+          else
+            raise Fog::HP::LB::NotFound
+          end
           response
+        end
+
+        def find_node(lb, node_id)
+          lb['nodes'].detect { |_| _['id'] == node_id }
         end
       end
     end
