8b6562604737c1f860309b8b0751440ddfd89a50
Andrew Kuklewicz <andrew@beginsinwonder.com>
2013-03-06 14:18:40 -0600

    fix 1.8.7 error, oops

--- lib/fog/internet_archive/models/storage/file.rb
+++ lib/fog/internet_archive/models/storage/file.rb
@@ -36,15 +36,15 @@ module Fog
         def set_metadata_array_headers(array_attribute, options={})
           attr_values = Array(self.send(array_attribute))
           opt_values = options.collect do |key,value| 
-            options.delete(key) if (key.to_s =~ /^x-(amz||archive)-meta(\d*)-#{array_attribute[0..-2]}/)
+            options.delete(key) if (key.to_s =~ /^x-(amz||archive)-meta(\d*)-#{array_attribute.to_s[0..-2]}/)
           end
           values = (attr_values + opt_values).compact.sort.uniq
           # got the values, now add them back to the options
           if values.size == 1
-            options["x-archive-meta-#{array_attribute[0..-2]}"] = values.first
+            options["x-archive-meta-#{array_attribute.to_s[0..-2]}"] = values.first
           elsif values.size > 1
             values[0,99].each_with_index do |value, i|
-              options["x-archive-meta#{format("%02d", i+1)}-#{array_attribute[0..-2]}"] = value
+              options["x-archive-meta#{format("%02d", i+1)}-#{array_attribute.to_s[0..-2]}"] = value
             end
           end
 
