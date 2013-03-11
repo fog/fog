8ad0bdb4b131d2c6b72fca52581771600fdc6ec7
Rupak Ganguly <rupakg@gmail.com>
2013-03-11 16:45:18 -0400

    [hp|compute] Add user_data option to create server call.

--- lib/fog/hp/requests/compute/create_server.rb
+++ lib/fog/hp/requests/compute/create_server.rb
@@ -101,6 +101,10 @@ module Fog
             data['server']['config_drive'] = options['config_drive']
           end
 
+          if options['user_data']
+            data['server']['user_data'] = options['user_data']
+          end
+
           request(
             :body     => Fog::JSON.encode(data),
             :expects  => 202,
