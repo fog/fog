a17ccf2dc080f7e62d127818881a6eca469f56a4
Nat Welch <nat@natwelch.com>
2013-03-12 14:35:19 -0700

    fix deprecation notice.

--- lib/fog/google/models/compute/servers.rb
+++ lib/fog/google/models/compute/servers.rb
@@ -10,7 +10,7 @@ module Fog
         model Fog::Compute::Google::Server
 
         def all
-          data = connection.list_servers.body["items"]
+          data = service.list_servers.body["items"]
           load(data)
         end
 
