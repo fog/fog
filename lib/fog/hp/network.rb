31ef1088dd4de70a3a50093bb8851aa53d2ae491
Rupak Ganguly <rupakg@gmail.com>
2013-03-21 16:37:27 -0400

    [hp|network] Add request method for delete_network.

--- lib/fog/hp/network.rb
+++ lib/fog/hp/network.rb
@@ -14,7 +14,7 @@ module Fog
 
       request_path 'fog/hp/requests/network'
       request :create_network
-      #request :delete_network
+      request :delete_network
       request :get_network
       request :list_networks
       #request :update_network
