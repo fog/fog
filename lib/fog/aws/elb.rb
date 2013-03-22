5e3ee29e58736db5ae6ad3990689653f31440c3d
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2013-03-22 11:45:05 +0200

    fix for AWS error message parsing

--- lib/fog/aws/elb.rb
+++ lib/fog/aws/elb.rb
@@ -191,7 +191,7 @@ module Fog
             :parser     => parser
           })
         rescue Excon::Errors::HTTPStatusError => error
-          if match = error.message.match(/<Code>(.*)<\/Code>(?:.*<Message>(.*)<\/Message>)?/m)
+          if match = error.message.match(/(?:.*<Code>(.*)<\/Code>)(?:.*<Message>(.*)<\/Message>)/m)
             case match[1]
             when 'CertificateNotFound'
               raise Fog::AWS::IAM::NotFound.slurp(error, match[2])
