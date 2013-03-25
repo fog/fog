7cb11d85bfa46285c23ab0dc707d2c07c99831a7
Sergio Rubio <rubiojr@frameos.org>
2013-03-25 22:28:06 +0100

    [digitalocean|compute] added create_ssh_key request tests

---
+++ tests/digitalocean/requests/compute/create_ssh_key_tests.rb
@@ -0,0 +1,22 @@
+Shindo.tests('Fog::Compute[:digitalocean] | create_ssh_key request', ['digitalocean', 'compute']) do
+
+  @key_format = {
+    'id'             => Integer,
+    'name'           => String,
+    'ssh_pub_key'    => String
+  }
+
+  service = Fog::Compute[:digitalocean]
+
+  tests('success') do
+
+    tests('#create_ssh_key').formats({'status' => 'OK', 'ssh_key' => @key_format}) do
+      @key = Fog::Compute[:digitalocean].create_ssh_key 'fookey', 'fookey'
+      @key.body
+    end
+    
+  end
+
+  service.destroy_ssh_key @key.body['ssh_key']['id']
+
+end
