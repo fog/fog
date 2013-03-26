963817d43914488a93bb99450f40b398d6f47fbc
Sergio Rubio <rubiojr@frameos.org>
2013-03-26 19:49:10 +0100

    [openstack|network] mock fixes in some OpenStack Network requests
    Updated mocks in some network request fixing some issues and keeping
    the mocking style used in other requests.

--- lib/fog/openstack/requests/network/get_router.rb
+++ lib/fog/openstack/requests/network/get_router.rb
@@ -15,19 +15,10 @@ module Fog
       class Mock
         def get_router(router_id)
           response = Excon::Response.new
-          if data = (self.data['routers'].find { |r| r['id'] == router_id })
+          if data = (self.data[:routers].find { |id,value| id == router_id })
             response.status = 200
             response.body = {
-              'router' => {
-                'status' => 'ACTIVE',
-                'external_gateway_info' => {
-                  'network_id' => '3c5bcddd-6af9-4e6b-9c3e-c153e521cab8'
-                },
-                'name' => 'router1',
-                'admin_state_up' => true,
-                'tenant_id' => '33a40233088643acb66ff6eb0ebea679',
-                'id' => 'a9254bdb-2613-4a13-ac4c-adc581fba50d'
-              }
+              'router' => data[1],
             }
             response
           else
