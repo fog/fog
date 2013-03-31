053f6d9b22b445fdbed7890f0884993526a16fd5
Sergio Rubio <rubiojr@frameos.org>
2013-03-31 21:33:36 +0200

    [digitalocean|compute] list_ssh_keys request mock fixes
    Removing ssh_pub_key was making tests fail when mocking randomly.

--- lib/fog/digitalocean/requests/compute/list_ssh_keys.rb
+++ lib/fog/digitalocean/requests/compute/list_ssh_keys.rb
@@ -20,9 +20,7 @@ module Fog
           response.status = 200
           response.body = {
             "status" => "OK",
-            # key listing does not return ssh_pub_key
-            # https://www.digitalocean.com/api#ssh_keys
-            "ssh_keys"  => self.data[:ssh_keys].map { |k| k.delete('ssh_pub_key'); k }
+            "ssh_keys"  => self.data[:ssh_keys]
           }
           response
         end
