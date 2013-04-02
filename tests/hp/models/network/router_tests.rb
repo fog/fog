e208b14c385d9a31925245fa96a757017a477069
Rupak Ganguly <rupakg@gmail.com>
2013-04-02 16:14:36 -0400

    [hp|network] Add router interface methods and tests to router models.

--- tests/hp/models/network/router_tests.rb
+++ tests/hp/models/network/router_tests.rb
@@ -1,5 +1,10 @@
 Shindo.tests('HP::Network | networking router model', ['hp', 'networking', 'router']) do
 
+  # needed to test router_interface calls
+  @network = HP[:network].networks.create(:name => 'fognetwork')
+  @subnet = HP[:network].subnets.create({:name => 'fogsubnet', :network_id => @network.id, :cidr => '13.13.13.13/13', :ip_version => 4})
+  @port = HP[:network].ports.create({:name => 'fogport', :network_id => @network.id})
+
   attributes = {:name => 'fogrouter', :admin_state_up => true}
   model_tests(HP[:network].routers, attributes, true)
 
@@ -17,10 +22,34 @@ Shindo.tests('HP::Network | networking router model', ['hp', 'networking', 'rout
       @router.save
     end
 
+    tests("#add_interface(#{@subnet.id}, nil) - with subnet").succeeds do
+      @router.add_interface(@subnet.id)
+    end
+    tests("#remove_interface(#{@subnet.id}, nil) - with subnet").succeeds do
+      @router.remove_interface(@subnet.id)
+    end
+    tests("#add_interface(nil, #{@port.id}) - with port").succeeds do
+      @router.add_interface(nil, @port.id)
+    end
+    ## deletes the port as well
+    tests("#remove_interface(nil, #{@port.id}) - with port").succeeds do
+      @router.remove_interface(nil, @port.id)
+    end
+
+    tests("#add_interface(#{@subnet.id}, #{@port.id}) - with both").succeeds do
+      @router.add_interface(@subnet.id, @port.id) == false
+    end
+    tests("#add_interface(#{@subnet.id}, #{@port.id}) - with both").succeeds do
+      @router.remove_interface(@subnet.id, @port.id) == false
+    end
+
     tests('#destroy').succeeds do
       @router.destroy
     end
 
   end
+
+  @subnet.destroy
+  @network.destroy
 
 end
