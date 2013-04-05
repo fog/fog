20d42c40fc54f768287422838c55c29f0121cbcc
Jeff McCune <jeff@puppetlabs.com>
2013-04-04 23:05:03 -0700

    (maint) Clean up whitespace errors

--- lib/fog/vsphere/requests/compute/list_templates.rb
+++ lib/fog/vsphere/requests/compute/list_templates.rb
@@ -3,7 +3,6 @@ module Fog
     class Vsphere
       class Real
         def list_templates(options = { })
-          
           options[:folder] ||= options['folder']
           if options[:folder] then
             list_all_templates_in_folder(options[:folder], options[:datacenter])
@@ -11,19 +10,19 @@ module Fog
             list_all_templates(options)
           end
         end
-        
+
         private
-        
+
         def list_all_templates_in_folder(path, datacenter_name)
           folder = get_raw_vmfolder(path, datacenter_name)
 
           vms = folder.children.grep(RbVmomi::VIM::VirtualMachine)
           # remove all virtual machines that are not template
           vms.delete_if { |v| not v.config.template }
-          
+
           vms.map(&method(:convert_vm_mob_ref_to_attr_hash))
         end
-        
+
         def list_all_templates(options = {})
           datacenters = find_datacenters(options[:datacenter])
 
