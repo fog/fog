2fb126cf86a0d237975588859bb971132b8643da
Dmitry Dedov <dmitry.dedov@tut.by>
2013-04-09 16:58:52 +0300

    [cloudstack] add create_template test

--- tests/cloudstack/requests/template_tests.rb
+++ tests/cloudstack/requests/template_tests.rb
@@ -28,7 +28,7 @@ Shindo.tests('Fog::Compute[:cloudstack] | template requests', ['cloudstack']) do
         'isextractable' => Fog::Boolean,
         'checksum' => Fog::Nullable::String,
         'sourcetemplateid' => Fog::Nullable::Integer,
-        'accountid' => Fog::Nullable::Integer,        
+        'accountid' => Fog::Nullable::Integer,
         'bootable' => Fog::Nullable::Boolean,
         'hostid' => Fog::Nullable::Integer,
         'hostname' => Fog::Nullable::String,
@@ -41,11 +41,40 @@ Shindo.tests('Fog::Compute[:cloudstack] | template requests', ['cloudstack']) do
     }
   }
 
+  # TODO: check this
+  #
+  # in documentation http://cloudstack.apache.org/docs/api/apidocs-4.0.0/user/createTemplate.html
+  # we have wrong response description
+  #
+  # real response for cloudstack api 4.0
+  # {"createtemplateresponse"=>
+  #   {"id"=>"15a98aee-a9af-4263-849c-3650c5b5f7ff", "jobid"=>"6db40d1a-5737-4955-afbc-9954ff49547a"}
+  # }
+  #
+  @create_template_format = {
+    'createtemplateresponse'=> {
+        'id' => String,
+        'jobid' => String
+    }
+  }
+
   tests('success') do
 
     tests('#list_templates').formats(@templates_format) do
       pending if Fog.mocking?
       Fog::Compute[:cloudstack].list_templates('templateFilter' => "executable")
+    end
+
+    tests('#create_template').formats(@create_template_format) do
+      compute = Fog::Compute[:cloudstack]
+
+      compute.data[:volumes] = { '1ef854d-279e-4e68-9059-74980fd7b29b' => {'id' => '1ef854d-279e-4e68-9059-74980fd7b29b' } }
+      compute.data[:os_types] = { 'daf124c8-95d8-4756-8e1c-1871b073babb' => {'id' => 'daf124c8-95d8-4756-8e1c-1871b073babb' } }
+
+      compute.create_template 'displaytext' => 'saved template',
+                                               'name' => 'template name',
+                                               'ostypeid' => 'daf124c8-95d8-4756-8e1c-1871b073babb',
+                                               'volumeid' => '1ef854d-279e-4e68-9059-74980fd7b29b'
     end
 
   end
