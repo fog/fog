580e5e13296f468e6ac8f4ec0589f1cff2ca8895
dm1try <dmitry.dedov@tut.by>
2013-04-20 23:04:06 +0300

    [cloudstack] tests, fix snapshot test

--- tests/cloudstack/compute/models/snapshot_tests.rb
+++ tests/cloudstack/compute/models/snapshot_tests.rb
@@ -3,19 +3,6 @@ def snapshot_tests(connection, params, mocks_implemented = true)
     if !Fog.mocking? || mocks_implemented
       @instance.wait_for { ready? }
     end
-
-    @volume = @instance.connection.volumes.create(params[:volumes_attributes])
-    @volume.wait_for { ready? }
-
-    tests('create').succeeds do
-      @instance.create :volume_id => @volume.id
-    end
-
-    tests('destroy').succeeds do
-      @instance.destroy
-    end
-
-    @volume.destroy
   end
 end
 
