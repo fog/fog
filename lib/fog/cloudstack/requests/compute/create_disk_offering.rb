187f986fc76a3b38a9eba1935007da4ce767b789
Dmitry Dedov <dmitry.dedov@tut.by>
2013-04-22 15:44:43 +0300

    [cloudstack] add create/delete disk_offering mocks

--- lib/fog/cloudstack/requests/compute/create_disk_offering.rb
+++ lib/fog/cloudstack/requests/compute/create_disk_offering.rb
@@ -14,6 +14,38 @@ module Fog
         end
 
       end # Real
+
+      class Mock
+
+        def create_disk_offering(options={})
+          disk_offering_id = Fog::Cloudstack.uuid
+
+          first_domain_data = data[:domains].first.last
+          domain_id = options['domainid'] || first_domain_data['id']
+          domain_name = data[:domains][domain_id]['name']
+
+          storage_type = options['storagetype'] || 'shared'
+          customized = options['customized'] || false
+          disk_size = options['disk_size'] || 1
+
+          disk_offering = {
+            "id"           => disk_offering_id,
+            "domainid"     => domain_id,
+            "domain"       => domain_name,
+            "name"         => options['name'],
+            "displaytext"  => options['display_text'],
+            "disksize"     => disk_size,
+            "created"      => Time.now.iso8601,
+            "iscustomized" => customized,
+            "storagetype"  => storage_type
+          }
+
+          self.data[:disk_offerings][disk_offering_id] = disk_offering
+
+          {'creatediskofferingresponse' => disk_offering}
+        end
+      end
+
     end # Cloudstack
   end # Compute
 end # Fog
--- lib/fog/cloudstack/requests/compute/delete_disk_offering.rb
+++ lib/fog/cloudstack/requests/compute/delete_disk_offering.rb
@@ -14,6 +14,18 @@ module Fog
         end
 
       end # Real
+
+      class Mock
+
+        def delete_disk_offering(options={})
+          disk_offering_id = options['id']
+          data[:disk_offerings].delete(disk_offering_id) if data[:disk_offerings][disk_offering_id]
+
+          { 'deletediskofferingresponse' => { 'success' => 'true' } }
+        end
+
+      end
+
     end # Cloudstack
   end # Compute
 end # Fog
