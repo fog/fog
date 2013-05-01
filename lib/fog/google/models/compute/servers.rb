a00f7691fff3f7f4ee44bd3bfdf9ca6657127da5
Nat Welch <nat@natwelch.com>
2013-04-30 17:15:31 -0700

    auto expand home.

--- lib/fog/google/models/compute/servers.rb
+++ lib/fog/google/models/compute/servers.rb
@@ -47,8 +47,8 @@ module Fog
             :image_name => "gcel-12-04-v20130225",
             :machine_type => "n1-standard-1",
             :zone_name => "us-central1-a",
-            :private_key_path => "~/.ssh/id_rsa",
-            :public_key_path => "~/.ssh/id_rsa.pub",
+            :private_key_path => File.expand("~/.ssh/id_rsa"),
+            :public_key_path => File.expand("~/.ssh/id_rsa.pub"),
           }
 
           server = create(defaults.merge(new_attributes))
