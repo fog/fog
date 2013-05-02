d4fc1bf13dc953910ba456044efdb573b6da0c1c
Weston Platter <westonplatter@gmail.com>
2013-05-01 19:26:31 -0700

    [docs::aws::rds] converted formatting from RDoc to YARD

--- lib/fog/aws/requests/rds/create_db_instance.rb
+++ lib/fog/aws/requests/rds/create_db_instance.rb
@@ -5,31 +5,31 @@ module Fog
 
         require 'fog/aws/parsers/rds/create_db_instance'
 
-        # create a db instance
-        # http://docs.amazonwebservices.com/AmazonRDS/latest/APIReference/API_CreateDBInstance.html
-        # ==== Parameters
-        # * DBInstanceIdentifier <~String> - name of the db instance to modify
-        #
-        # * AllocatedStorage  <~Integer> Storage space, in GB
-        # * AutoMinorVersionUpgrade <~Boolean> Indicates that minor version upgrades will be applied automatically to the DB Instance during the maintenance window
-        # * AvailabilityZone <~String> The availability zone to create the instance in
-        # * BackupRetentionPeriod  <~Integer> 0-8 The number of days to retain automated backups.
-        # * DBInstanceClass <~String> The new compute and memory capacity of the DB Instance
-        # * DBName <~String> The name of the database to create when the DB Instance is created
-        # * DBParameterGroupName <~String> The name of the DB Parameter Group to apply to this DB Instance
-        # * DBSecurityGroups <~Array> A list of DB Security Groups to authorize on this DB Instance
-        # * Engine <~String> The name of the database engine to be used for this instance.
-        # * EngineVersion <~String> The version number of the database engine to use.
-        # * MasterUsername <~String> The db master user
-        # * MasterUserPassword  <~String> The new password for the DB Instance master user
-        # * MultiAZ <~Boolean> Specifies if the DB Instance is a Multi-AZ deployment
-        # * Port <~Integer> The port number on which the database accepts connections.
-        # * PreferredBackupWindow <~String> The daily time range during which automated backups are created if automated backups are enabled
-        # * PreferredMaintenanceWindow <~String> The weekly time range (in UTC) during which system maintenance can occur, which may result in an outage
-        # * DBSubnetGroupName <~String> The name, if any, of the VPC subnet for this RDS instance
-        # ==== Returns
-        # * response<~Excon::Response>:
-        #   * body<~Hash>:
+        # Create a db instance
+        # 
+        # @param DBInstanceIdentifier [String] name of the db instance to modify
+        # @param AllocatedStorage [Integer] Storage space, in GB
+        # @param AutoMinorVersionUpgrade [Boolean] Indicates that minor version upgrades will be applied automatically to the DB Instance during the maintenance window
+        # @param AvailabilityZone [String] The availability zone to create the instance in
+        # @param BackupRetentionPeriod [Integer] 0-8 The number of days to retain automated backups.
+        # @param DBInstanceClass [String] The new compute and memory capacity of the DB Instance
+        # @param DBName [String] The name of the database to create when the DB Instance is created
+        # @param DBParameterGroupName [String] The name of the DB Parameter Group to apply to this DB Instance
+        # @param DBSecurityGroups [Array] A list of DB Security Groups to authorize on this DB Instance
+        # @param Engine [String] The name of the database engine to be used for this instance.
+        # @param EngineVersion [String] The version number of the database engine to use.
+        # @param MasterUsername [String] The db master user
+        # @param MasterUserPassword [String] The new password for the DB Instance master user
+        # @param MultiAZ [Boolean] Specifies if the DB Instance is a Multi-AZ deployment
+        # @param Port [Integer] The port number on which the database accepts connections.
+        # @param PreferredBackupWindow [String] The daily time range during which automated backups are created if automated backups are enabled
+        # @param PreferredMaintenanceWindow [String] The weekly time range (in UTC) during which system maintenance can occur, which may result in an outage
+        # @param DBSubnetGroupName [String] The name, if any, of the VPC subnet for this RDS instance
+        # 
+        # @return [Excon::Response]:
+        #   * body [Hash]:
+        # 
+        # @see http://docs.amazonwebservices.com/AmazonRDS/latest/APIReference/API_CreateDBInstance.html
         def create_db_instance(db_name, options={})
 
           if security_groups = options.delete('DBSecurityGroups')
