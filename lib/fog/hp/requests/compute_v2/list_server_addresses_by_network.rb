36ec19f0351ed96b88bc6165d45a0ca48405f860
Rupak Ganguly <rupakg@gmail.com>
2013-05-08 16:50:02 -0400

    [hp|compute_v2] Fix mocks for server addresses. Fix create_server and server model to accept networks and availability zones params.

--- lib/fog/hp/requests/compute_v2/list_server_addresses_by_network.rb
+++ lib/fog/hp/requests/compute_v2/list_server_addresses_by_network.rb
@@ -7,17 +7,19 @@ module Fog
         #
         # ==== Parameters
         # * 'server_id'<~String> - UUId of server to list addresses for
-        # * 'network_id'<~String> - UUId of the network to list addresses for
+        # * 'network_name'<~String> - Name of the network to list addresses for
         #
         # ==== Returns
         # * response<~Excon::Response>:
         #   * body<~Hash>:
         #     * 'custom'<~Array> - IP addresses for the server, for the network. The network name can change based on setup.
-        def list_server_addresses_by_network(server_id, network_id)
+        #       * 'version'<~Integer> - IP version, 4 or 6
+        #       * 'addr'<~String> - IP address
+        def list_server_addresses_by_network(server_id, network_name)
           request(
             :expects  => [200,203],
             :method   => 'GET',
-            :path     => "servers/#{server_id}/ips/#{network_id}"
+            :path     => "servers/#{server_id}/ips/#{Fog::HP.escape(network_name)}"
           )
         end
 
@@ -25,12 +27,13 @@ module Fog
 
       class Mock
 
-        def list_server_addresses_by_network(server_id, network_id)
+        def list_server_addresses_by_network(server_id, network_name)
           response = Excon::Response.new
           if server = list_servers_detail.body['servers'].detect {|_| _['id'] == server_id}
             response.status = 200
-            # since we cannot get to networks from compute, just return all addresses
-            response.body = { 'addresses' => server['addresses'] }
+            # get the addresses for the network, which is 'custom' in case of mocks
+            address = server['addresses'].select { |key, _| key == network_name}
+            response.body = address
             response
           else
             raise Fog::Compute::HPV2::NotFound
