89c569fee24233b38c7717c508dec035ff49a015
Eric Wong <ericsyw@gmail.com>
2013-05-23 07:35:02 +0800

    [stormondemand|storage] Add Storage service and cluster API

--- lib/fog/storm_on_demand.rb
+++ lib/fog/storm_on_demand.rb
@@ -6,7 +6,8 @@ module Fog
     extend Fog::Provider
 
     service(:compute, 'storm_on_demand/compute', 'Compute')
-
+    service(:storage, 'storm_on_demand/storage', 'Storage')
+    
   end
 end
 
---
+++ lib/fog/storm_on_demand/models/storage/cluster.rb
@@ -0,0 +1,20 @@
+require 'fog/core/model'
+
+module Fog
+  module Storage
+    class StormOnDemand
+
+      class Cluster < Fog::Model
+        identity :id
+        attribute :description
+        attribute :zone
+
+        def initialize(attributes={})
+          super
+        end
+
+      end
+      
+    end
+  end
+end
