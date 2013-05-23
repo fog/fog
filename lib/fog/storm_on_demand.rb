3b567d29dd9ec0309b03147803efa0d6a88c4454
Eric Wong <ericsyw@gmail.com>
2013-05-23 17:12:27 +0800

    [stormondemand|dns] Add DNS Service to StormOnDemand

--- lib/fog/storm_on_demand.rb
+++ lib/fog/storm_on_demand.rb
@@ -7,6 +7,7 @@ module Fog
 
     service(:compute, 'storm_on_demand/compute', 'Compute')
     service(:storage, 'storm_on_demand/storage', 'Storage')
+    service(:dns, 'storm_on_demand/dns', 'DNS')
     
   end
 end
