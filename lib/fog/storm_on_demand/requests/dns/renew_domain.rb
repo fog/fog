a1982511e1c1eca8f87b7c71fc2eadbc71adb3ef
Eric Wong <ericsyw@gmail.com>
2013-05-23 11:07:13 +0800

    [stormondemand|dns] Add DNS service and domain APIs

---
+++ lib/fog/storm_on_demand/requests/dns/renew_domain.rb
@@ -0,0 +1,16 @@
+module Fog
+  module DNS
+    class StormOnDemand
+      class Real
+
+        def renew_domain(options={})
+          request(
+            :path => '/Network/DNS/Domain/renew',
+            :body => Fog::JSON.encode(:params => options)
+          )
+        end
+
+      end
+    end
+  end
+end
