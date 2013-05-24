3de8f5c9c4ef742d60786c0c221be09a52699aee
Eric Wong <ericsyw@gmail.com>
2013-05-24 17:29:12 +0800

    [stormondemand|billing] Add Billing service and related APIs

--- lib/fog/storm_on_demand.rb
+++ lib/fog/storm_on_demand.rb
@@ -8,6 +8,7 @@ module Fog
     service(:compute, 'storm_on_demand/compute', 'Compute')
     service(:storage, 'storm_on_demand/storage', 'Storage')
     service(:dns, 'storm_on_demand/dns', 'DNS')
+    service(:billing, 'storm_on_demand/billing', 'Billing')
     
   end
 end
