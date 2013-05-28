08fc2f1eac35fa8744e1ac73e3564a382b3def31
Rodrigo Estebanez <restebanez@mdsol.com>
2013-05-28 13:31:02 +0200

    server.tags implemented

---
+++ lib/fog/vcloud/models/compute/tag.rb
@@ -0,0 +1,16 @@
+module Fog
+  module Vcloud
+    class Compute
+      class Tag < Fog::Vcloud::Model
+        identity :href, :aliases => :Href
+        attribute :links, :aliases => :Link, :type => :array
+        ignore_attributes :xmlns, :xmlns_i, :xmlns_xsi, :xmlns_xsd
+
+        attribute :key, :aliases => :Key
+        attribute :value, :aliases => :Value
+        
+        
+      end
+    end
+  end
+end
