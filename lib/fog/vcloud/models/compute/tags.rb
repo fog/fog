620483cf73c9ffb9208da92fd62267992f37c9b4
Rodrigo Estebanez <restebanez@mdsol.com>
2013-05-28 20:28:06 +0200

    Tags#create implemented

--- lib/fog/vcloud/models/compute/tags.rb
+++ lib/fog/vcloud/models/compute/tags.rb
@@ -22,6 +22,10 @@ module Fog
         rescue Fog::Errors::NotFound
           nil
         end
+        
+        def create(opts)
+          service.configure_metadata(opts.merge(href: href))
+        end
       end
     end
   end
