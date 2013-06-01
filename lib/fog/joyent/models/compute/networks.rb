5fc41c8dc202c3c140391eee5d5b496fc28f7f61
Blake Irvin and Eric Saxby <pair+blake+sax@wanelo.com>
2013-05-31 18:01:42 -0700

    Add joyent API version and network support GH-1853
    Adding support for listing networks in the Joyent Cloud.
    :joyent_version was defined in a way that it would always
    fall back to the default '~6.5'. Creating servers with
    a particular network requires setting :joyent_version to '~7.0'
    or greater.

---
+++ lib/fog/joyent/models/compute/networks.rb
@@ -0,0 +1,15 @@
+require 'fog/joyent/models/compute/network'
+module Fog
+  module Compute
+    class Joyent
+      class Networks < Fog::Collection
+
+        model Fog::Compute::Joyent::Network
+
+        def all
+          load(service.list_networks.body)
+        end
+      end
+    end
+  end
+end
