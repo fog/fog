4bbf46ebe24ccc564b9f9f8477bc6bd9ebfbceb7
Rodrigo Estebanez <restebanez@mdsol.com>
2013-06-11 18:12:54 +0200

    get_organization implemented

---
+++ lib/fog/vcloudng/parsers/compute/get_organization.rb
@@ -0,0 +1,51 @@
+module Fog
+  module Parsers
+    module Vcloudng
+      module Compute
+
+        class GetOrganization < VcloudngParser
+
+          def reset
+            @response = { 'Links' => [] }
+          end
+
+          def start_element(name, attributes)
+            super
+            case name
+            when 'Link'
+              link = extract_attributes(attributes)
+              until attributes.empty?
+                if attributes.first.is_a?(Array)
+                  attribute = attributes.shift
+                  link[attribute.first] = attribute.last
+                else
+                  link[attributes.shift] = attributes.shift
+                end
+              end
+              @response['Links'] << link
+            when 'Org'
+              org = extract_attributes(attributes)
+              until attributes.empty?
+                if attributes.first.is_a?(Array)
+                  attribute = attributes.shift
+                  org[attribute.first] = attribute.last
+                else
+                  org[attributes.shift] = attributes.shift
+                end
+              end
+              @response['href'] = org['href']
+              @response['name'] = org['name']
+            end
+          end
+
+          def end_element(name)
+            if name == 'Description'
+              @response[name] = value
+            end
+          end
+
+        end
+      end
+    end
+  end
+end
---
+++ lib/fog/vcloudng/requests/compute/get_organization.rb
@@ -0,0 +1,39 @@
+module Fog
+  module Vcloudng
+    module Compute
+      class Real
+        
+        require 'fog/vcloudng/parsers/compute/get_organization'
+        
+        # Get details of an organization
+        #
+        # ==== Parameters
+        # * organization_id<~Integer> - Id of organization to lookup
+        #
+        # ==== Returns
+        # * response<~Excon::Response>:
+        #   * body<~Hash>:
+        #     * 'description'<~String> - Description of organization
+        #     * 'links'<~Array> - An array of links to entities in the organization
+        #       * 'href'<~String> - location of link
+        #       * 'name'<~String> - name of link
+        #       * 'rel'<~String> - action to perform
+        #       * 'type'<~String> - type of link
+        #     * 'name'<~String> - Name of organization
+        def get_organization(organization_id)
+        
+          request({
+            :expects  => 200,
+            :headers  => { 'Accept' => 'application/*+xml;version=1.5' },
+            :method   => 'GET',
+            :parser   => Fog::Parsers::Vcloudng::Compute::GetOrganization.new,
+            :path     => "org/#{organization_id}"
+          })
+        end
+
+      end
+
+
+    end
+  end
+end
