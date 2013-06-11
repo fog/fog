cb131cf74e3874a4c70ad3ddba6086c5eb8cbdec
Rupak Ganguly <rupakg@gmail.com>
2013-06-11 13:58:08 -0400

    Bump version and update changelog.

--- lib/fog/hp.rb
+++ lib/fog/hp.rb
@@ -5,7 +5,7 @@ module Fog
 
     # define a specific version for the HP Provider
     unless const_defined?(:VERSION)
-      VERSION = '0.0.20'
+      VERSION = '0.0.21'
     end
 
     extend Fog::Provider
