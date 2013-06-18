14ac44ce62161c9c4efc7ad3a2710cb5b66d49a3
Rodrigo Estebanez <restebanez@mdsol.com>
2013-06-18 16:14:53 +0200

    parsed body as comment

--- lib/fog/vcloudng/parsers/compute/get_vapp_template.rb
+++ lib/fog/vcloudng/parsers/compute/get_vapp_template.rb
@@ -1,22 +1,22 @@
 #
 # <?xml version="1.0" encoding="UTF-8"?>
-# <VAppTemplate xmlns="http://www.vmware.com/vcloud/v1.5" xmlns:ovf="http://schemas.dmtf.org/ovf/envelope/1" ovfDescriptorUploaded="true" goldMaster="false" status="8" name="DEVWEB" id="urn:vcloud:vapptemplate:ed2b234f-c03f-460b-b877-bedd2255dfb3" type="application/vnd.vmware.vcloud.vAppTemplate+xml" href="https://devlab.mdsol.com/api/vAppTemplate/vappTemplate-ed2b234f-c03f-460b-b877-bedd2255dfb3" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://schemas.dmtf.org/ovf/envelope/1 http://schemas.dmtf.org/ovf/envelope/1/dsp8023_1.1.0.xsd http://www.vmware.com/vcloud/v1.5 http://10.194.1.65/api/v1.5/schema/master.xsd">
-#     <Link rel="catalogItem" type="application/vnd.vmware.vcloud.catalogItem+xml" href="https://devlab.mdsol.com/api/catalogItem/5b3f97f1-13bf-450e-a632-126aac3bb3d9"/>
-#     <Link rel="enable" href="https://devlab.mdsol.com/api/vAppTemplate/vappTemplate-ed2b234f-c03f-460b-b877-bedd2255dfb3/action/enableDownload"/>
-#     <Link rel="disable" href="https://devlab.mdsol.com/api/vAppTemplate/vappTemplate-ed2b234f-c03f-460b-b877-bedd2255dfb3/action/disableDownload"/>
-#     <Link rel="ovf" type="text/xml" href="https://devlab.mdsol.com/api/vAppTemplate/vappTemplate-ed2b234f-c03f-460b-b877-bedd2255dfb3/ovf"/>
-#     <Link rel="down" type="application/vnd.vmware.vcloud.owner+xml" href="https://devlab.mdsol.com/api/vAppTemplate/vappTemplate-ed2b234f-c03f-460b-b877-bedd2255dfb3/owner"/>
-#     <Link rel="down" type="application/vnd.vmware.vcloud.metadata+xml" href="https://devlab.mdsol.com/api/vAppTemplate/vappTemplate-ed2b234f-c03f-460b-b877-bedd2255dfb3/metadata"/>
+# <VAppTemplate xmlns="http://www.vmware.com/vcloud/v1.5" xmlns:ovf="http://schemas.dmtf.org/ovf/envelope/1" ovfDescriptorUploaded="true" goldMaster="false" status="8" name="DEVWEB" id="urn:vcloud:vapptemplate:ed2b234f-c03f-460b-b877-bedd2255dfb3" type="application/vnd.vmware.vcloud.vAppTemplate+xml" href="https://example.com/api/vAppTemplate/vappTemplate-ed2b234f-c03f-460b-b877-bedd2255dfb3" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://schemas.dmtf.org/ovf/envelope/1 http://schemas.dmtf.org/ovf/envelope/1/dsp8023_1.1.0.xsd http://www.vmware.com/vcloud/v1.5 http://10.194.1.65/api/v1.5/schema/master.xsd">
+#     <Link rel="catalogItem" type="application/vnd.vmware.vcloud.catalogItem+xml" href="https://example.com/api/catalogItem/5b3f97f1-13bf-450e-a632-126aac3bb3d9"/>
+#     <Link rel="enable" href="https://example.com/api/vAppTemplate/vappTemplate-ed2b234f-c03f-460b-b877-bedd2255dfb3/action/enableDownload"/>
+#     <Link rel="disable" href="https://example.com/api/vAppTemplate/vappTemplate-ed2b234f-c03f-460b-b877-bedd2255dfb3/action/disableDownload"/>
+#     <Link rel="ovf" type="text/xml" href="https://example.com/api/vAppTemplate/vappTemplate-ed2b234f-c03f-460b-b877-bedd2255dfb3/ovf"/>
+#     <Link rel="down" type="application/vnd.vmware.vcloud.owner+xml" href="https://example.com/api/vAppTemplate/vappTemplate-ed2b234f-c03f-460b-b877-bedd2255dfb3/owner"/>
+#     <Link rel="down" type="application/vnd.vmware.vcloud.metadata+xml" href="https://example.com/api/vAppTemplate/vappTemplate-ed2b234f-c03f-460b-b877-bedd2255dfb3/metadata"/>
 #     <Description>Windows Server 2008 R2 Web Edition</Description>
 #     <Owner type="application/vnd.vmware.vcloud.owner+xml">
-#         <User type="application/vnd.vmware.admin.user+xml" name="cthayer" href="https://devlab.mdsol.com/api/admin/user/66b8c149-15e1-42f5-b391-2d0e6573a960"/>
+#         <User type="application/vnd.vmware.admin.user+xml" name="cthayer" href="https://example.com/api/admin/user/66b8c149-15e1-42f5-b391-2d0e6573a960"/>
 #     </Owner>
 #     <Children>
-#         <Vm goldMaster="false" name="DEVWEB" id="urn:vcloud:vm:12af4ee1-04ad-4ae5-bab2-2d5db4296c85" type="application/vnd.vmware.vcloud.vm+xml" href="https://devlab.mdsol.com/api/vAppTemplate/vm-12af4ee1-04ad-4ae5-bab2-2d5db4296c85">
-#             <Link rel="up" type="application/vnd.vmware.vcloud.vAppTemplate+xml" href="https://devlab.mdsol.com/api/vAppTemplate/vappTemplate-ed2b234f-c03f-460b-b877-bedd2255dfb3"/>
-#             <Link rel="down" type="application/vnd.vmware.vcloud.metadata+xml" href="https://devlab.mdsol.com/api/vAppTemplate/vm-12af4ee1-04ad-4ae5-bab2-2d5db4296c85/metadata"/>
+#         <Vm goldMaster="false" name="DEVWEB" id="urn:vcloud:vm:12af4ee1-04ad-4ae5-bab2-2d5db4296c85" type="application/vnd.vmware.vcloud.vm+xml" href="https://example.com/api/vAppTemplate/vm-12af4ee1-04ad-4ae5-bab2-2d5db4296c85">
+#             <Link rel="up" type="application/vnd.vmware.vcloud.vAppTemplate+xml" href="https://example.com/api/vAppTemplate/vappTemplate-ed2b234f-c03f-460b-b877-bedd2255dfb3"/>
+#             <Link rel="down" type="application/vnd.vmware.vcloud.metadata+xml" href="https://example.com/api/vAppTemplate/vm-12af4ee1-04ad-4ae5-bab2-2d5db4296c85/metadata"/>
 #             <Description>Windows Server 2008 R2 Web Edition</Description>
-#             <NetworkConnectionSection type="application/vnd.vmware.vcloud.networkConnectionSection+xml" href="https://devlab.mdsol.com/api/vAppTemplate/vm-12af4ee1-04ad-4ae5-bab2-2d5db4296c85/networkConnectionSection/" ovf:required="false">
+#             <NetworkConnectionSection type="application/vnd.vmware.vcloud.networkConnectionSection+xml" href="https://example.com/api/vAppTemplate/vm-12af4ee1-04ad-4ae5-bab2-2d5db4296c85/networkConnectionSection/" ovf:required="false">
 #                 <ovf:Info>Specifies the available VM network connections</ovf:Info>
 #                 <PrimaryNetworkConnectionIndex>0</PrimaryNetworkConnectionIndex>
 #                 <NetworkConnection network="none" needsCustomization="true">
@@ -26,7 +26,7 @@
 #                     <IpAddressAllocationMode>NONE</IpAddressAllocationMode>
 #                 </NetworkConnection>
 #             </NetworkConnectionSection>
-#             <GuestCustomizationSection type="application/vnd.vmware.vcloud.guestCustomizationSection+xml" href="https://devlab.mdsol.com/api/vAppTemplate/vm-12af4ee1-04ad-4ae5-bab2-2d5db4296c85/guestCustomizationSection/" ovf:required="false">
+#             <GuestCustomizationSection type="application/vnd.vmware.vcloud.guestCustomizationSection+xml" href="https://example.com/api/vAppTemplate/vm-12af4ee1-04ad-4ae5-bab2-2d5db4296c85/guestCustomizationSection/" ovf:required="false">
 #                 <ovf:Info>Specifies Guest OS Customization Settings</ovf:Info>
 #                 <Enabled>true</Enabled>
 #                 <ChangeSid>false</ChangeSid>
@@ -41,13 +41,13 @@
 #             <VAppScopedLocalId>DEVWEB</VAppScopedLocalId>
 #         </Vm>
 #     </Children>
-#     <ovf:NetworkSection xmlns:ns12="http://www.vmware.com/vcloud/v1.5" ns12:href="https://devlab.mdsol.com/api/vAppTemplate/vappTemplate-ed2b234f-c03f-460b-b877-bedd2255dfb3/networkSection/" ns12:type="application/vnd.vmware.vcloud.networkSection+xml">
+#     <ovf:NetworkSection xmlns:ns12="http://www.vmware.com/vcloud/v1.5" ns12:href="https://example.com/api/vAppTemplate/vappTemplate-ed2b234f-c03f-460b-b877-bedd2255dfb3/networkSection/" ns12:type="application/vnd.vmware.vcloud.networkSection+xml">
 #         <ovf:Info>The list of logical networks</ovf:Info>
 #         <ovf:Network ovf:name="none">
 #             <ovf:Description>This is a special place-holder used for disconnected network interfaces.</ovf:Description>
 #         </ovf:Network>
 #     </ovf:NetworkSection>
-#     <NetworkConfigSection type="application/vnd.vmware.vcloud.networkConfigSection+xml" href="https://devlab.mdsol.com/api/vAppTemplate/vappTemplate-ed2b234f-c03f-460b-b877-bedd2255dfb3/networkConfigSection/" ovf:required="false">
+#     <NetworkConfigSection type="application/vnd.vmware.vcloud.networkConfigSection+xml" href="https://example.com/api/vAppTemplate/vappTemplate-ed2b234f-c03f-460b-b877-bedd2255dfb3/networkConfigSection/" ovf:required="false">
 #         <ovf:Info>The configuration parameters for logical networks</ovf:Info>
 #         <NetworkConfig networkName="none">
 #             <Description>This is a special place-holder used for disconnected network interfaces.</Description>
@@ -57,17 +57,134 @@
 #             <IsDeployed>false</IsDeployed>
 #         </NetworkConfig>
 #     </NetworkConfigSection>
-#     <LeaseSettingsSection type="application/vnd.vmware.vcloud.leaseSettingsSection+xml" href="https://devlab.mdsol.com/api/vAppTemplate/vappTemplate-ed2b234f-c03f-460b-b877-bedd2255dfb3/leaseSettingsSection/" ovf:required="false">
+#     <LeaseSettingsSection type="application/vnd.vmware.vcloud.leaseSettingsSection+xml" href="https://example.com/api/vAppTemplate/vappTemplate-ed2b234f-c03f-460b-b877-bedd2255dfb3/leaseSettingsSection/" ovf:required="false">
 #         <ovf:Info>Lease settings section</ovf:Info>
-#         <Link rel="edit" type="application/vnd.vmware.vcloud.leaseSettingsSection+xml" href="https://devlab.mdsol.com/api/vAppTemplate/vappTemplate-ed2b234f-c03f-460b-b877-bedd2255dfb3/leaseSettingsSection/"/>
+#         <Link rel="edit" type="application/vnd.vmware.vcloud.leaseSettingsSection+xml" href="https://example.com/api/vAppTemplate/vappTemplate-ed2b234f-c03f-460b-b877-bedd2255dfb3/leaseSettingsSection/"/>
 #         <StorageLeaseInSeconds>7776000</StorageLeaseInSeconds>
 #         <StorageLeaseExpiration>2013-09-11T15:53:20.900Z</StorageLeaseExpiration>
 #     </LeaseSettingsSection>
-#     <CustomizationSection type="application/vnd.vmware.vcloud.customizationSection+xml" href="https://devlab.mdsol.com/api/vAppTemplate/vappTemplate-ed2b234f-c03f-460b-b877-bedd2255dfb3/customizationSection/" goldMaster="false" ovf:required="false">
+#     <CustomizationSection type="application/vnd.vmware.vcloud.customizationSection+xml" href="https://example.com/api/vAppTemplate/vappTemplate-ed2b234f-c03f-460b-b877-bedd2255dfb3/customizationSection/" goldMaster="false" ovf:required="false">
 #         <ovf:Info>VApp template customization section</ovf:Info>
 #         <CustomizeOnInstantiate>true</CustomizeOnInstantiate>
 #     </CustomizationSection>
 # </VAppTemplate>
+
+#module Boolean; end
+#class TrueClass; include Boolean; end
+#class FalseClass; include Boolean; end
+#
+#module Fog
+#  module Parsers
+#    module Vcloudng
+#      module Compute
+#
+#
+#        class GetVappTemplate < VcloudngParser
+#          
+#          
+#
+#          def customization_section
+#  
+#          end
+#
+#          def lease_settings_section
+#  
+#          end
+#
+#
+#          def network_section
+#  
+#          end
+#
+#
+#          def vm
+#  
+#          end
+#
+#          def children
+#            {
+#              'elements' => { 'Vm' => vm }
+#            }
+#          end
+#
+#          def link
+#           { 
+#             'attributes' => { 'rel' => String, 
+#                               'type' => String,
+#                               'href' => String 
+#                             }
+#           }
+#          end
+#                                                                
+#
+#
+#          def owner
+#            { 
+#              'attributes' => { 'type' => String },
+#              'elements' =>  { 'user' => user }
+#            }
+#          end
+#
+#          def user
+#           { 
+#             'elements' => { 'type' => String, 
+#                             'name' => String, 
+#                             'href' => String  
+#                           }
+#           }
+#          end
+#          
+#          def vapp_template
+#            { 
+#              'attributes' => { 'goldMaster' => Boolean, 
+#                               'status' => Integer, 
+#                               'name' => String, 
+#                               'type' => String, 
+#                               'href' => String 
+#                             },
+#             'elements' => {  'Link' => link,
+#                              'Description' => String,
+#                              'Owner' => owner,
+#                              'Children' => children,
+#                              'NetworkSection' => network_section,
+#                              'LeaseSettingsSection' => lease_settings_section,
+#                              'CustomizationSection' => customization_section
+#                            } 
+#            
+#            }
+#          end
+#          
+#          @schema = { 'VAppTemplate' => vapp_template }
+#
+#          def reset
+#            @response = { 'Links' => [], 'Children' => [] }
+#            @current_path = []
+#          end
+#
+#          def start_element(name, attributes)
+#            super
+#            if is_an_attribute?(name)
+#              @last_element = name
+#              current = extract_attributes(attributes)
+#            else
+#              @current_path << name if @current_path.last != name
+#
+#            end
+#          end
+#          
+#          def end_element(name)
+#            case name
+#            when @schema['elements'].keys?
+#              @schema['attributes'][name] ==
+#            end
+#          end
+#
+#        end
+#
+#      end
+#    end
+#  end
+#end
 
 module Fog
   module Parsers
@@ -186,3 +303,4 @@ module Fog
     end
   end
 end
+
