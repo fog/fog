1aaa084bbf915b962b9b6e359e81c1e58819e94a
Rodrigo Estebanez <restebanez@mdsol.com>
2013-06-18 16:14:31 +0200

    parsed body as comment

--- lib/fog/vcloudng/parsers/compute/get_organizations.rb
+++ lib/fog/vcloudng/parsers/compute/get_organizations.rb
@@ -1,3 +1,18 @@
+#
+# <?xml version="1.0" encoding="UTF-8"?>
+# <OrgList xmlns="http://www.vmware.com/vcloud/v1.5" type="application/vnd.vmware.vcloud.orgList+xml" href="https://example.com/api/org/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.vmware.com/vcloud/v1.5 http://10.194.1.65/api/v1.5/schema/master.xsd">
+#     <Org type="application/vnd.vmware.vcloud.org+xml" name="DevOps" href="https://example.com/api/org/c6a4c623-c158-41cf-a87a-dbc1637ad55a"/>
+# </OrgList>
+#
+# parsed:
+# {"OrgList"=>
+#   [{"type"=>"application/vnd.vmware.vcloud.org+xml",
+#     "name"=>"DevOps",
+#     "href"=>
+#      "https://example.com/api/org/c6a4c623-c158-41cf-a87a-dbc1637ad55a",
+#     "id"=>"c6a4c623-c158-41cf-a87a-dbc1637ad55a"}]}
+#    
+
 module Fog
   module Parsers
     module Vcloudng
@@ -13,14 +28,7 @@ module Fog
             super
             if name == 'Org'
               organization = extract_attributes(attributes)
-              until attributes.empty?
-                if attributes.first.is_a?(Array)
-                  attribute = attributes.shift
-                  organization[attribute.first] = attribute.last
-                else
-                  organization[attributes.shift] = attributes.shift
-                end
-              end
+              organization['id'] = organization['href'].split('/').last
               @response['OrgList'] << organization
             end
           end
