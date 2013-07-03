4781d517385e819eeca0ba19ad2c87de5b5935e1
James Bence <jbence@mdsol.com>
2013-07-03 08:18:29 -0700

    Construct hash with => (for 1.8.7)

--- lib/fog/aws.rb
+++ lib/fog/aws.rb
@@ -307,7 +307,7 @@ module Fog
         matcher = lambda {|s| s.match(/(?:.*<Code>(.*)<\/Code>)(?:.*<Message>(.*)<\/Message>)/m)}
         [error.message, error.response.body].each(&Proc.new {|s|
           match = matcher.call(s)
-          return {code: match[1].split('.').last, message: match[2]} if match
+          return {:code => match[1].split('.').last, :message => match[2]} if match
         })
         {} # we did not match the message or response body
       end
