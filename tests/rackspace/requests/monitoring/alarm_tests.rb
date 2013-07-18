6b5662fd05c01b931b331926ac6b896d9b308dc5
Daniel Reichert <Daniel.Reichert@rackspace.com>
2013-07-18 10:43:23 -0700

    Added alarm list and alarm get tests

--- tests/rackspace/requests/monitoring/alarm_tests.rb
+++ tests/rackspace/requests/monitoring/alarm_tests.rb
@@ -1,4 +1,4 @@
-Shindo.tests('Fog::Rackspace::Monitoring | check_alarms', ['rackspace','rackspacemonitoring']) do
+Shindo.tests('Fog::Rackspace::Monitoring | alarm_tests', ['rackspace']) do
   pending if Fog.mocking? 
 
   account = Fog::Rackspace::Monitoring.new
@@ -26,22 +26,34 @@ Shindo.tests('Fog::Rackspace::Monitoring | check_alarms', ['rackspace','rackspac
       options = { :label => "Bar"}
       account.update_alarm(entity_id,alarm_id,options).data
     end
+    tests('#list alarms').formats(LIST_HEADERS_FORMAT) do
+      account.list_alarms(entity_id).data[:headers]
+    end
+    tests('#get alarm').formats(LIST_HEADERS_FORMAT) do
+      account.get_alarm(entity_id,alarm_id).data[:headers]
+    end
     # delete is not currently supported in fog
     #tests('#delete alarm').formats(DELETE_DATA_FORMAT) do
       #account.delete_alarm(entity_id,check_id).data
     #end
   end
   tests('failure') do
-    tests('#create new alarm(-1)').raises(Fog::Rackspace::Monitoring::BadRequest) do
+    tests('#fail to create new alarm(-1)').raises(Fog::Rackspace::Monitoring::BadRequest) do
       account.create_alarm(entity_id, {:type => ""})
     end
     # Commenting out update because incorrect update throws a 502
-    #tests('#update invalid alarm(-1)').raises(Fog::Rackspace::Monitoring::NotFound) do
+    #tests('#fail to update invalid alarm(-1)').raises(Fog::Rackspace::Monitoring::NotFound) do
       #options = { :testing => "Bar" }
       #response = account.update_alarm(-1,-1,options)
     #end
+    tests('#fail to list alarms').raises(Fog::Rackspace::Monitoring::NotFound) do
+      account.list_alarms(-1)
+    end
+    tests('#fail to get alarm').raises(Fog::Rackspace::Monitoring::NotFound) do
+      account.get_alarm(-1,-1)
+    end
     # delete is not currently supported in fog
-    #tests('#delete alarm(-1)').raises(Fog::Rackspace::Monitoring::NotFound) do
+    #tests('#fail to delete alarm(-1)').raises(Fog::Rackspace::Monitoring::NotFound) do
       #account.delete_alarm(-1,-1)
     #end
   end
