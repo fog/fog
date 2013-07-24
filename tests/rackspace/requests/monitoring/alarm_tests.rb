7f2f380d0fe2ca771b1368fbd330dfc2676d7647
Daniel Reichert <Daniel.Reichert@rackspace.com>
2013-07-24 11:43:06 -0700

    Forgot to add underscore for test groups

--- tests/rackspace/requests/monitoring/alarm_tests.rb
+++ tests/rackspace/requests/monitoring/alarm_tests.rb
@@ -1,4 +1,4 @@
-Shindo.tests('Fog::Rackspace::Monitoring | alarm_tests', ['rackspace','rackspacemonitoring']) do
+Shindo.tests('Fog::Rackspace::Monitoring | alarm_tests', ['rackspace','rackspace_monitoring']) do
   pending if Fog.mocking?
 
   account = Fog::Rackspace::Monitoring.new
