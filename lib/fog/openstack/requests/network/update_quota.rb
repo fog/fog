a970f3853a788519d97ca345d00dc527ea31c885
Evan Petrie <ep873k@att.com>
2013-07-26 16:18:50 -0700

    add support for openstack network quota endpoints

---
+++ lib/fog/openstack/requests/network/update_quota.rb
@@ -0,0 +1,33 @@
+module Fog
+  module Network
+    class OpenStack
+      class Real
+
+        def update_quota(tenant_id, options = {})
+          request(
+            :body     => Fog::JSON.encode({ 'quota' => options} ),
+            :expects  => 200,
+            :method   => 'PUT',
+            :path     => "/quotas/#{tenant_id}"
+          )
+        end
+
+      end
+
+      class Mock
+
+        def update_quota(tenant_id, options = {})
+          self.data[:quota_updated] = self.data[:quota].merge options
+
+          response = Excon::Response.new
+          response.status = 200
+          response.body = { 'quota' => self.data[:quota_updated] }
+          response
+        end
+
+      end
+
+    end
+  end
+end
+
