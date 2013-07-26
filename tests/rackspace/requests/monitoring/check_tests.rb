6368faf8cf4dc4345cc42a91eadaf7d71f40553a
Daniel Reichert <Daniel.Reichert@rackspace.com>
2013-07-26 09:56:24 -0700

    WIP:  Adding get tests for checks
    Currently one of the tests fail, pushing progress.  Working
    test commit to follow soon.

--- tests/rackspace/requests/monitoring/check_tests.rb
+++ tests/rackspace/requests/monitoring/check_tests.rb
@@ -1,4 +1,4 @@
-Shindo.tests('Fog::Rackspace::Monitoring | check_tests', ['rackspace', 'rackspace_monitoring']) do
+Shindo.tests('Fog::Rackspace::Monitoring | check_tests', ['rackspace', 'rackspacemonitoring']) do
   pending if Fog.mocking? 
 
   account = Fog::Rackspace::Monitoring.new
@@ -9,6 +9,9 @@ Shindo.tests('Fog::Rackspace::Monitoring | check_tests', ['rackspace', 'rackspac
       response = account.create_check(entity_id, CHECK_CREATE_OPTIONS).data
       check_id = response[:headers]['X-Object-ID']
       response
+    end
+    tests('#get check').formats(LIST_HEADERS_FORMAT) do
+      account.get_check(entity_id,check_id).data
     end
     tests('#update check').formats(DATA_FORMAT) do
       options = { :label => "Bar"}
@@ -21,6 +24,9 @@ Shindo.tests('Fog::Rackspace::Monitoring | check_tests', ['rackspace', 'rackspac
   tests('failure') do
     tests('#create new check(-1)').raises(Fog::Rackspace::Monitoring::BadRequest) do
       account.create_check(entity_id, {:type => ""})
+    end
+    tests('#get check(-1)').raises(Fog::Rackspace::Monitoring::NotFound) do
+      account.get_check(-1, -1)
     end
     tests('#update invalid check(-1)').raises(Fog::Rackspace::Monitoring::NotFound) do
       options = { :testing => "Bar" }
