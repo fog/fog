c97c5662779b8bc64ae0fd645d973e8a7906aa60
Brian D. Burns <iosctr@gmail.com>
2013-07-28 20:37:51 -0400

    [openstack|storage] add default Accept header

--- lib/fog/openstack/storage.rb
+++ lib/fog/openstack/storage.rb
@@ -145,6 +145,7 @@ module Fog
             response = @connection.request(params.merge({
               :headers  => {
                 'Content-Type' => 'application/json',
+                'Accept' => 'application/json',
                 'X-Auth-Token' => @auth_token
               }.merge!(params[:headers] || {}),
               :host     => @host,
