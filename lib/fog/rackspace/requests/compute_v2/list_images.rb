25be8d0a3baeb74d9514d9dc6f00b32eff88b510
dJason <jasonwalp@yahoo.com>
2013-08-20 10:38:58 -0600

    Update list_images.rb
    Comments were copied from list_flavors.rb, correcting documentation link

--- lib/fog/rackspace/requests/compute_v2/list_images.rb
+++ lib/fog/rackspace/requests/compute_v2/list_images.rb
@@ -8,14 +8,14 @@ module Fog
         #   * body [Hash]:
         #     * images [Array]:
         #       * [Hash]:
-        #         * id [String] - flavor id
+        #         * id [String] - image id
         #         * links [Array] - image links
         #         * name [String] - image name
         # @raise [Fog::Compute::RackspaceV2::NotFound] - HTTP 404
         # @raise [Fog::Compute::RackspaceV2::BadRequest] - HTTP 400
         # @raise [Fog::Compute::RackspaceV2::InternalServerError] - HTTP 500
         # @raise [Fog::Compute::RackspaceV2::ServiceError]
-        # @see http://docs.rackspace.com/servers/api/v2/cs-devguide/content/List_Flavors-d1e4188.html        
+        # @see http://docs.rackspace.com/servers/api/v2/cs-devguide/content/List_Images-d1e4435.html
         def list_images
           request(
             :expects => [200, 203],
