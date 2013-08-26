7f2a094a7735915b88ea12cf3bf2a9c8faba3262
Max Lincoln <max@devopsy.com>
2013-08-26 19:27:36 -0300

    Add json tests - only passing on yajl

---
+++ tests/core/json_tests.rb
@@ -0,0 +1,21 @@
+Shindo.tests('Fog::JSON', ['core']) do
+  tests('decode') do
+    tests('nil').raises(MultiJson::LoadError) do
+      Fog::JSON.decode(nil)
+    end
+    tests('object').raises(MultiJson::LoadError) do
+      Fog::JSON.decode(['a', 'b'])
+    end
+
+    tests('empty').returns(nil) do
+      Fog::JSON.decode("")
+    end
+    tests('{}').returns({}) do
+      Fog::JSON.decode('{}')
+    end
+    tests('[]').returns([]) do
+      Fog::JSON.decode('[]')
+    end
+  end
+end
+
