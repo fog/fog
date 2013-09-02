95b353ff7ff223895b61a3f02bcac5f4a67130de
Keith Duncan <keith.duncan@github.com>
2013-09-02 13:12:46 +0100

    Don’t escape '/' characters in keys
    Add test for this behaviour

--- tests/aws/signed_params_tests.rb
+++ tests/aws/signed_params_tests.rb
@@ -2,4 +2,8 @@
 
 Shindo.tests('AWS | signed_params', ['aws']) do
   returns( Fog::AWS.escape( "'Stöp!' said Fred_-~." ) ) { "%27St%C3%B6p%21%27%20said%20Fred_-~." }
+
+  tests('Keys can contain a hierarchical prefix which should not be escaped') do
+    returns( Fog::AWS.escape( "key/with/prefix" ) ) { "key/with/prefix" }
+  end
 end
