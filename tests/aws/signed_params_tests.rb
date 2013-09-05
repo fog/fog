9950015e579c42b237a6ca3d1d78152388698df4
Keith Duncan <keith.duncan@github.com>
2013-09-05 16:26:54 +0100

    Pack takes Unicode characters, not UTF-8 encodings

--- tests/aws/signed_params_tests.rb
+++ tests/aws/signed_params_tests.rb
@@ -8,10 +8,10 @@ Shindo.tests('AWS | signed_params', ['aws']) do
   end
 
   tests('Keys should be canonicalised using Unicode NFC') do
-    returns( Fog::AWS.escape( ["C3A9".to_i(16)].pack("U*") ) ) { "%C3%A9" }
+    returns( Fog::AWS.escape( ["00E9".to_i(16)].pack("U*") ) ) { "%C3%A9" }
 
     tests('Characters with combining mark should be combined and then escaped') do
-      returns( Fog::AWS.escape( ["0065".to_i(16), "CC81".to_i(16)].pack("U*") ) ) { "%C3%A9" }
+      returns( Fog::AWS.escape( ["0065".to_i(16), "0301".to_i(16)].pack("U*") ) ) { "%C3%A9" }
     end
   end
 end
