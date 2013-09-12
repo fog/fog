a47f11a7ec1f89c6c3874205cc46866d800d6774
Nick Osborn <nick.osborn@digital.cabinet-office.gov.uk>
2013-09-12 09:46:42 +0100

    [vcloud_director] Allow for multiple Orgs, rephrased.

--- lib/fog/vcloud_director/models/compute/organizations.rb
+++ lib/fog/vcloud_director/models/compute/organizations.rb
@@ -20,7 +20,7 @@ module Fog
         
         def item_list
           data = service.get_organizations.body
-          orgs = data[:Org].is_a?(Array) ? data[:Org] : [data[:Org]]
+          orgs = data[:Org].is_a?(Hash) ? [data[:Org]] : data[:Org]
           orgs.each {|org| service.add_id_from_href!(org)}
           orgs
         end
