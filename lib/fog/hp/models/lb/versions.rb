76ed7e87ec01027109860d215d09d9b39b627417
Terry Howe <thowe@hp.com>
2013-09-17 08:21:22 -0600

    Update versions.rb
    Change in LB API

--- lib/fog/hp/models/lb/versions.rb
+++ lib/fog/hp/models/lb/versions.rb
@@ -8,13 +8,13 @@ module Fog
         model Fog::HP::LB::Version
 
         def all
-          data = service.list_versions.body['version']
+          data = service.list_versions.body['versions']
           data = [data] unless data.kind_of? Array
           load(data)
         end
 
         def get(record_id)
-          record = service.get_versions(record_id).body['version']
+          record = service.get_versions(record_id).body['versions']
           new(record)
         rescue Fog::HP::LB::NotFound
           nil
