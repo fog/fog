873d195ddd8aa4874d892763adc5f8ade8210043
rsalm <rs@iki.fi>
2013-09-18 20:59:35 +0300

    [vcloud_director] Fix listing catalog items when only a single item exists.

--- lib/fog/vcloud_director/models/compute/catalog_items.rb
+++ lib/fog/vcloud_director/models/compute/catalog_items.rb
@@ -12,9 +12,13 @@ module Fog
 
         private
 
+        def ensure_list(items)
+          items.is_a?(Hash) ? [items] : items
+        end
+
         def item_list
           data = service.get_catalog(catalog.id).body
-          items = data[:CatalogItems][:CatalogItem].select { |link| link[:type] == "application/vnd.vmware.vcloud.catalogItem+xml" }
+          items = ensure_list(data[:CatalogItems][:CatalogItem]).select { |link| link[:type] == "application/vnd.vmware.vcloud.catalogItem+xml" }
           items.each{|item| service.add_id_from_href!(item) }
           items
         end
