113dadd48d23aceba84f5330a4629411f20832fa
Shawn Catanzarite <me@shawncatz.com>
2013-09-25 10:29:22 -0700

    remove find_by_name for now

--- lib/fog/dynect/models/dns/records.rb
+++ lib/fog/dynect/models/dns/records.rb
@@ -52,29 +52,6 @@ module Fog
           })
         end
 
-        def find_by_name(name)
-          requires :zone
-          data = []
-          service.get_all_records(zone.domain, options).body['data'].select{|url| url =~ /\/#{name}\//}.each do |url|
-            (_, _, t, _, fqdn, id) = url.split('/')
-            type = t.gsub(/Record$/, '')
-
-            # leave out the default, read only records
-            next if ['NS', 'SOA'].include?(type)
-
-            record = service.get_record(type, zone.domain, fqdn, 'record_id' => id).body['data']
-
-            data << {
-                :identity => record['record_id'],
-                :fqdn => record['fqdn'],
-                :type => record['record_type'],
-                :rdata => record['rdata']
-            }
-          end
-
-          load(data)
-        end
-
         def new(attributes = {})
           requires :zone
           super({:zone => zone}.merge!(attributes))
