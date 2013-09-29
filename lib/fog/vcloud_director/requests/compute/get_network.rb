62ba706273998e94c93e55ae31ae613605343b50
Nick Osborn <nick.osborn@digital.cabinet-office.gov.uk>
2013-09-29 21:27:58 +0100

    [vcloud_director] More mocking (1.8.7 compliant)

--- lib/fog/vcloud_director/requests/compute/get_network.rb
+++ lib/fog/vcloud_director/requests/compute/get_network.rb
@@ -22,7 +22,44 @@ module Fog
           )
         end
       end
+
+      class Mock
+        def get_network(network_id)
+          response = Excon::Response.new
+
+          unless valid_uuid?(network_id)
+            response.status = 400
+            raise Excon::Errors.status_error({:expect => 200}, response)
+          end
+          unless network = data[:networks][network_id]
+            response.status = 403
+            raise Excon::Errors.status_error({:expect => 200}, response)
+          end
+
+          body =
+            {:name=>network[:name],
+             :href=>make_href("network/#{network_id}"),
+             :type=>"application/vnd.vmware.vcloud.orgNetwork+xml",
+             :id=>network_id,
+             :description=>nil,
+             :is_inherited=>network[:IsInherited],
+             :gateway=>network[:Gateway],
+             :netmask=>network[:Netmask],
+             :dns1=>network[:Dns1],
+             :dns2=>network[:Dns2],
+             :dns_suffix=>network[:DnsSuffix]}
+
+          body[:ip_ranges] = network[:IpRanges].map do |ip_range|
+            {:start_address=>ip_range[:StartAddress],
+             :end_address=>ip_range[:EndAddress]}
+          end
+
+          response.status = 200
+          response.headers = {'Content-Type' => "#{body[:type]};version=#{api_version}"}
+          response.body = body
+          response
+        end
+      end
     end
   end
 end
-
--- tests/vcloud_director/requests/compute/vdc_tests.rb
+++ tests/vcloud_director/requests/compute/vdc_tests.rb
@@ -4,15 +4,17 @@ Shindo.tests('Compute::VcloudDirector | vdc requests', ['vclouddirector']) do
 
   tests('Get current organization') do
     session = @service.get_current_session.body
-    org_href = session[:Link].detect {|l| l[:type] == 'application/vnd.vmware.vcloud.org+xml'}[:href]
-    org_uuid = org_href.split('/').last
-    @org = @service.get_organization(org_uuid).body
+    link = session[:Link].detect do |l|
+      l[:type] == 'application/vnd.vmware.vcloud.org+xml'
+    end
+    @org = @service.get_organization(link[:href].split('/').last).body
   end
 
   tests('#get_vdc').data_matches_schema(VcloudDirector::Compute::Schema::VDC_TYPE) do
-    vdc_href = @org[:Link].detect {|l| l[:type] == 'application/vnd.vmware.vcloud.vdc+xml'}[:href]
-    vdc_uuid = vdc_href.split('/').last
-    vdc = @service.get_vdc(vdc_uuid).body
+    link = @org[:Link].detect do |l|
+      l[:rel] == 'down' && l[:type] == 'application/vnd.vmware.vcloud.vdc+xml'
+    end
+    vdc = @service.get_vdc(link[:href].split('/').last).body
     vdc[:AvailableNetworks][:Network] = [vdc[:AvailableNetworks][:Network]] if vdc[:AvailableNetworks][:Network].is_a?(Hash)
     vdc[:ResourceEntities][:ResourceEntity] = [vdc[:ResourceEntities][:ResourceEntity]] if vdc[:ResourceEntities][:ResourceEntity].is_a?(Hash)
     if vdc.has_key?(:VdcStorageProfiles)
