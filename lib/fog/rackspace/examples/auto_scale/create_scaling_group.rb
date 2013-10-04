238e0d98c662c49252ae73237d10911f7a071581
Kyle Rames <kyle.rames@rackspace.com>
2013-10-04 09:45:21 -0500

    [rackspace|auto_scale] update create scaling group to use GroupBuilder.

--- lib/fog/rackspace/examples/auto_scale/create_scaling_group.rb
+++ lib/fog/rackspace/examples/auto_scale/create_scaling_group.rb
@@ -3,6 +3,7 @@
 # This example demonstrates creating an auto scaling group with the Rackpace Open Cloud
 
 require 'fog'
+require 'fog/rackspace/models/auto_scale/group_builder'
 
 # UUID for INTERNET
 INTERNET = '00000000-0000-0000-0000-000000000000'
@@ -66,16 +67,13 @@ compute_service = Fog::Compute.new({
 scaling_group_name = get_user_input "Enter name of scaling group"
 
 # prompt for cool down period
-cool_down = get_user_input_as_int "Enter cool down period in seconds"
+cooldown = get_user_input_as_int "Enter cool down period in seconds"
 
 # prompt for miniumum number of entities
 min_entities = get_user_input_as_int "Enter minimum number of servers"
 
 # prompt for max number of entities
 max_entities = get_user_input_as_int "Enter maximum number of servers"
-
-# prompt for base name server name
-server_name = get_user_input "Enter base name for servers in scaling group '#{scaling_group_name}'"
 
 # retrieve list of images from computer service
 print "Loading available server images...."
@@ -88,45 +86,30 @@ image = select_image(images)
 # pick first server flavor
 flavor = compute_service.flavors.first
 
-server_template =   {
-  "name" => "autoscale_server",
-  "imageRef" => image.id,
-  "flavorRef" => flavor.id,
-  "OS-DCF =>diskConfig" => "MANUAL",
-  "metadata" => {
-    "build_config" => "core",
-    "meta_key_1" => "meta_value_1",
-    "meta_key_2" => "meta_value_2"
-  },
-  "networks" => [
-    {
-      "uuid" => "11111111-1111-1111-1111-111111111111"
-    },
-    {
-      "uuid" => "00000000-0000-0000-0000-000000000000"
-    }
-  ],
-  "personality" => [
+attributes = {
+  :server_name => "autoscale_server",
+  :image => image,
+  :flavor => flavor,
+  :networks => [INTERNET, SERVICE_NET],
+  :personality => [
     {
       "path" => "/root/.csivh",
       "contents" => "VGhpcyBpcyBhIHRlc3QgZmlsZS4="
     }
-  ]
-  }
-
-# create launch configuration
-launch_config = Fog::Rackspace::AutoScale::LaunchConfig.new :type => :launch_server, :args => {:server => server_template }
-
-# create group configuration
-group_config = Fog::Rackspace::AutoScale::GroupConfig.new :max_entities => max_entities,
+  ],
+  :max_entities => max_entities,
   :min_entities => min_entities,
-  :cooldown => cool_down,
+  :cooldown => cooldown,
   :name => scaling_group_name,
-  :metadata => { "created_by" => "autoscale sample script" }
+  :metadata => { "created_by" => "autoscale sample script" },
+  :launch_config_type => :launch_server
+}
 
-# create scaling group
-group = auto_scale_service.groups.create :group_config => group_config,
-  :launch_config => launch_config
+# Use builder to create group
+group = Fog::Rackspace::AutoScale::GroupBuilder.build(auto_scale_service, attributes)
+
+# save the built group
+group.save
 
 puts "\nScaling Group #{scaling_group_name} (#{group.id}) was created!"
 puts "State: #{group.state}"
