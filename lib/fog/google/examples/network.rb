ff2865516a08e39876bc1401896569251283d833
Nat Welch <nat@natwelch.com>
2013-10-04 15:31:14 -0700

    [google|compute] examples tweaks.

--- lib/fog/google/examples/network.rb
+++ lib/fog/google/examples/network.rb
@@ -18,4 +18,6 @@ def test
 
   # The network won't have any firewall rules, so we won't be able to ssh in.
   server.wait_for { ready? }
+
+  raise "Cloud not delete server." unless server.destroy
 end
