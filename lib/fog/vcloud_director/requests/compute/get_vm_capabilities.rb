bd01b4b55cf03c56abf466ab48c9c2696522836e
Nick Osborn <nick.osborn@digital.cabinet-office.gov.uk>
2013-10-10 07:27:34 +0100

    [vcloud_director] Yet more request methods.

--- lib/fog/vcloud_director/requests/compute/get_vm_capabilities.rb
+++ lib/fog/vcloud_director/requests/compute/get_vm_capabilities.rb
@@ -2,20 +2,19 @@ module Fog
   module Compute
     class VcloudDirector
       class Real
-        # Retrieve the runtime info section of a VM.
+        # Gets capabilities for the VM identified by id.
         #
         # @param [String] id Object identifier of the VM.
         # @return [Excon::Response]
         #   * body<~Hash>:
-        # @see http://pubs.vmware.com/vcd-51/topic/com.vmware.vcloud.api.reference.doc_51/doc/operations/GET-RuntimeInfoSectionType.html
-        # @since vCloud API version 1.5
-        def get_runtime_info_section_type(id)
+        # @see http://pubs.vmware.com/vcd-51/topic/com.vmware.vcloud.api.reference.doc_51/doc/operations/GET-VmCapabilities.html
+        def get_vm_capabilities(id)
           request(
             :expects    => 200,
             :idempotent => true,
             :method     => 'GET',
             :parser     => Fog::ToHashDocument.new,
-            :path       => "vApp/#{id}/runtimeInfoSection"
+            :path       => "vApp/#{id}/vmCapabilities"
           )
         end
       end
