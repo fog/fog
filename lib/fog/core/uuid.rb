bda470486a25bc529a17a8c3624412dcf3e4552a
Kyle Rames <kyle.rames@rackspace.com>
2013-10-11 07:51:23 -0500

    [core] updated UUID to use it's own UUID implementation if one was not available from the ruby lib.

--- lib/fog/core/uuid.rb
+++ lib/fog/core/uuid.rb
@@ -8,14 +8,16 @@ module Fog
         if supported?
           SecureRandom.uuid
         else
-          raise "UUID generation is not supported by your ruby implementation. Please try upgrading to Ruby 1.9.x."
+          ary = SecureRandom.random_bytes(16).unpack("NnnnnN")
+          ary[2] = (ary[2] & 0x0fff) | 0x4000
+          ary[3] = (ary[3] & 0x3fff) | 0x8000
+          "%08x-%04x-%04x-%04x-%04x%08x" % ary
         end
       end
 
       def supported?
         SecureRandom.respond_to?(:uuid)
       end
-
     end
   end
 end
