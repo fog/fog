2d777e27e127deeb7e0c1f4233a0d10de1ddd318
Sneha Somwanshi <sneha.vishwas-somwanshi@digital.cabinet-office.gov.uk>
2013-10-14 16:52:10 +0100

    broke generator code into smaller methods

--- lib/fog/vcloud_director/generators/compute/edge_gateway.rb
+++ lib/fog/vcloud_director/generators/compute/edge_gateway.rb
@@ -8,131 +8,145 @@ module Fog
           end
 
           def generate_xml
-            firewall_config = @configuration[:firewall_service]
-            nat_config = @configuration[:nat_service]
-            lb_config = @configuration[:load_balancer_service]
-            Nokogiri::XML::Builder.new(:encoding => 'UTF-8') do
-              EdgeGatewayServiceConfiguration(
-                  'xmlns' => "http://www.vmware.com/vcloud/v1.5",
-                  'xmlns:xsi' => "http://www.w3.org/2001/XMLSchema-instance",
-                  'xsi:schemaLocation' => "http://www.vmware.com/vcloud/v1.5 http://vendor-api-url.net/v1.5/schema/master.xsd"
-              ) {
-                FirewallService {
-                  IsEnabled 'true'
-                  DefaultAction firewall_config[:default_action] if firewall_config[:default_action]
-                  LogDefaultAction firewall_config[:log_default_action] if firewall_config[:log_default_action]
-                  firewall_config[:rules].each do |rule|
-                    FirewallRule {
-                      Id rule[:id]
-                      IsEnabled rule[:is_enabled] if rule[:is_enabled]
-                      MatchOnTranslate rule[:match_on_translate] if rule[:match_on_translate]
-                      Description rule[:description]
-                      Policy rule[:policy]
-
-                      Protocols {
-                        rule[:protocols].each do |protocol|
-                          send(protocol.to_s.capitalize, true)
-                        end
-                      }
-                      IcmpSubType "any" if rule[:protocols].include? :icmp
-                      Port rule[:destination][:port] == "Any" ? "-1" : rule[:destination][:port]
-                      DestinationPortRange rule[:destination][:port]
-                      DestinationIp rule[:destination][:ip]
-                      SourcePort rule[:source][:port] == "Any" ? "-1" : rule[:source][:port]
-                      SourcePortRange rule[:source][:port]
-                      SourceIp rule[:source][:ip]
-                    }
-
-                  end
-                } if firewall_config
-                NatService {
-                  IsEnabled nat_config[:is_enabled]
-
-                  nat_config[:rules].each do |rule|
-                    NatRule {
-                      RuleType rule[:rule_type]
-                      IsEnabled rule[:is_enabled]
-                      Id rule[:id]
-                      GatewayNatRule {
-                        Interface(:name => rule[:interface][:name], :href => rule[:interface][:href])
-                        OriginalIp rule[:original][:ip]
-                        OriginalPort rule[:original][:port] if rule[:original][:port]
-                        TranslatedIp rule[:translated][:ip]
-                        TranslatedPort rule[:translated][:port] if rule[:translated][:port]
-                        Protocol rule[:protocol] if rule[:rule_type] == "DNAT"
-                      }
-                    }
-                  end
-                } if nat_config
-
-                LoadBalancerService {
-                  IsEnabled lb_config[:is_enabled] if lb_config[:is_enabled]
-                  lb_config[:pools].each do |pool|
-                    Pool {
-                      Name pool[:name]
-
-                      pool[:service_ports].each do |service_port|
-                        ServicePort {
-                          IsEnabled service_port[:is_enabled]
-                          Protocol service_port[:protocol]
-                          Algorithm service_port[:algorithm]
-                          Port service_port[:port]
-                          HealthCheckPort service_port[:health_check_port]
-                          HealthCheck {
-                            Mode service_port[:health_check][:mode]  #tcp, http, ssl
-                            HealthThreshold service_port[:health_check][:health_threshold]
-                            UnhealthThreshold service_port[:health_check][:unhealth_threshold]
-                            Interval service_port[:health_check][:interval]
-                            Timeout service_port[:health_check][:timeout]
-                          }
-                        }
-                      end
-
-                      pool[:members].each do |member|
-                        Member {
-                          IpAddress member[:ip_address]
-                          Weight member[:weight]
-                          member[:service_ports].each do |member_service_port|
-                          ServicePort {
-                            Protocol member_service_port[:protocol]
-                            Port member_service_port[:port]
-                            HealthCheckPort member_service_port[:health_check_port]
-                          }
-                          end
-                        }
-                      end
-
-                    }
-                  end
-                  lb_config[:virtual_servers].each do |virtual_server|
-                    VirtualServer {
-                      IsEnabled virtual_server[:is_enabled]
-                      Name virtual_server[:name]
-                      Description virtual_server[:description]
-                      Interface(:href => virtual_server[:interface][:href], :name => virtual_server[:interface][:name] )
-                      IpAddress virtual_server[:ip_address]
-                      virtual_server[:service_profiles].each do |service_profile|
-                        ServiceProfile {
-                          IsEnabled service_profile[:is_enabled]
-                          Protocol service_profile[:protocol]
-                          Port service_profile[:port]
-                          Persistence {
-                            Method service_profile[:persistence][:method]
-                            if service_profile[:persistence][:method] == 'COOKIE'
-                              CookieName service_profile[:persistence][:cookie_name]
-                              CookieMode service_profile[:persistence][:cookie_mode]
-                            end
-                          }
-                        }
-                      end
-                      Logging virtual_server[:logging]
-                      Pool  virtual_server[:pool]
-                    }
-                  end
-                } if lb_config
-
+            Nokogiri::XML::Builder.new(:encoding => 'UTF-8') do |xml|
+              xml.EdgeGatewayServiceConfiguration('xmlns' => "http://www.vmware.com/vcloud/v1.5"){
+                build_firewall_service(xml)
+                build_nat_service(xml)
+                build_load_balancer_service(xml)
               }
             end.to_xml
+          end
+
+          private
+
+          def build_load_balancer_service(xml)
+            lb_config = @configuration[:load_balancer_service]
+            return unless lb_config
+
+            xml.LoadBalancerService {
+              xml.IsEnabled lb_config[:is_enabled] if lb_config[:is_enabled]
+              lb_config[:pools].each do |pool|
+                xml.Pool {
+                  xml.Name pool[:name]
+
+                  pool[:service_ports].each do |service_port|
+                    xml.ServicePort {
+                      xml.IsEnabled service_port[:is_enabled]
+                      xml.Protocol service_port[:protocol]
+                      xml.Algorithm service_port[:algorithm]
+                      xml.Port service_port[:port]
+                      xml.HealthCheckPort service_port[:health_check_port]
+                      xml.HealthCheck {
+                        xml.Mode service_port[:health_check][:mode] #tcp, http, ssl
+                        xml.HealthThreshold service_port[:health_check][:health_threshold]
+                        xml.UnhealthThreshold service_port[:health_check][:unhealth_threshold]
+                        xml.Interval service_port[:health_check][:interval]
+                        xml.Timeout service_port[:health_check][:timeout]
+                      }
+                    }
+                  end
+
+                  pool[:members].each do |member|
+                    xml.Member {
+                      xml.IpAddress member[:ip_address]
+                      xml.Weight member[:weight]
+                      member[:service_ports].each do |member_service_port|
+                        xml.ServicePort {
+                          xml.Protocol member_service_port[:protocol]
+                          xml.Port member_service_port[:port]
+                          xml.HealthCheckPort member_service_port[:health_check_port]
+                        }
+                      end
+                    }
+                  end
+
+                }
+              end
+              lb_config[:virtual_servers].each do |virtual_server|
+                xml.VirtualServer {
+                  xml.IsEnabled virtual_server[:is_enabled]
+                  xml.Name virtual_server[:name]
+                  xml.Description virtual_server[:description]
+                  xml.Interface(:href => virtual_server[:interface][:href], :name => virtual_server[:interface][:name])
+                  xml.IpAddress virtual_server[:ip_address]
+                  virtual_server[:service_profiles].each do |service_profile|
+                    xml.ServiceProfile {
+                      xml.IsEnabled service_profile[:is_enabled]
+                      xml.Protocol service_profile[:protocol]
+                      xml.Port service_profile[:port]
+                      xml.Persistence {
+                        xml.Method service_profile[:persistence][:method]
+                        if service_profile[:persistence][:method] == 'COOKIE'
+                          xml.CookieName service_profile[:persistence][:cookie_name]
+                          xml.CookieMode service_profile[:persistence][:cookie_mode]
+                        end
+                      }
+                    }
+                  end
+                  xml.Logging virtual_server[:logging]
+                  xml.Pool virtual_server[:pool]
+                }
+              end
+            }
+          end
+
+          def build_nat_service(xml)
+            nat_config = @configuration[:nat_service]
+            return unless nat_config
+
+            xml.NatService {
+              xml.IsEnabled nat_config[:is_enabled]
+
+              nat_config[:rules].each do |rule|
+                xml.NatRule {
+                  xml.RuleType rule[:rule_type]
+                  xml.IsEnabled rule[:is_enabled]
+                  xml.Id rule[:id]
+                  xml.GatewayNatRule {
+                    xml.Interface(:name => rule[:interface][:name], :href => rule[:interface][:href])
+                    xml.OriginalIp rule[:original][:ip]
+                    xml.OriginalPort rule[:original][:port] if rule[:original][:port]
+                    xml.TranslatedIp rule[:translated][:ip]
+                    xml.TranslatedPort rule[:translated][:port] if rule[:translated][:port]
+                    xml.Protocol rule[:protocol] if rule[:rule_type] == "DNAT"
+                  }
+                }
+              end
+            }
+          end
+
+          def build_firewall_service(xml)
+            firewall_config = @configuration[:firewall_service]
+            return unless firewall_config
+
+            xml.FirewallService {
+              xml.IsEnabled 'true'
+              xml.DefaultAction firewall_config[:default_action] if firewall_config[:default_action]
+              xml.LogDefaultAction firewall_config[:log_default_action] if firewall_config[:log_default_action]
+              firewall_config[:rules].each do |rule|
+                xml.FirewallRule {
+                  xml.Id rule[:id]
+                  xml.IsEnabled rule[:is_enabled] if rule[:is_enabled]
+                  xml.MatchOnTranslate rule[:match_on_translate] if rule[:match_on_translate]
+                  xml.Description rule[:description]
+                  xml.Policy rule[:policy]
+
+                  xml.Protocols {
+                    rule[:protocols].each do |protocol|
+                      xml.send(protocol.to_s.capitalize, true)
+                    end
+                  }
+                  xml.IcmpSubType "any" if rule[:protocols].include? :icmp
+                  xml.Port rule[:destination][:port] == "Any" ? "-1" : rule[:destination][:port]
+                  xml.DestinationPortRange rule[:destination][:port]
+                  xml.DestinationIp rule[:destination][:ip]
+                  xml.SourcePort rule[:source][:port] == "Any" ? "-1" : rule[:source][:port]
+                  xml.SourcePortRange rule[:source][:port]
+                  xml.SourceIp rule[:source][:ip]
+                }
+
+              end
+            }
           end
 
 
