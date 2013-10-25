49debe9146bc4c6369a3f55c47f4ce350953cd65
Kyle Rames <kyle.rames@rackspace.com>
2013-10-25 09:29:28 -0500

    [rackspace|identity] re-implementing service catalog in hopes of providing a faster more flexible solution

--- lib/fog/rackspace/models/identity/service_catalog.rb
+++ lib/fog/rackspace/models/identity/service_catalog.rb
@@ -8,36 +8,48 @@ module Fog
         
         def initialize(attributes)
           @service = attributes.delete(:service)
-          @catalog = {}
+          @catalog = attributes.delete(:catalog) || {}
         end
         
         def services
-          catalog.keys
+          catalog.collect {|s| s["name"]}
         end
         
         def get_endpoints(service_type)
-          service_type = service_type.is_a?(String) ? service_type.to_sym : service_type
-          catalog[service_type]
+          h = catalog.find {|service| service["name"] == service_type.to_s}
+          h ? h["endpoints"] : {}
         end
         
         def display_service_regions(service_type)
           endpoints = get_endpoints(service_type)
-          endpoints.collect { |k,v| ":#{k}" }.join(", ")
+          regions = endpoints.collect do |e|
+            e["region"] ? ":#{e["region"].downcase}" : ":global"
+          end
+          regions.join(", ")
         end
         
-        def get_endpoint(service_type, region=nil)
-          endpoint = get_endpoints(service_type)
-          raise "Unable to locate endpoint for service #{service_type}" unless endpoint
+        def get_endpoint(service_type, region=nil, service_net=nil)
+          service_region = region_key(region)
 
-          return endpoint if endpoint.is_a?(String) #There is only one endpoint for service
+          network_type = service_net ? "internalURL" : "publicURL"
 
-          unless region
+          endpoints = get_endpoints(service_type)
+          raise "Unable to locate endpoint for service #{service_type}" if endpoints.empty?
+
+          if endpoints.size > 1 && region.nil?
             raise "There are multiple endpoints available for #{service_type}. Please specify one of the following regions: #{display_service_regions(service_type)}."
           end
-          region = region.is_a?(String) ? region.to_sym : region
-          endpoint = get_endpoints(service_type)[region]
-          raise "Unknown region :#{region} for service #{service_type}. Please use one of the following regions: #{display_service_regions(service_type)}" unless endpoint
-          endpoint
+
+          # select multiple endpoints
+          endpoint = endpoints.find {|e| matching_region?(e, service_region) }
+          return endpoint[network_type] if endpoint && endpoint[network_type]
+
+          # endpoint doesnt have region
+          if endpoints.size == 1 && matching_region?(endpoints[0], "GLOBAL")
+            return endpoints[0][network_type]
+          end
+
+          raise "Unknown region :#{region} for service #{service_type}. Please use one of the following regions: #{display_service_regions(service_type)}"
         end
         
         def reload
@@ -47,52 +59,17 @@ module Fog
         end
                 
         def self.from_response(service, hash)
-          service_catalog = ServiceCatalog.new :service => service
-          begin
-            services = hash["access"]["serviceCatalog"]
-            services.each do |serv|
-              name = serv["name"] ? serv["name"].to_sym : nil
-              next unless name
-              service_catalog.send(:add_endpoints, name, serv)
-            end
-          rescue => e
-            Logger.warning "Exception occurred while loading service catalog - #{e.inspect}"
-            Logger.warning "Unable to load service catalog. Please specify endpoints manually"
-          end
-          service_catalog
+          ServiceCatalog.new :service => service, :catalog => hash["access"]["serviceCatalog"]
         end
 
         private
         
-        def add_endpoints(service_name, hash)
-          begin
-            endpoints = hash["endpoints"]
-            if endpoints.size == 1
-              catalog[service_name] = endpoints[0]["publicURL"].freeze
-            else
-              catalog[service_name] = endpoints_from_array(service_name, endpoints)
-            end
-          rescue => e
-            Logger.warning "Exception occurred while loading #{service_name} service endpoints - #{e.inspect}"
-            Logger.warning "Unable to load service endpoints for #{service_name}. Please specify #{service_name} endpoints manually."
-          end
+        def matching_region?(h, region)
+          region_key(h["region"]) == region
         end
         
-        def endpoints_from_array(service_name, endpoints)
-          hash = {}
-          endpoints.each do |endpoint|
-            begin
-              region_name = endpoint["region"]
-              region = region_name ? region_name.downcase.to_sym : :global
-              url = endpoint["publicURL"].freeze
-              hash[region] = url
-            rescue => e
-              Logger.warning "Exception occurred while loading #{service_name} service endpoints - #{e.inspect}"
-              Logger.warning "Unable to parse #{service_name} endpoint - #{endpoint}"
-              Logger.warning "You may need to specify #{service_name} endpoints manually."
-            end
-          end
-          hash
+        def region_key(region)
+          (region.nil? || region.empty?) ? "GLOBAL" : region.to_s.upcase
         end
         
       end
