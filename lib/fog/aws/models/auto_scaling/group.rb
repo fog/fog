fog/fog

commit ef47f6316ead386f9274f5c4bc4f828946e0ef54

Use ruby-style attribute setters in AutoScaling::Group initializer

lib/fog/aws/models/auto_scaling/group.rb
@@ -25,18 +25,18 @@ module Fog
         attribute :vpc_zone_identifier,       :aliases => 'VPCZoneIdentifier'
 
         def initialize(attributes={})
-          attributes['DefaultCooldown'] ||= 300
-          attributes['DesiredCapacity'] ||= 0
-          attributes['EnabledMetrics'] ||= []
-          attributes['HealthCheckGracePeriod'] ||= 0
-          attributes['HealthCheckType'] ||= 'EC2'
-          attributes['Instances'] ||= []
-          attributes['LoadBalancerNames'] ||= []
-          attributes['MaxSize'] ||= 0
-          attributes['MinSize'] ||= 0
-          attributes['SuspendedProcesses'] ||= []
-          attributes['Tags'] ||= []
-          attributes['TerminationPolicies'] ||= ['Default']
+          attributes['Instances'] = []
+          self.default_cooldown = 300
+          self.desired_capacity = 0
+          self.enabled_metrics = []
+          self.health_check_grace_period = 0
+          self.health_check_type = 'EC2'
+          self.load_balancer_names = []
+          self.max_size = 0
+          self.min_size = 0
+          self.suspended_processes = []
+          self.tags = []
+          self.termination_policies = ['Default']
           super
         end
 

tests/aws/models/auto_scaling/groups_test.rb
@@ -18,5 +18,10 @@ Shindo.tests('AWS::AutoScaling | group', ['aws', 'auto_scaling_m']) do
   model_tests(Fog::AWS[:auto_scaling].groups, params, true) do
     @instance.update
   end
+  
+  test("setting attributes in the constructor") do
+    group = Fog::AWS[:auto_scaling].groups.new(min_size: 1, max_size: 2)
+    group.min_size == 1 && group.max_size == 2
+  end
 
 end

commit f1d53271dbad80ee2fd54669f6ce98f31b2d9f1e

Remove override of instances instance-setting in autoscale groups
Fixes issue introduced in ef47f6316ead386f9274f5c4bc4f828946e0ef54

lib/fog/aws/models/auto_scaling/group.rb
@@ -25,7 +25,7 @@ module Fog
         attribute :vpc_zone_identifier,       :aliases => 'VPCZoneIdentifier'
 
         def initialize(attributes={})
-          attributes['Instances'] = []
+          attributes['Instances'] ||= []
           self.default_cooldown = 300
           self.desired_capacity = 0
           self.enabled_metrics = []