09ab9fecd4a75849d1960d81f2c0179484c44b91
Kevin Menard <nirvdrum@gmail.com>
2013-11-01 14:19:11 -0400

    [vsphere|compute] Renamed 'vm' to 'server' to be more in line with other fog providers.

--- lib/fog/vsphere/models/compute/interfaces.rb
+++ lib/fog/vsphere/models/compute/interfaces.rb
@@ -9,27 +9,27 @@ module Fog
 
         model Fog::Compute::Vsphere::Interface
 
-        attr_accessor :vm
+        attr_accessor :server
 
         def all(filters = {})
-          requires :vm
-          case vm
+          requires :server
+          case server
             when Fog::Compute::Vsphere::Server
-              load service.list_vm_interfaces(vm.id)
+              load service.list_vm_interfaces(server.id)
             when Fog::Compute::Vsphere::Template
-              load service.list_template_interfaces(vm.id)
+              load service.list_template_interfaces(server.id)
             else
             raise 'interfaces should have vm or template'
           end
         end
 
         def get(id)
-          requires :vm
-          case vm
+          requires :server
+          case server
             when Fog::Compute::Vsphere::Server
-              interface=service.get_vm_interface(vm.id, :key => id, :mac=> id, :name => id)
+              interface=service.get_vm_interface(server.id, :key => id, :mac=> id, :name => id)
             when Fog::Compute::Vsphere::Template
-              interface=service.get_template_interfaces(vm.id, :key => id, :mac=> id, :name => id)
+              interface=service.get_template_interfaces(server.id, :key => id, :mac=> id, :name => id)
             else
             raise 'interfaces should have vm or template'
           end
