62e129d3eaaf43c894b60bf45cdf59c3a4070c38
Matheus Mina <mfbmina@gmail.com>
2013-11-29 16:31:31 -0200

    changed shutdown to reboot in reboot server method

--- lib/fog/xenserver/requests/compute/reboot_server.rb
+++ lib/fog/xenserver/requests/compute/reboot_server.rb
@@ -5,7 +5,7 @@ module Fog
       class Real
         
         def reboot_server( ref, stype = 'clean' )
-          @connection.request({:parser => Fog::Parsers::XenServer::Base.new, :method => "VM.#{stype}_shutdown"}, ref)
+          @connection.request({:parser => Fog::Parsers::XenServer::Base.new, :method => "VM.#{stype}_reboot"}, ref)
         end
         
       end
