19ef48a29b11b386a5e997c1b46ca0cfe07de608
Joe Yates <joe.g.yates@gmail.com>
2013-12-05 14:16:00 +0100

    Give 'versionId' value the expected structure

--- tests/aws/requests/storage/delete_multiple_objects_tests.rb
+++ tests/aws/requests/storage/delete_multiple_objects_tests.rb
@@ -2,10 +2,11 @@ Shindo.tests('AWS::Storage | delete_multiple_objects', ['aws']) do
   @directory = Fog::Storage[:aws].directories.create(:key => 'fogobjecttests-' + Time.now.to_i.to_s(32))
 
   tests("doesn't alter options") do
-    options = {:quiet => true, 'versionId' => {'fog_object' => '12345'}}
+    version_id = {'fog_object' => ['12345']}
+    options = {:quiet => true, 'versionId' => version_id}
     Fog::Storage[:aws].delete_multiple_objects(@directory.identity, ['fog_object'], options)
 
     test(":quiet is unchanged") { options[:quiet] }
-    test("'versionId' is unchanged") { options['versionId'] == '12345' }
+    test("'versionId' is unchanged") { options['versionId'] == version_id }
   end
 end
