fc8ec4e6e83a4788d79caf951723dcf2c859dccf
Kevin Olbrich <kolbrich@6fusion.com>
2013-12-12 18:25:31 +0000

    metrics and fields

--- lib/fog/joyent/models/analytics/metric.rb
+++ lib/fog/joyent/models/analytics/metric.rb
@@ -4,8 +4,13 @@ module Fog
   module Joyent
     class Analytics
       class Metric < Fog::Model
-        attribute :name
+        attribute :module
+        attribute :stat
         attribute :label
+        attribute :interval
+        attribute :fields
+        attribute :unit
+        attribute :type
 
       end
     end
