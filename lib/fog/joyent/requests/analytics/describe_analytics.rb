55d328b28377548d9b751d67701cd8e93e96363b
Kevin Olbrich <kolbrich@6fusion.com>
2013-12-12 17:17:01 +0000

    describe analytics support

---
+++ lib/fog/joyent/requests/analytics/describe_analytics.rb
@@ -0,0 +1,15 @@
+module Fog
+  module Joyent
+    class Analytics
+      class Real
+        def describe_analytics
+          request(
+              :path => "#{@joyent_username}/analytics",
+              :method => "GET",
+              :expects => 200
+          )
+        end
+      end
+    end
+  end
+end
