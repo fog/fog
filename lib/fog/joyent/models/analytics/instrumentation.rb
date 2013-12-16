3b2a7ca5ee17c582f00378c3031f511ccc9c2b30
Kevin Olbrich <kolbrich@6fusion.com>
2013-12-16 19:11:07 +0000

    map internal attribute names to those used on the remote service before making api call to persist the data.

--- lib/fog/joyent/models/analytics/instrumentation.rb
+++ lib/fog/joyent/models/analytics/instrumentation.rb
@@ -16,14 +16,24 @@ module Fog
         attribute :idle_max, aliases: 'idle-max', :type => :integer
         attribute :transformations, :type => :array
         attribute :persist_data, aliases: 'persist-data', :type => :boolean
-        attribute :crtime, :type => :integer
+        attribute :crtime
         attribute :value_scope, aliases: 'value-scope'
         attribute :uris, :type => :array
+
+        def crtime=(new_crtime)
+          attributes[:crtime] = Time.at(new_crtime.to_i / 1000)
+        end
 
         def save
           requires :joyent_module, :stat
           munged_attributes = attributes.dup
           munged_attributes[:module] = munged_attributes.delete(:joyent_module)
+          munged_attributes[:'value-dimension'] = munged_attributes.delete(:value_dimension) || (self.decomposition.size + 1)
+          munged_attributes[:'value-arity'] = munged_attributes.delete(:value_arity) || 'scalar'
+          munged_attributes[:'retention-time'] = munged_attributes.delete(:retention_time) || 600
+          munged_attributes[:'idle-max'] = munged_attributes.delete(:idle_max) || 3600
+          munged_attributes[:'persist-data'] = munged_attributes.delete(:persist_data) || false
+          munged_attributes[:'value-scope'] = munged_attributes.delete(:value_scope) || 'interval'
           data = service.create_instrumentation(munged_attributes)
           merge_attributes(data.body)
           true
