471599d7c07b1542216529b8ed8ac72c173181f6
Alexander Lomov <lomov.as@gmail.com>
2013-12-26 14:20:33 +0300

    fix block sequence for server tests for google

--- tests/google/models/compute/server_tests.rb
+++ tests/google/models/compute/server_tests.rb
@@ -3,29 +3,31 @@ Shindo.tests("Fog::Compute[:google] | server model", ['google']) do
   @zone = 'us-central1-a'
   @disk = create_test_disk(Fog::Compute[:google], @zone)
 
-  model_tests(Fog::Compute[:google].servers, {:name => 'fogservername', :zone_name => @zone, :machine_type => 'n1-standard-1', :disks => [@disk]})
+  server_tests(Fog::Compute[:google], {:name => 'fogservername', :zone_name => @zone, :machine_type => 'n1-standard-1', :disks => [@disk]}) do 
+    tests('servers') do
+      @instance = nil
+      test('#bootstrap') do
+        attributes = Fog.mocking? ? {:public_key_path => nil, :private_key_path => nil} : {}
+        @instance = Fog::Compute[:google].servers.bootstrap(attributes)
+        @instance.ready?
+      end
 
-  tests('servers') do
-    @instance = nil
-    test('#bootstrap') do
-      attributes = Fog.mocking? ? {:public_key_path => nil, :private_key_path => nil} : {}
-      @instance = Fog::Compute[:google].servers.bootstrap(attributes)
-      @instance.ready?
-    end
+      test('#sshable?') do
+        @instance.wait_for { sshable? }
+        @instance.sshable?
+      end
 
-    test('#sshable?') do
-      @instance.wait_for { sshable? }
-      @instance.sshable?
-    end
+      test('#ssh') do
+        pending if Fog.mocking?
+        @instance.ssh("uname") == "Linux"
+      end
 
-    test('#ssh') do
-      pending if Fog.mocking?
-      @instance.ssh("uname") == "Linux"
-    end
-
-    test('#destroy') do
-      response = @instance.destroy
-      response.body['operationType'] == 'delete'
+      test('#destroy') do
+        response = @instance.destroy
+        response.body['operationType'] == 'delete'
+      end
     end
   end
+# asdas
+
 end
