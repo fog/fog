25c518cbc2e6bce3064960c54d1e8ede0cd8c4aa
Dave Donahue <dave@12spokes.com>
2013-12-27 10:03:22 -0600

    [digitalocean|compute] additional test coverage and some maintenance
    * Add created_at field to mock response
    * Assert that the created_at field is returned
    * Update references to nonexistent images

--- tests/digitalocean/requests/compute/create_server_tests.rb
+++ tests/digitalocean/requests/compute/create_server_tests.rb
@@ -13,7 +13,7 @@ Shindo.tests('Fog::Compute[:digitalocean] | create_server request', ['digitaloce
   tests('success') do
 
     tests('#create_server').formats({'status' => 'OK', 'droplet' => @server_format}) do
-      image = service.images.find { |img| img.name == 'Ubuntu 12.04 x64' }
+      image = service.images.find { |img| img.name == 'Ubuntu 13.10 x64' }
       flavor = service.flavors.find { |f| f.name == '512MB' }
       data = Fog::Compute[:digitalocean].create_server fog_server_name,
                                                        flavor.id,
