0bf0c854252b9832d9f0e86e6fa8e043d6ce8d53
Sneha Somwanshi <sneha.vishwas-somwanshi@digital.cabinet-office.gov.uk>
2014-01-06 17:36:42 +0000

    [vcloud_director] making xml generators reusable

--- lib/fog/vcloud_director/requests/compute/put_cpu.rb
+++ lib/fog/vcloud_director/requests/compute/put_cpu.rb
@@ -5,6 +5,8 @@ module Fog
         extend Fog::Deprecation
         deprecate :put_vm_cpu, :put_cpu
 
+        require 'fog/vcloud_director/generators/compute/cpu_item'
+
         # Update the RASD item that specifies CPU properties of a VM.
         #
         # This operation is asynchronous and returns a task that you can
@@ -18,19 +20,7 @@ module Fog
         # @see http://pubs.vmware.com/vcd-51/topic/com.vmware.vcloud.api.reference.doc_51/doc/operations/PUT-Cpu.html
         # @since vCloud API version 0.9
         def put_cpu(id, num_cpus)
-          data = <<EOF
-          <Item xmlns="http://www.vmware.com/vcloud/v1.5" xmlns:rasd="http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/CIM_ResourceAllocationSettingData" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:ns12="http://www.vmware.com/vcloud/v1.5" ns12:href="#{end_point}vApp/#{id}/virtualHardwareSection/cpu" ns12:type="application/vnd.vmware.vcloud.rasdItem+xml" xsi:schemaLocation="http://www.vmware.com/vcloud/v1.5 http://10.194.1.65/api/v1.5/schema/master.xsd http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/CIM_ResourceAllocationSettingData http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2.22.0/CIM_ResourceAllocationSettingData.xsd">
-            <rasd:AllocationUnits>hertz * 10^6</rasd:AllocationUnits>
-            <rasd:Description>Number of Virtual CPUs</rasd:Description>
-            <rasd:ElementName>#{num_cpus} virtual CPU(s)</rasd:ElementName>
-            <rasd:InstanceID>4</rasd:InstanceID>
-            <rasd:Reservation>0</rasd:Reservation>
-            <rasd:ResourceType>3</rasd:ResourceType>
-            <rasd:VirtualQuantity>#{num_cpus}</rasd:VirtualQuantity>
-            <rasd:Weight>0</rasd:Weight>
-            <Link rel="edit" type="application/vnd.vmware.vcloud.rasdItem+xml" href="#{end_point}vApp/#{id}/virtualHardwareSection/cpu"/>
-          </Item>
-EOF
+          data = Fog::Generators::Compute::VcloudDirector::CpuItem.new.generate_xml(id, num_cpus, end_point)
 
           request(
             :body    => data,
