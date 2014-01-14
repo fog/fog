fdedb5d00e33f319aacaa1afe1cd15ffc2bbb565
Carlos Sanchez <csanchez@maestrodev.com>
2014-01-14 12:04:06 +0100

    Fix typos that make ruby 1.8 break

--- lib/fog/google/requests/compute/set_metadata.rb
+++ lib/fog/google/requests/compute/set_metadata.rb
@@ -25,7 +25,7 @@ module Fog
           result = self.build_result(
             api_method,
             parameters,
-            body_object=body_object,
+            body_object=body_object
           )
           response = self.build_response(result)
         end
