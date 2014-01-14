7d6e56f73ee9f93604d3d7fee1a04171a36e08d9
Alexander Lomov <lomov.as@gmail.com>
2014-01-14 14:04:52 +0300

    fix disk names to have no interseptions with disk names that was left after they exited with exceptions

--- tests/google/models/compute/disks_tests.rb
+++ tests/google/models/compute/disks_tests.rb
@@ -1,5 +1,7 @@
 Shindo.tests("Fog::Compute[:google] | disks", ['google']) do
 
-  collection_tests(Fog::Compute[:google].disks, {:name => 'fogdiskname', :zone_name => 'us-central1-a', :size_gb => 10})
+  disk_name = 'fog-test-disk-' + Time.now.to_i.to_s
+
+  collection_tests(Fog::Compute[:google].disks, {:name => disk_name, :zone_name => 'us-central1-a', :size_gb => 10})
 
 end
