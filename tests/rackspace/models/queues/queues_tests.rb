f32645b1a8ef96d569ea4a073216aae5f240fb36
Ash Wilson <ash.wilson@rackspace.com>
2014-01-20 17:37:12 -0500

    Er, actually enable queues_tests, too.

--- tests/rackspace/models/queues/queues_tests.rb
+++ tests/rackspace/models/queues/queues_tests.rb
@@ -4,5 +4,5 @@ Shindo.tests('Fog::Rackspace::Queues | queues', ['rackspace']) do
   options = {
     :name => "fog_instance_#{Time.now.to_i.to_s}",
   }
-  collection_tests(service.queues, options, false)
+  collection_tests(service.queues, options)
 end
