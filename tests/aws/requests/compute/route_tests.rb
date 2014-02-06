e7bb96fbb26d00752ef9a31e48372383a97a9c47
Eric Herot <eric.github@herot.com>
2014-02-06 14:20:00 -0500

    Switch to hashed parameters method for handling replace_route arguments

--- tests/aws/requests/compute/route_tests.rb
+++ tests/aws/requests/compute/route_tests.rb
@@ -98,18 +98,18 @@ Shindo.tests('Fog::Compute[:aws] | route table requests', ['aws']) do
     #   - using network interface
     #
     Fog::Compute[:aws].attach_internet_gateway(@alt_internet_gateway_id, vpc.id).body
-    tests("#replace_route('#{@route_table_id}', '#{@destination_cidr_block}', '#{@alt_internet_gateway_id}', 'nil')").formats(AWS::Compute::Formats::BASIC) do
-      Fog::Compute[:aws].replace_route(@route_table_id, @destination_cidr_block, @alt_internet_gateway_id, nil).body
+    tests("#replace_route('#{@route_table_id}', {'destinationCidrBlock' => '#{@destination_cidr_block}', 'gatewayId' => '#{@alt_internet_gateway_id}'})").formats(AWS::Compute::Formats::BASIC) do
+      Fog::Compute[:aws].replace_route(@route_table_id, {'destinationCidrBlock' => @destination_cidr_block, 'gatewayId' => @alt_internet_gateway_id}).body
     end
 
     instance = Fog::Compute[:aws].servers.create(:image_id => @ami, :flavor_id => 't1.micro', :key_name => 'fog-test-key', :subnet_id => @subnet_id)
     instance.wait_for { state.eql? "running" }
-    tests("#replace_route('#{@route_table_id}', '10.0.10.0/22', 'nil', '#{instance.id}')").formats(AWS::Compute::Formats::BASIC) do
-      Fog::Compute[:aws].replace_route(@route_table_id, '10.0.10.0/22', nil, instance.id).body
+    tests("#replace_route('#{@route_table_id}', {'destinationCidrBlock' => '10.0.10.0/22', 'instanceId' => '#{instance.id}'})").formats(AWS::Compute::Formats::BASIC) do
+      Fog::Compute[:aws].replace_route(@route_table_id, {'destinationCidrBlock' => '10.0.10.0/22', 'instanceId' => instance.id}).body
     end
 
-    tests("#replace_route('#{@route_table_id}', '10.0.10.0/21', 'nil', 'nil', '#{@network_interface_id}')").formats(AWS::Compute::Formats::BASIC) do
-      Fog::Compute[:aws].replace_route(@route_table_id, '10.0.10.0/21', nil, nil, @network_interface_id).body
+    tests("#replace_route('#{@route_table_id}', {'destinationCidrBlock' => '10.0.10.0/21', 'networkInterfaceId' => '#{@network_interface_id}'})").formats(AWS::Compute::Formats::BASIC) do
+      Fog::Compute[:aws].replace_route(@route_table_id, {'destinationCidrBlock' => '10.0.10.0/21', 'networkInterfaceId' => @network_interface_id}).body
     end
 
     # Tests describe_route_tables
@@ -246,30 +246,30 @@ Shindo.tests('Fog::Compute[:aws] | route table requests', ['aws']) do
     tests('#replace_route').raises(ArgumentError) do
       Fog::Compute[:aws].replace_route
     end
-    tests("#replace_route('rtb-00000000', '#{@destination_cidr_block}', '#{@internet_gateway_id}')").raises(Fog::Compute::AWS::NotFound) do
-      Fog::Compute[:aws].replace_route('rtb-00000000', @destination_cidr_block, @internet_gateway_id)
+    tests("#replace_route('rtb-00000000', {'destinationCidrBlock' => '#{@destination_cidr_block}', 'internetGatewayId' => '#{@internet_gateway_id}'})").raises(Fog::Compute::AWS::NotFound) do
+      Fog::Compute[:aws].replace_route('rtb-00000000', {'destinationCidrBlock' => @destination_cidr_block, 'internetGatewayId' => @internet_gateway_id})
     end
-    tests("#replace_route('rtb-00000000', '#{@destination_cidr_block}')").raises(Fog::Compute::AWS::NotFound) do
-      Fog::Compute[:aws].replace_route('rtb-00000000', @destination_cidr_block)
+    tests("#replace_route('rtb-00000000', {'destinationCidrBlock' => '#{@destination_cidr_block}'})").raises(Fog::Compute::AWS::NotFound) do
+      Fog::Compute[:aws].replace_route('rtb-00000000', {'destinationCidrBlock' => @destination_cidr_block})
     end
-    tests("#replace_route('#{@route_table_id}', '#{@destination_cidr_block}', 'igw-00000000')").raises(Fog::Compute::AWS::NotFound) do
-      Fog::Compute[:aws].replace_route(@route_table_id, @destination_cidr_block, 'igw-00000000')
+    tests("#replace_route('#{@route_table_id}', {'destinationCidrBlock' => '#{@destination_cidr_block}', 'gatewayId' => 'igw-00000000'})").raises(Fog::Compute::AWS::NotFound) do
+      Fog::Compute[:aws].replace_route(@route_table_id, {'destinationCidrBlock' => @destination_cidr_block, 'gatewayId' => 'igw-00000000'})
     end
-    tests("#replace_route('rtb-00000000', '#{@destination_cidr_block}', 'nil', '#{instance.id}')").raises(Fog::Compute::AWS::NotFound) do
-      Fog::Compute[:aws].replace_route('rtb-00000000', @destination_cidr_block, instance.id)
+    tests("#replace_route('rtb-00000000', {'destinationCidrBlock' => '#{@destination_cidr_block}', 'instanceId' => '#{instance.id}'})").raises(Fog::Compute::AWS::NotFound) do
+      Fog::Compute[:aws].replace_route('rtb-00000000', {'destinationCidrBlock' => @destination_cidr_block, 'instanceId' => instance.id})
     end
-    tests("#replace_route('#{@route_table_id}', '#{@destination_cidr_block}', 'nil', 'i-00000000')").raises(Fog::Compute::AWS::NotFound) do
-      Fog::Compute[:aws].replace_route(@route_table_id, @destination_cidr_block, nil, 'i-00000000')
+    tests("#replace_route('#{@route_table_id}', {'destinationCidrBlock' => '#{@destination_cidr_block}', 'instanceId' => 'i-00000000'})").raises(Fog::Compute::AWS::NotFound) do
+      Fog::Compute[:aws].replace_route(@route_table_id, {'destinationCidrBlock' => @destination_cidr_block, 'instanceId' => 'i-00000000'})
     end
-    tests("#replace_route('#{@route_table_id}', '#{@destinationCidrBlock}', 'nil', 'nil', 'eni-00000000')").raises(Fog::Compute::AWS::NotFound) do
-      Fog::Compute[:aws].replace_route(@route_table_id, @destination_cidr_block, nil, nil, 'eni-00000000')
+    tests("#replace_route('#{@route_table_id}', {'destinationCidrBlock' => '#{@destinationCidrBlock}', 'networkInterfaceId' => 'eni-00000000'})").raises(Fog::Compute::AWS::NotFound) do
+      Fog::Compute[:aws].replace_route(@route_table_id, {'destinationCidrBlock' => @destination_cidr_block, 'networkInterfaceId' => 'eni-00000000'})
     end
-    tests("#replace_route('#rtb-00000000', '#{@destination_cidr_block}', 'nil, 'nil', '#{@network_interface_id}')").raises(Fog::Compute::AWS::NotFound) do
-      Fog::Compute[:aws].replace_route('rtb-00000000', @destination_cidr_block, nil, nil, @network_interface_id)
+    tests("#replace_route('#rtb-00000000', {'destinationCidrBlock' => '#{@destination_cidr_block}', 'networkInterfaceId' => '#{@network_interface_id}'})").raises(Fog::Compute::AWS::NotFound) do
+      Fog::Compute[:aws].replace_route('rtb-00000000', {'destinationCidrBlock' => @destination_cidr_block, 'networkInterfaceId' => @network_interface_id})
     end
     if !Fog.mocking?
-      tests("#create_route less specific destination_cidr_block").raises(Fog::Compute::AWS::Error) do
-        Fog::Compute[:aws].replace_route(@route_table_id, '10.0.10.0/25', @internet_gateway_id)
+      tests("#replace_route less specific destination_cidr_block").raises(Fog::Compute::AWS::Error) do
+        Fog::Compute[:aws].replace_route(@route_table_id, {'destinationCidrBlock' => '10.0.10.0/25', 'gatewayId' => @internet_gateway_id})
       end
     end
 
