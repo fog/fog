38fc98001683eeee5c1842e0d45b830bf594671b
radekg <radek@gruchalski.com>
2014-02-18 16:07:19 +0100

    [aws|region validation] Error message of the test to match error of the validator.

--- tests/aws/requests/compute/region_tests.rb
+++ tests/aws/requests/compute/region_tests.rb
@@ -20,7 +20,7 @@ Shindo.tests('Fog::Compute[:aws] | region requests', ['aws']) do
 
     tests("#incorrect_region") do
 
-      raises(ArgumentError, "Incorrect region world-antarctica-1") do
+      raises(ArgumentError, "Unknown region: world-antarctica-1") do
         Fog::Compute::AWS.new({:aws_access_key_id => 'dummykey', 
                 :aws_secret_access_key => 'dummysecret', 
                 :aws_session_token => 'dummytoken',
