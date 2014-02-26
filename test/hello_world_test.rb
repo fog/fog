18814bbefaf5915566270e11c674c45b3d938d5d
Paul Thornthwaite <tokengeek@gmail.com>
2014-02-26 22:49:41 +0000

    [GH-2630] Bring in Minitest
    This adds Minitest as a dependency and replaces the old `rake test`
    by a Rake test runner.
    
    Travis now includes the Minitest run before the Shindo run

---
+++ test/hello_world_test.rb
@@ -0,0 +1,8 @@
+require "minitest/autorun"
+
+class HelloWorldTest < Minitest::Test
+  # This is a placeholder to ensure minitest is being picked up by rake
+  def test_truth
+    assert true
+  end
+end
