47f17133310ba00ba422141ad2026ea25b05a953
Benson Kalahar <bensonk42@gmail.com>
2014-03-20 01:54:30 -0700

    Cleans up load balancer example

--- lib/fog/google/examples/load-balance.rb
+++ lib/fog/google/examples/load-balance.rb
@@ -9,56 +9,68 @@ def test
   # Setup
   gce = Fog::Compute.new provider: 'Google'
   servers = []
-  disks = []
 
   (1..3).each do |i|
-    puts "Creating disk #{name}-#{i}"
-    disk = gce.disks.create(
-      name: "#{name}-#{i}",
-      size_gb: 10,
-      auto_delete: true,
-      zone_name: zone,
-      source_image: 'debian-7-wheezy-v20131120'
-    )
-    disk.wait_for { disk.ready? }
-    disk.reload
-    disks << disk
+    begin
+      disk = gce.disks.create(
+        name: "#{name}-#{i}",
+        size_gb: 10,
+          zone_name: zone,
+          source_image: 'debian-7-wheezy-v20131120'
+      )
+      disk.wait_for { disk.ready? }
+      disk.reload
+    rescue
+      puts "Failed to create disk #{name}-#{i}"
+    end
   
-    puts "Creating server #{name}-#{i}"
-    server = gce.servers.create(
-      name: "#{name}-#{i}",
-      disks: [disk],
-      machine_type: 'f1-micro',
-      zone_name: zone
-    )
-    servers << server
+    begin
+      server = gce.servers.create(
+        name: "#{name}-#{i}",
+        disks: [ disk.get_as_boot_disk(true, true) ],
+          machine_type: 'f1-micro',
+          zone_name: zone
+      )
+      servers << server
+    rescue
+      puts "Failed to create instance #{name}-#{i}"
+    end
   end
 
-  puts "Creating health check #{name}"
-  health = gce.http_health_checks.new(name: name)
-  health.save
-  health.reload
+  begin
+    health = gce.http_health_checks.new(name: name)
+    health.save
+    health.reload
+  rescue
+    puts "Failed to create health check #{name}"
+  end
 
-  puts "Creating health check #{name}"
-  pool = gce.target_pools.new(
-    name: name,
-    region: region,
-    health_checks: health.self_link,
-    instances: servers.map(&:self_link)
-  )
-  pool.save
-  pool.reload
+  begin
+    pool = gce.target_pools.new(
+      name: name,
+      region: region,
+      health_checks: health.self_link,
+      instances: servers.map(&:self_link)
+    )
+    pool.save
+    pool.reload
+  rescue
+    puts "Failed to create target pool #{name}"
+  end
 
-  puts "Creating forwarding rule #{name}"
-  rule = gce.forwarding_rules.new(
-    name: name,
-    region: region,
-    port_range: '1-65535',
-    ip_protocol: 'TCP',
-    target: pool.self_link
-  )
-  rule.save
-  rule.reload
+  begin
+    rule = gce.forwarding_rules.new(
+      name: name,
+      region: region,
+      port_range: '1-65535',
+      ip_protocol: 'TCP',
+      target: pool.self_link
+    )
+    rule.save
+    rule.reload
+  rescue
+    puts "Failed to create forwarding rule #{name}"
+  end
 
   
   # TODO(bensonk): Install apache, create individualized htdocs, and run some
@@ -66,16 +78,27 @@ def test
 
 
   # Cleanup
-  puts 'Destroying forwarding rule'
-  rule.destroy
-  puts 'Destroying target pool'
-  pool.destroy
-  puts 'Destroying health check'
-  health.destroy
-  puts 'Destroying servers and disks'
-  servers.each &:destroy
+  begin
+    rule.destroy
+  rescue
+    puts "Failed to clean up forwarding rule."
+  end
 
-  # TODO(bensonk): make auto_delete work correctly. This is super pathetic.
-  sleep 120
-  disks.each &:destroy
+  begin
+    pool.destroy
+  rescue
+    puts "Failed to clean up target pool."
+  end
+
+  begin
+    health.destroy
+  rescue
+    puts "Failed to clean up health check."
+  end
+
+  begin
+    servers.each &:destroy
+  rescue
+    puts "Failed to clean up instances."
+  end
 end
