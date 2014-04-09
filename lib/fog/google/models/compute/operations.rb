42c5ae3d198aeac0177dd48f744f50701e4971ea
Ferran Rodenas <frodenas@gmail.com>
2014-04-09 13:29:49 -0700

    [google|compute] Complete Operations support
    - Add missing Operation attributes
    - Remove unnecessary attribute aliases
    - Add "zone_name" attibute for backward compatibility
    - Add "destroy" method for an Operation
    - Add "all" method for Operations
    - Fix a bug when building an empty response (ie on delete operations)
    - Return nil on NotFound Operations (behave like other fog resources)

--- lib/fog/google/models/compute/operations.rb
+++ lib/fog/google/models/compute/operations.rb
@@ -9,8 +9,18 @@ module Fog
 
         model Fog::Compute::Google::Operation
 
+        def all(filters = {})
+          if filters['zone']
+            data = service.list_zone_operations(filters['zone']).body
+          elsif filters['region']
+            data = service.list_region_operations(filters['region']).body
+          else
+            data = service.list_global_operations.body
+          end
+          load(data['items'] || [])
+        end
+
         def get(identity, zone=nil, region=nil)
-          puts "Getting operation #{identity} zone=#{zone} region=#{region}"
           if not zone.nil?
             response = service.get_zone_operation(zone, identity)
           elsif not region.nil?
@@ -20,6 +30,8 @@ module Fog
           end
           return nil if response.nil?
           new(response.body)
+        rescue Fog::Errors::NotFound
+          nil
         end
 
       end
