0a05ba6efbf0316a23a0ffb764ab2471df9ffe1d
Ferran Rodenas <frodenas@gmail.com>
2014-04-10 00:26:01 -0700

    [google|compute] Add Addresses support
    - Add "Addresses" model and collection
    - Add "list_aggregated_addresses" request
    - Modify "insert" request to allow "description" property

--- lib/fog/google/compute.rb
+++ lib/fog/google/compute.rb
@@ -10,6 +10,7 @@ module Fog
       request_path 'fog/google/requests/compute'
       request :list_servers
       request :list_addresses
+      request :list_aggregated_addresses
       request :list_disks
       request :list_firewalls
       request :list_images
@@ -92,6 +93,9 @@ module Fog
 
       model :disk
       collection :disks
+
+      model :address
+      collection :addresses
 
       model :operation
       collection :operations
