8bb2d422b379e9c507d3a884cb42e51f05f1bbe1
Achim Ledermüller <achim.ledermueller@netways.de>
2014-05-14 09:32:16 +0200

    [opennebula] VNC console cleanup

--- lib/fog/opennebula/requests/compute/get_vnc_console.rb
+++ lib/fog/opennebula/requests/compute/get_vnc_console.rb
@@ -15,49 +15,31 @@ module Fog
         #     * type <~String>
         def get_vnc_console(server_id, console_type)
           body = {
-#            'os-getVNCConsole' => {
-#              'type' => console_type
-#            }
               :type => "novnc",
               :proxy_port => "29876",
               :password => "null",
               :token => "3n32dtwpsdj5jkug3b4w",
-              :proxy_host => "one.adm.netways.de"
+              :proxy_host => "example.com"
           }
-#          server_action(server_id, body)
         end # def get_vnc_console
       end # class Real
 
       class Real 
         def get_vnc_console(server_id, console_type, onevm_object)
           logger = Fog::Logger.new
-	  $conf = {"vnc_proxy_port" => "29876", "vnc_proxy_ipv6" => "", "vnc_proxy_support_wss" => "", "vnc_proxy_cert" => "", "vnc_proxy_key" => ""}
+          $conf = {"vnc_proxy_port" => "29876", "vnc_proxy_ipv6" => "", "vnc_proxy_support_wss" => "", "vnc_proxy_cert" => "", "vnc_proxy_key" => ""}
           $vnc = OpenNebulaVNC.new($conf, logger)
-	  puts server_id.inspect
           ret = startvnc(onevm_object,$vnc)
 
           response = Excon::Response.new
           response.status = ret[0]
           response.body = ret[1]
-#            :type => "novnc",
-#	          :proxy_port => "29876",
-#	          :password => "null",
-#	          :token => "3n32dtwpsdj5jkug3b4w",
-#	        #:proxy_host => "one.adm.netways.de"
-#          }
           response
         end # def get_vnc_console
 
         def startvnc(onevm_object, vnc)
-           # resource = retrieve_resource("vm",id)
-           # if OpenNebula.is_error?(resource)
-           #     return [404, resource.to_json]
-           # end
-    
             return vnc.proxy(onevm_object)
         end #def startvnc
-
-
       end # class Mock
     end # class OpenNebula
   end # module Compute
