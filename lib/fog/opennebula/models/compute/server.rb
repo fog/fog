2b142918a2a9f09b24544a4d86429fd619e7e701
Achim Ledermüller <achim.ledermueller@netways.de>
2014-05-14 09:11:14 +0200

    [opennebula] fixed indentation

--- lib/fog/opennebula/models/compute/server.rb
+++ lib/fog/opennebula/models/compute/server.rb
@@ -5,21 +5,21 @@ module Fog
     class OpenNebula
       class Server < Fog::Compute::Server
         identity :id
-	attribute :template_str
-	attribute :name
-	attribute :uuid
-	attribute :state
-	attribute :status
-	attribute :ip
-	attribute :mac
-	attribute :vcpu
-	attribute :cpu
-	attribute :memory
-	attribute :user
-	attribute :gid
-	attribute :group
-	attribute :onevm_object
-	attribute :flavor
+        attribute :template_str
+        attribute :name
+        attribute :uuid
+        attribute :state
+        attribute :status
+        attribute :ip
+        attribute :mac
+        attribute :vcpu
+        attribute :cpu
+        attribute :memory
+        attribute :user
+        attribute :gid
+        attribute :group
+        attribute :onevm_object
+        attribute :flavor
 
         def save
           #if persisted?
@@ -30,62 +30,61 @@ module Fog
           #reload
         end
 
-	def interfaces
-		#[] << Fog::Compute::OpenNebula::Interfaces.new
-		[]
-	end
+        def interfaces
+          #[] << Fog::Compute::OpenNebula::Interfaces.new
+          []
+        end
 
-	def interfaces_attributes=(attributes)
-		true
-	end
+        def interfaces_attributes=(attributes)
+          true
+        end
 
-	def vm_ip_address
-	  ip
-	end
-
+        def vm_ip_address
+          ip
+        end
 
         def private_ip_address
-	  ip
-	end
+          ip
+        end
 
-	def public_ip_address
-	  ip
-	end
+        def public_ip_address
+          ip
+        end
 
-	def vm_mac_address
-	  mac
-	end
-	
-	def start
-		if status == 4
-		  service.vm_resume(id)
-		end
-	        return true
-	end	
+        def vm_mac_address
+          mac
+        end
 
-	def stop
-	Fog::Logger.warning("stop VM: ID:#{id}")
-	  service.vm_stop(id)
-	end
+        def start
+          if status == 4
+            service.vm_resume(id)
+          end
+          return true
+        end	
+
+        def stop
+          Fog::Logger.warning("stop VM: ID:#{id}")
+          service.vm_stop(id)
+        end
 
         def destroy
           service.vm_destroy(id)
         end
 
         def ready?
-	  (status == 3) 
+          (status == 3) 
         end
-	
-	def template_id
-	  4
-	end
 
-	def console_output
+        # dummy method - used by a rails-view to get the id of the default template, but i have no clou what a global default should be
+        # method should be remove here
+        def template_id
+          1
+        end
+
+        def console_output
           requires :id
-	  service.get_vnc_console(id, "vnc", onevm_object)
-	end
-
-
+          service.get_vnc_console(id, "vnc", onevm_object)
+        end
       end
     end
   end
