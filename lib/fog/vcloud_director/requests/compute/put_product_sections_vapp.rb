8ae4285f1b2df598ec2607350103081b5313eefc
Simas Cepaitis <simas.cepaitis@snc.lt>
2014-05-20 16:41:29 +0100

    Add custom_fields to vapp, remove debug info

--- lib/fog/vcloud_director/requests/compute/put_product_sections_vapp.rb
+++ lib/fog/vcloud_director/requests/compute/put_product_sections_vapp.rb
@@ -15,21 +15,20 @@ module Fog
         # @see http://pubs.vmware.com/vcd-51/topic/com.vmware.vcloud.api.reference.doc_51/doc/operations/PUT-VAppMetadataItem-metadata.html
         # @since vCloud API version 1.5
         def put_product_sections_vapp(id, sections)
-            xml  = '<ProductSectionList xmlns="http://www.vmware.com/vcloud/v1.5" xmlns:ovf="http://schemas.dmtf.org/ovf/envelope/1">'
-            xml += '<ovf:ProductSection>'
-            xml += '<ovf:Info>Global vApp Custom Properties</ovf:Info>'
-            xml += '<ovf:Category>Global</ovf:Category>'
+          xml  = '<ProductSectionList xmlns="http://www.vmware.com/vcloud/v1.5" xmlns:ovf="http://schemas.dmtf.org/ovf/envelope/1">'
+          xml += '<ovf:ProductSection>'
+          xml += '<ovf:Info>Global vApp Custom Properties</ovf:Info>'
+          xml += '<ovf:Category>Global</ovf:Category>'
 
-            sections.each do |section|
-              section[:user_configurable] ||= true
-              section[:type]              ||= "string"
-              section[:password]          ||= false
-              xml += "<ovf:Property ovf:userConfigurable='#{section[:user_configurable]}' ovf:type='#{section[:type]}' ovf:password='#{section[:password]}' ovf:key='#{section[:id]}' ovf:value='#{section[:value]}'/>"
-            end
+          sections.each do |section|
+            section[:user_configurable] ||= true
+            section[:type]              ||= "string"
+            section[:password]          ||= false
+            xml += "<ovf:Property ovf:userConfigurable='#{section[:user_configurable]}' ovf:type='#{section[:type]}' ovf:password='#{section[:password]}' ovf:key='#{section[:id]}' ovf:value='#{section[:value]}'/>"
+          end
 
-            xml += '</ovf:ProductSection>'
-            xml += "</ProductSectionList>"
-            puts xml
+          xml += '</ovf:ProductSection>'
+          xml += "</ProductSectionList>"
 
           request(
             :body    => xml,
