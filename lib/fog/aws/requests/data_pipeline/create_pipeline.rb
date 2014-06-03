19f976bfe2481c1a6a128d56b988fa82c340ff32
Anthony Accardi <anthony@swipely.com>
2014-06-03 16:45:44 -0400

    Add support for tags for Data Pipeline

--- lib/fog/aws/requests/data_pipeline/create_pipeline.rb
+++ lib/fog/aws/requests/data_pipeline/create_pipeline.rb
@@ -7,15 +7,17 @@ module Fog
         # ==== Parameters
         # * UniqueId <~String> - A unique ID for of the pipeline
         # * Name <~String> - The name of the pipeline
+        # * Tags <~Hash> - Key/value string pairs to categorize the pipeline
         # * Description <~String> - Description of the pipeline
         # ==== Returns
         # * response<~Excon::Response>:
         #   * body<~Hash>:
-        def create_pipeline(unique_id, name, description=nil)
+        def create_pipeline(unique_id, name, description=nil, tags=nil)
           params = {
             'uniqueId' => unique_id,
             'name' => name,
           }
+          params['tags'] = tags.map {|k,v| {"key" => k.to_s, "value" => v.to_s}} unless tags.nil? || tags.empty?
           params['Description'] = description if description
 
           response = request({
@@ -28,7 +30,7 @@ module Fog
       end
 
       class Mock
-        def create_pipeline(unique_id, name, description=nil)
+        def create_pipeline(unique_id, name, description=nil, tags=nil)
           Fog::Mock.not_implemented
         end
       end
