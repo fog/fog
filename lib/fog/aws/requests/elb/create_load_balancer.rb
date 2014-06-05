1b151ff3ed8a2016270d4f1452738f4d9bc0c03e
Eugene Howe <eugene@xtreme-computers.net>
2014-06-05 11:55:35 -0400

    properly initialize compute object inside elb methods

--- lib/fog/aws/requests/elb/create_load_balancer.rb
+++ lib/fog/aws/requests/elb/create_load_balancer.rb
@@ -99,8 +99,9 @@ module Fog
                              Fog::Compute::AWS::Mock.data[region][@aws_access_key_id][:security_groups]['amazon-elb-sg']
                            when 'EC2-VPC-Default'
                              # find or create default vpc
-                             unless vpc = Fog::Compute[:aws].vpcs.all.first
-                               vpc = Fog::Compute[:aws].vpcs.create('cidr_block' => '10.0.0.0/24')
+                             compute = Fog::Compute::AWS::new(aws_access_key_id: @aws_access_key_id, aws_secret_access_key: @aws_secret_access_key)
+                             unless vpc = compute.vpcs.all.first
+                               vpc = compute.vpcs.create('cidr_block' => '10.0.0.0/24')
                              end
 
                              default_sg = Fog::Compute::AWS::Mock.data[region][@aws_access_key_id][:security_groups].values.find { |sg|
