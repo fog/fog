f828eebdd1707d8b9c85b638721aa2a06bf73b5b
Daniel Aragao <daragao@thoughtworks.com>
2014-06-11 13:04:43 +1000

    Adding network_adapters to VM parser and model as a single IP address doesn't cut when dealing with multiple networks.

--- lib/fog/vcloud_director/parsers/compute/vm.rb
+++ lib/fog/vcloud_director/parsers/compute/vm.rb
@@ -22,13 +22,51 @@ module Fog
               @response[:vm].merge!(vm_attrs.reject {|key,value| ![:href, :name, :status, :type, :deployed].include?(key)})
               @response[:vm][:id] = @response[:vm][:href].split('/').last
               @response[:vm][:status] = human_status(@response[:vm][:status])
-            else
-              parse_start_element name, attributes, @response[:vm]
+            when 'HostResource'
+              @current_host_resource = extract_attributes(attributes)
+            when 'Connection'
+              @current_network_connection = extract_attributes(attributes)
+            when 'Link'
+              @links << extract_attributes(attributes)
             end
           end
 
           def end_element(name)
-            parse_end_element name, @response[:vm]
+            case name
+            when 'IpAddress'
+              @response[:vm][:ip_address] = value
+            when 'Description'
+              if @in_operating_system
+                @response[:vm][:operating_system] = value
+                @in_operating_system = false
+              end
+            when 'ResourceType'
+              @resource_type = value
+            when 'VirtualQuantity'
+              case @resource_type
+              when '3'
+                @response[:vm][:cpu] = value
+              when '4'
+                @response[:vm][:memory] = value
+              end
+            when 'ElementName'
+              @element_name = value
+            when 'Item'
+              case @resource_type
+              when '17' # disk
+                @response[:vm][:disks] ||= []
+                @response[:vm][:disks] << { @element_name => @current_host_resource[:capacity].to_i }
+              when '10' # nic
+                @response[:vm][:network_adapters] ||= []
+                @response[:vm][:network_adapters] << {
+                  :ip_address => @current_network_connection[:ipAddress],
+                  :primary => (@current_network_connection[:primaryNetworkConnection] == 'true'),
+                  :ip_allocation_mode => @current_network_connection[:ipAddressingMode]
+                }
+              end
+            when 'Link'
+              @response[:vm][:links] = @links
+            end
           end
 
           def human_status(status)
--- lib/fog/vcloud_director/parsers/compute/vms.rb
+++ lib/fog/vcloud_director/parsers/compute/vms.rb
@@ -33,14 +33,52 @@ module Fog
               @response[:id] = @response[:href].split('/').last
             when 'Children'
               @in_children = true
-            else
-              parse_start_element name, attributes, @vm
+            when 'HostResource'
+              @current_host_resource = extract_attributes(attributes)
+            when 'Connection'
+              @current_network_connection = extract_attributes(attributes)
+            when 'Link'
+              @links << extract_attributes(attributes)
             end
           end
 
           def end_element(name)
             if @in_children
-              if name == 'Vm'
+              case name
+              when 'IpAddress'
+                @vm[:ip_address] = value
+              when 'Description'
+                if @in_operating_system
+                  @vm[:operating_system] = value
+                  @in_operating_system = false
+                end
+              when 'ResourceType'
+                @resource_type = value
+              when 'VirtualQuantity'
+                case @resource_type
+                when '3'
+                  @vm[:cpu] = value
+                when '4'
+                  @vm[:memory] = value
+                end
+              when 'ElementName'
+                @element_name = value
+              when 'Item'
+                case @resource_type
+                when '17' # disk
+                  @vm[:disks] ||= []
+                  @vm[:disks] << { @element_name => @current_host_resource[:capacity].to_i }
+                when '10' # nic
+                  @vm[:network_adapters] ||= []
+                  @vm[:network_adapters] << {
+                    :ip_address => @current_network_connection[:ipAddress],
+                    :primary => (@current_network_connection[:primaryNetworkConnection] == 'true'),
+                    :ip_allocation_mode => @current_network_connection[:ipAddressingMode]
+                  }
+                end
+              when 'Link'
+                @vm[:links] = @links
+              when 'Vm'
                 @response[:vms] << @vm
                 @vm = {}
               else
