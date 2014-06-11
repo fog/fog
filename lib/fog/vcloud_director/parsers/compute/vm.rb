5f5b1d6e152a592f4a57a7905e5165c067d6d8ce
Daniel Aragao <daragao@thoughtworks.com>
2014-06-11 13:04:43 +1000

    Adding network_adapters to VM parser and model as a single IP address doesn't cut when dealing with multiple networks.

--- lib/fog/vcloud_director/parsers/compute/vm.rb
+++ lib/fog/vcloud_director/parsers/compute/vm.rb
@@ -30,6 +30,41 @@ module Fog
 
           def end_element(name)
             parse_end_element name, @response[:vm]
+            case name
+            when 'IpAddress'
+              @response[:vm][:ip_address] = value
+            when 'Description'
+              if @in_operating_system
+                @response[:vm][:operating_system] = value
+                @in_operating_system = false
+              end
+            when 'ResourceType'
+              @resource_type = value
+            when 'VirtualQuantity'
+              case @resource_type
+              when '3'
+                @response[:vm][:cpu] = value
+              when '4'
+                @response[:vm][:memory] = value
+              end
+            when 'ElementName'
+              @element_name = value
+            when 'Item'
+              case @resource_type
+              when '17' # disk
+                @response[:vm][:disks] ||= []
+                @response[:vm][:disks] << { @element_name => @current_host_resource[:capacity].to_i }
+              when '10' # nic
+                @response[:vm][:network_adapters] ||= []
+                @response[:vm][:network_adapters] << {
+                  :ip_address => @current_network_connection[:ipAddress],
+                  :primary => (@current_network_connection[:primaryNetworkConnection] == 'true'),
+                  :ip_allocation_mode => @current_network_connection[:ipAddressingMode]
+                }
+              end
+            when 'Link'
+              @response[:vm][:links] = @links
+            end
           end
 
           def human_status(status)
