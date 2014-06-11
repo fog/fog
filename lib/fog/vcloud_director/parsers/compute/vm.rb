5508c1afa1d5940c38ee835f3e990a9246645406
Daniel Aragao <daragao@thoughtworks.com>
2014-06-11 15:35:55 +1000

    DRYer refactored version.

--- lib/fog/vcloud_director/parsers/compute/vm.rb
+++ lib/fog/vcloud_director/parsers/compute/vm.rb
@@ -1,8 +1,12 @@
+require 'fog/vcloud_director/parsers/compute/vm_parser_helper'
+
 module Fog
   module Parsers
     module Compute
       module VcloudDirector
         class Vm < VcloudDirectorParser
+          include VmParserHelper
+
           def reset
             @in_operating_system = false
             @in_children = false
@@ -13,59 +17,18 @@ module Fog
 
           def start_element(name, attributes)
             super
-            case name
-            when 'OperatingSystemSection'
-              @in_operating_system = true
-            when 'Vm'
+            if name == 'Vm'
               vm_attrs = extract_attributes(attributes)
               @response[:vm].merge!(vm_attrs.reject {|key,value| ![:href, :name, :status, :type].include?(key)})
               @response[:vm][:id] = @response[:vm][:href].split('/').last
               @response[:vm][:status] = human_status(@response[:vm][:status])
-            when 'HostResource'
-              @current_host_resource = extract_attributes(attributes)
-            when 'Connection'
-              @current_network_connection = extract_attributes(attributes)
-            when 'Link'
-              @links << extract_attributes(attributes)
+            else
+              parse_start_element name, attributes, @response[:vm]
             end
           end
 
           def end_element(name)
-            case name
-            when 'IpAddress'
-              @response[:vm][:ip_address] = value
-            when 'Description'
-              if @in_operating_system
-                @response[:vm][:operating_system] = value
-                @in_operating_system = false
-              end
-            when 'ResourceType'
-              @resource_type = value
-            when 'VirtualQuantity'
-              case @resource_type
-              when '3'
-                @response[:vm][:cpu] = value
-              when '4'
-                @response[:vm][:memory] = value
-              end
-            when 'ElementName'
-              @element_name = value
-            when 'Item'
-              case @resource_type
-              when '17' # disk
-                @response[:vm][:disks] ||= []
-                @response[:vm][:disks] << { @element_name => @current_host_resource[:capacity].to_i }
-              when '10' # nic
-                @response[:vm][:network_adapters] ||= []
-                @response[:vm][:network_adapters] << { 
-                  :ip_address => @current_network_connection[:ipAddress], 
-                  :primary => (@current_network_connection[:primaryNetworkConnection] == 'true'),
-                  :ip_allocation_mode => @current_network_connection[:ipAddressingMode]
-                }
-              end
-            when 'Link'
-              @response[:vm][:links] = @links
-            end
+            parse_end_element name, @response[:vm]
           end
 
           def human_status(status)
