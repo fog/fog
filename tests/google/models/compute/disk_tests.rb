6d683ed00a8924ab9d775cbe4bdbfa1a5baf3f1c
A.S. Lomoff <lomov.as@gmail.com>
2014-06-14 19:45:24 +0300

    rewrite the way model is waited in live tests

--- tests/google/models/compute/disk_tests.rb
+++ tests/google/models/compute/disk_tests.rb
@@ -3,10 +3,7 @@ Shindo.tests("Fog::Compute[:google] | disk model", ['google']) do
   model_tests(Fog::Compute[:google].disks, {:name => 'fog-disk-model-tests',
                                             :zone => 'us-central1-a',
                                             :size_gb => 10}) do |model|
-    Fog.wait_for do 
-      model.reload
-      model.status != 'CREATING'
-    end
+    model.wait_for { ready? }
   end
 
 end
