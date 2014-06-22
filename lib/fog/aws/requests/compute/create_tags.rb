659ae4ffef5897f3e20867dfb934298bb78b3316
Ben House <ben@benhouse.io>
2014-06-22 00:10:00 -0700

    [aws] add mock tagging for acls and vpcs, refactor types

--- lib/fog/aws/requests/compute/create_tags.rb
+++ lib/fog/aws/requests/compute/create_tags.rb
@@ -37,27 +37,47 @@ module Fog
       end
 
       class Mock
+        MOCK_TAG_TYPES = {
+          'ami' => 'image',
+          'i' => 'instance',
+          'snap' => 'snapshot',
+          'vol' => 'volume',
+          'igw' => 'internet_gateway',
+          'acl' => 'network_acl',
+          'vpc' => 'vpc'
+        }
+
         def create_tags(resources, tags)
           resources = [*resources]
 
           tagged = resources.map do |resource_id|
-            type = case resource_id
-            when /^ami\-[a-z0-9]{8}$/i
-              'image'
-            when /^i\-[a-z0-9]{8}$/i
-              'instance'
-            when /^snap\-[a-z0-9]{8}$/i
-              'snapshot'
-            when /^vol\-[a-z0-9]{8}$/i
-              'volume'
-            when /^igw\-[a-z0-9]{8}$/i
-              'internet_gateway'
-            end
-            if type && ((type == 'image' && visible_images[resource_id]) || self.data[:"#{type}s"][resource_id])
-              { 'resourceId' => resource_id, 'resourceType' => type }
+            if match = resource_id.match(/^(\w+)-[a-z0-9]{8}/i)
+              id = match.captures.first
             else
-              raise(Fog::Service::NotFound.new("The #{type} ID '#{resource_id}' does not exist"))
+              raise(Fog::Service::NotFound.new("Unknown resource id #{resource_id}"))
             end
+
+            if MOCK_TAG_TYPES.has_key? id
+              type = MOCK_TAG_TYPES[id]
+            else
+              raise(Fog::Service::NotFound.new("Mocking tags of resource #{resource_id} has not been implemented"))
+            end
+
+            case type
+              when 'image'
+                unless visible_images.has_key? resource_id
+                 raise(Fog::Service::NotFound.new("Cannot tag #{resource_id}, the image does not exist"))
+                end
+              when 'vpc'
+                if self.data[:vpcs].select {|v| v['vpcId'] == resource_id }.empty?
+                  raise(Fog::Service::NotFound.new("Cannot tag #{resource_id}, the vpc does not exist"))
+                end
+              else
+                unless self.data[:"#{type}s"][resource_id]
+                 raise(Fog::Service::NotFound.new("Cannot tag #{resource_id}, the #{type} does not exist"))
+                end
+            end
+            { 'resourceId' => resource_id, 'resourceType' => type }
           end
 
           tags.each do |key, value|
