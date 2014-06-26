2481c7b190b2c5a8ab2081f0a44016c0c36c28bd
Ben House <ben@benhouse.io>
2014-06-25 19:48:53 -0700

    move tagged_resources method to compute

--- lib/fog/aws/compute.rb
+++ lib/fog/aws/compute.rb
@@ -166,6 +166,16 @@ module Fog
       end
 
       class Mock
+        MOCKED_TAG_TYPES = {
+          'ami' => 'image',
+          'i' => 'instance',
+          'snap' => 'snapshot',
+          'vol' => 'volume',
+          'igw' => 'internet_gateway',
+          'acl' => 'network_acl',
+          'vpc' => 'vpc'
+        }
+
         include Fog::AWS::CredentialFetcher::ConnectionMethods
         include Fog::AWS::RegionMethods
 
@@ -329,6 +339,39 @@ module Fog
         def set_supported_platforms(values)
           self.data[:account_attributes].find { |h| h["attributeName"] == "supported-platforms" }["values"] = values
         end
+
+        def tagged_resources(resources)
+          Array(resources).map do |resource_id|
+            if match = resource_id.match(/^(\w+)-[a-z0-9]{8}/i)
+              id = match.captures.first
+            else
+              raise(Fog::Service::NotFound.new("Unknown resource id #{resource_id}"))
+            end
+
+            if MOCKED_TAG_TYPES.has_key? id
+              type = MOCKED_TAG_TYPES[id]
+            else
+              raise(Fog::Service::NotFound.new("Mocking tags of resource #{resource_id} has not been implemented"))
+            end
+
+            case type
+              when 'image'
+                unless visible_images.has_key? resource_id
+                 raise(Fog::Service::NotFound.new("Cannot tag #{resource_id}, the image does not exist"))
+                end
+              when 'vpc'
+                if self.data[:vpcs].select {|v| v['vpcId'] == resource_id }.empty?
+                  raise(Fog::Service::NotFound.new("Cannot tag #{resource_id}, the vpc does not exist"))
+                end
+              else
+                unless self.data[:"#{type}s"][resource_id]
+                 raise(Fog::Service::NotFound.new("Cannot tag #{resource_id}, the #{type} does not exist"))
+                end
+            end
+            { 'resourceId' => resource_id, 'resourceType' => type }
+          end
+        end
+
 
         def apply_tag_filters(resources, filters, resource_id_key)
           tag_set_fetcher = lambda {|resource| self.data[:tag_sets][resource[resource_id_key]] }
--- lib/fog/aws/requests/compute/create_tags.rb
+++ lib/fog/aws/requests/compute/create_tags.rb
@@ -37,48 +37,9 @@ module Fog
       end
 
       class Mock
-        MOCK_TAG_TYPES = {
-          'ami' => 'image',
-          'i' => 'instance',
-          'snap' => 'snapshot',
-          'vol' => 'volume',
-          'igw' => 'internet_gateway',
-          'acl' => 'network_acl',
-          'vpc' => 'vpc'
-        }
-
         def create_tags(resources, tags)
           resources = [*resources]
-
-          tagged = resources.map do |resource_id|
-            if match = resource_id.match(/^(\w+)-[a-z0-9]{8}/i)
-              id = match.captures.first
-            else
-              raise(Fog::Service::NotFound.new("Unknown resource id #{resource_id}"))
-            end
-
-            if MOCK_TAG_TYPES.has_key? id
-              type = MOCK_TAG_TYPES[id]
-            else
-              raise(Fog::Service::NotFound.new("Mocking tags of resource #{resource_id} has not been implemented"))
-            end
-
-            case type
-              when 'image'
-                unless visible_images.has_key? resource_id
-                 raise(Fog::Service::NotFound.new("Cannot tag #{resource_id}, the image does not exist"))
-                end
-              when 'vpc'
-                if self.data[:vpcs].select {|v| v['vpcId'] == resource_id }.empty?
-                  raise(Fog::Service::NotFound.new("Cannot tag #{resource_id}, the vpc does not exist"))
-                end
-              else
-                unless self.data[:"#{type}s"][resource_id]
-                 raise(Fog::Service::NotFound.new("Cannot tag #{resource_id}, the #{type} does not exist"))
-                end
-            end
-            { 'resourceId' => resource_id, 'resourceType' => type }
-          end
+          tagged = tagged_resources(resources)
 
           tags.each do |key, value|
             self.data[:tags][key] ||= {}
