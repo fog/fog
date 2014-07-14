ac55f7638f5d6668e8d5276ae66ff9602c51eb70
Chris Roberts <chrisroberts.code@gmail.com>
2014-07-14 08:05:26 -0700

    Fetch all events when loading collection

--- lib/fog/aws/models/orchestration/events.rb
+++ lib/fog/aws/models/orchestration/events.rb
@@ -14,12 +14,23 @@ module Fog
           self.stack = stack if stack
           if(self.stack)
             unless(self.stack.attributes['Events'])
-              self.stack.attributes['Events'] = service.describe_stack_events(
-                self.stack.stack_name
-              ).body['StackEvents']
+              self.stack.attributes['Events'] = fetch_events
             end
             load(self.stack.attributes['Events'])
           end
+        end
+
+        def fetch_events(next_token=nil)
+          stack_events = []
+          options = next_token ? {'NextToken' => next_token} : {}
+          result = service.describe_stack_events(
+            self.stack.stack_name, options
+          )
+          stack_events += result.body['StackEvents']
+          if(result.body['NextToken'])
+            stack_events += fetch_events(result.body['NextToken'])
+          end
+          stack_events
         end
 
       end
