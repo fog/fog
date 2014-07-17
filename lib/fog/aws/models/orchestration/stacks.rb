6e42a0b9f5e360a06a05e2382262cbbac7bdc889
Chris Roberts <chrisroberts.code@gmail.com>
2014-07-17 08:23:55 -0700

    Fetch all stacks. Cache result in attributes to prevent continual re-fetching

--- lib/fog/aws/models/orchestration/stacks.rb
+++ lib/fog/aws/models/orchestration/stacks.rb
@@ -16,14 +16,35 @@ module Fog
         end
 
         def all
-          describe = service.describe_stacks.body['Stacks']
-          list = service.list_stacks(self.filters).body['StackSummaries']
+          unless(attributes['Stacks'])
+            attributes['Stacks'] = fetch_stacks
+          end
+          load(attributes['Stacks'])
+        end
+
+        def fetch_paged_results(result_key, next_token=nil, &block)
+          list = []
+          options = next_token ? {'NextToken' => next_token} : {}
+          result = block.call(options)
+          list += result.body[result_key]
+          if(token = result.body['NextToken'])
+            list += fetch_paged_results(result_key, token, &block)
+          end
+          list
+        end
+
+        def fetch_stacks
+          describe = fetch_paged_results('Stacks') do |options|
+            service.describe_stacks(options)
+          end
+          list = fetch_paged_results('StackSummaries') do |options|
+            service.list_stacks(options.merge(self.filters))
+          end
           stack_defs = list.map do |stack_hash|
             desc_hash = describe.detect{|h| h['StackId'] == stack_hash['StackId']}
             stack_hash.merge!(desc_hash) if desc_hash
             stack_hash
           end
-          load(stack_defs)
         end
 
         def find_by_name(name)
