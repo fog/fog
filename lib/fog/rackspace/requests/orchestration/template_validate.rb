1f9d4ea86e4df7db4dcd270fbf2d8a565bc2a637
Chris Roberts <chrisroberts.code@gmail.com>
2014-07-25 14:40:23 -0700

    Add template validation request

---
+++ lib/fog/rackspace/requests/orchestration/template_validate.rb
@@ -0,0 +1,28 @@
+module Fog
+  module Orchestration
+    class Rackspace
+      class Real
+
+        # Return template for stack
+        #
+        # @param options [Hash] request options
+        # @option options [String] :template JSON template
+        # @option options [String] :template_url URL of the template
+        # @see http://docs.rackspace.com/orchestration/api/v1/orchestration-devguide/content/POST_template_validate_v1__tenant_id__validate_Templates.html
+        def template_validate(options)
+          request(
+            :expects  => 200,
+            :path => 'validate',
+            :method => 'POST'
+          )
+        end
+
+      end
+
+      class Mock
+        def template_validate(options)
+        end
+      end
+    end
+  end
+end
