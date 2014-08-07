bf46f69fe92605b017873fc9424d8bbf46b5aa50
snyquist2 <snyquist@google.com>
2014-08-07 03:26:22 +0000

    corrected naming on tests

--- tests/google/models/compute/target_instance_tests.rb
+++ tests/google/models/compute/target_instance_tests.rb
@@ -3,5 +3,5 @@ Shindo.tests("Fog::Compute[:google] | target instance model", ['google']) do
   random_string = SecureRandom.hex
   @zone = 'us-central1-a'
   instance = create_test_server(Fog::Compute[:google], @zone)  
-  model_tests(Fog::Compute[:google].target_instances, {:name => 'fog-test-target-instance-#{random_string}', :instance => instance.self_link, :zone => @zone})
+  model_tests(Fog::Compute[:google].target_instances, {:name => "fog-test-target-instance-#{random_string}", :instance => instance.self_link, :zone => @zone})
 end
--- tests/google/models/compute/url_maps_tests.rb
+++ tests/google/models/compute/url_maps_tests.rb
@@ -1,5 +1,7 @@
+require 'securerandom'
 Shindo.tests("Fog::Compute[:google] | url maps model", ['google']) do
   backend_service = create_test_backend_service(Fog::Compute[:google])  
-  collection_tests(Fog::Compute[:google].url_maps, {:name => 'fog-url-maps-test', :defaultService => backend_service.self_link})
+  random_string = SecureRandom.hex
+  collection_tests(Fog::Compute[:google].url_maps, {:name => "fog-url-maps-test-#{random_string}", :defaultService => backend_service.self_link})
 
 end
