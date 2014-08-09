7ab3278dc1b875080cb3d8c77668ae1e2c87cc6d
A.S. Lomoff <lomov.as@gmail.com>
2014-08-09 23:20:09 +0300

    fix mistyping in openstack storage directory model

--- lib/fog/openstack/models/storage/directory.rb
+++ lib/fog/openstack/models/storage/directory.rb
@@ -10,7 +10,7 @@ module Fog
         attribute :bytes, :aliases => 'X-Container-Bytes-Used'
         attribute :count, :aliases => 'X-Container-Object-Count'
         
-        attr_reader :public
+        attr_writer :public
 
         def destroy
           requires :key
