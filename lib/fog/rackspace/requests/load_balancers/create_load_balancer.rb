805b8edb7d2aba629286f159afea097b399d0b00
Ash Wilson <smashwilson@gmail.com>
2014-08-13 15:57:35 -0400

    Yard docs and optional parameters for lbs.

--- lib/fog/rackspace/requests/load_balancers/create_load_balancer.rb
+++ lib/fog/rackspace/requests/load_balancers/create_load_balancer.rb
@@ -2,8 +2,29 @@ module Fog
   module Rackspace
     class LoadBalancers
       class Real
-        def create_load_balancer(name, protocol, port, virtual_ips, nodes, options = {})
-          data = {
+
+        # Issue an asynchronous request to create a new Load Balancer.
+        #
+        # @param name [String] human-friendly identifier for the balancer that will be shown in
+        #   the web UI.
+        # @param protocol [String] well-known protocol describing the traffic to be load balanced.
+        # @param port [Integer] port for the balancer to listen on and balance to.
+        # @param virtual_ips [Array<Hash>] description of the kind of IP address to bind to, or id
+        #   of the existing virtual IP from another balancer. Examples: `{ 'type' => 'PUBLIC' }`,
+        #   `{ 'type' => 'PRIVATE' }`, `{ 'id' => 1234 }`
+        # @param nodes [Array<Hash>] collection of
+        # @option options [String] :algorithm balancing algorithm for the balancer to use.
+        #   See http://docs.rackspace.com/loadbalancers/api/v1.0/clb-devguide/content/Algorithms-d1e4367.html
+        # @option options [String] :timeout amount of time the load balancer will wait for a response
+        #   from a back-end node before terminating the connection. Defaults to 30 seconds, may be
+        #   increased to a maximum of 120 seconds.
+        #
+        def create_load_balancer(name, protocol, port = nil, virtual_ips = [{'type' => 'PUBLIC'}], nodes = nil, options = {})
+          lb_data = {
+            'name' => name,
+            'protocol' => protocol,
+            'virtualIps' => virtual_ips
+          }
             'loadBalancer' => {
               'name' => name,
               'port' => port,
@@ -12,9 +33,12 @@ module Fog
             }
           }
 
-          data['loadBalancer']['nodes'] = nodes if nodes && !nodes.empty?
-          data['loadBalancer']['algorithm'] = options[:algorithm] if options.key? :algorithm
-          data['loadBalancer']['timeout'] = options[:timeout] if options.key? :timeout
+          lb_data['nodes'] = nodes if nodes && !nodes.empty?
+          lb_data['port'] = port if port
+          lb_data['algorithm'] = options[:algorithm] if options.key? :algorithm
+          lb_data['timeout'] = options[:timeout] if options.key? :timeout
+
+          data = { 'loadBalancer' => lb_data }
 
           request(
             :body     => Fog::JSON.encode(data),
@@ -23,6 +47,7 @@ module Fog
             :path     => 'loadbalancers.json'
           )
         end
+
       end
 
       class Mock
