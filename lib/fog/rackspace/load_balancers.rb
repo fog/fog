7f7dcce3e8ae34e74aa41d20dbea743f3ed78d63
Ash Wilson <smashwilson@gmail.com>
2014-08-13 14:42:45 -0400

    Remove the deprecation notices.
    They'll now all be errors, instead.

--- lib/fog/rackspace/load_balancers.rb
+++ lib/fog/rackspace/load_balancers.rb
@@ -185,10 +185,6 @@ module Fog
             regions = @identity_service.service_catalog.display_service_regions(service_name)
             Fog::Logger.deprecation("Please specify region using :rackspace_region rather than :rackspace_endpoint. Valid regions for :rackspace_region are #{regions}.")
           end
-
-          unless options[:rackspace_region]
-            Fog::Logger.deprecation("Default region support will be removed in an upcoming release. Please switch to manually setting your endpoint. This requires setting the :rackspace_region option")
-          end
         end
 
         def append_tenant_v1(credentials)
