a2d41af9d71cdae6b16a21d6830c82f263d372e3
Nat Welch <nat@natwelch.com>
2014-09-17 11:19:59 -0700

    [google|compute] Change target pool code to be correct and consistent with fog.

--- tests/google/models/compute/target_pools_tests.rb
+++ tests/google/models/compute/target_pools_tests.rb
@@ -4,5 +4,8 @@ Shindo.tests("Fog::Compute[:google] | target pools model", ['google']) do
   region = 'us-central1'
   instance = create_test_server(Fog::Compute[:google], 'us-central1-a')
   health_check = create_test_http_health_check(Fog::Compute[:google])
-  collection_tests(Fog::Compute[:google].target_pools, {:name => "fog-test-target-pool-#{random_string}", :region => region, :instances => [instance.self_link], :healthChecks => [health_check.self_link]})
+  collection_tests(
+    Fog::Compute[:google].target_pools,
+    {:name => "fog-test-target-pool-#{random_string}", :region => region, :instances => [instance.self_link], :healthChecks => [health_check.self_link]}
+  )
 end
