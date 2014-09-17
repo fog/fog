782961e93aafb7bf0d9d87643879b83f517238dc
Nat Welch <nat@natwelch.com>
2014-09-17 10:07:12 -0700

    [google|compute] Change how get_region works for mocks.

--- tests/google/models/compute/target_pools_tests.rb
+++ tests/google/models/compute/target_pools_tests.rb
@@ -5,5 +5,4 @@ Shindo.tests("Fog::Compute[:google] | target pools model", ['google']) do
   instance = create_test_server(Fog::Compute[:google], 'us-central1-a')
   health_check = create_test_http_health_check(Fog::Compute[:google])
   collection_tests(Fog::Compute[:google].target_pools, {:name => "fog-test-target-pool-#{random_string}", :region => region, :instances => [instance.self_link], :healthChecks => [health_check.self_link]})
-
 end
