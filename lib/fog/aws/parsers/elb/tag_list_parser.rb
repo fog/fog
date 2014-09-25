3604bcc12c1af0b8b39a5fbb198eda0b66816fe7
Ben Chadwick <bchadwick@mdsol.com>
2014-09-25 17:25:03 -0400

    update parser

--- lib/fog/aws/parsers/elb/tag_list_parser.rb
+++ lib/fog/aws/parsers/elb/tag_list_parser.rb
@@ -11,23 +11,42 @@ module Fog
             @this_value = nil
             @tags       = Hash.new
             @response   = { 'DescribeTagsResult' => { 'LoadBalancers' => [] }, 'ResponseMetadata' => {} }
+            @in_tags = false
+          end
+
+          def start_element(name, attrs = [])
+            super
+            case name
+              when 'member'
+                unless @in_tags
+                  @load_balancer_name = nil
+                  @tags = {}
+                end
+              when 'Tags'
+                @in_tags = true
+            end
           end
 
           def end_element(name)
             super
             case name
-              when 'Tag'
-                @tags[@this_key] = @this_value
-                @this_key, @this_value = nil, nil
+              when 'member'
+                if @in_tags
+                  @tags[@this_key] = @this_value
+                  @this_key, @this_value = nil, nil
+                else
+                  @response['DescribeTagsResult']['LoadBalancers'] << { 'Tags' => @tags, 'LoadBalancerName' => @load_balancer_name }
+                end
               when 'Key'
                 @this_key = value
               when 'Value'
                 @this_value = value
-              when 'TagDescriptions'
-                @response['DescribeTagsResult']['LoadBalancers'] << { 'TagDescriptions' => @tags }
-                @tags = {}
+              when 'LoadBalancerName'
+                @load_balancer_name = value
               when 'RequestId'
                 @response['ResponseMetadata'][name] = value
+              when 'Tags'
+                @in_tags = false
             end
           end
 
