3e44844a45d0fa3d99558b3d7bb1bba7c8db6e9a
Ethan Devenport <ethand@obsidiansand.com>
2014-09-29 17:34:08 -0700

    Removed unused code in core.rb.

--- lib/fog/profitbricks/core.rb
+++ lib/fog/profitbricks/core.rb
@@ -18,26 +18,7 @@ module Fog
                     xml[:soapenv].Body(&block)
                 end
             end
+
         end
-
-        #def self.to_boolean(string)
-        #    return true if string == true || string =~ (/(true|t|yes|y|1)$/i)
-        #    return false if string == false || string.blank? || string =~ (/(false|f|no|n|0)$/i)
-        #    raise ArgumentError.new("invalid value for Boolean: \"#{self}\"")
-        #end
-
-        #def self.parse_error(xml_fault)
-        #    service_fault = {}
-        #
-        #    Nokogiri::XML(xml_fault).xpath(
-        #      '//ns2:ProfitbricksServiceFault',
-        #      'xmlns:ns2' => 'http://ws.api.profitbricks.com/'
-        #    ).each do |element|
-        #        element.children.each do |child|
-        #            service_fault[child.name] = child.text
-        #        end
-        #    end
-        #    puts "#{service_fault['faultCode']}: #{service_fault['message']}" 
-        #end
     end
 end
