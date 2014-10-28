7aec3290623e8200f6ca0b1943b6e4f9382b5100
Eric Sakowski <sakowski@gmail.com>
2014-10-28 16:55:22 -0400

    Added tags attr to AWS security_groups and made sg.save() create tags

--- lib/fog/aws/parsers/compute/describe_security_groups.rb
+++ lib/fog/aws/parsers/compute/describe_security_groups.rb
@@ -8,8 +8,9 @@ module Fog
             @ip_permission = { 'groups' => [], 'ipRanges' => []}
             @ip_permission_egress = { 'groups' => [], 'ipRanges' => []}
             @ip_range = {}
-            @security_group = { 'ipPermissions' => [], 'ipPermissionsEgress' => [] }
+            @security_group = { 'ipPermissions' => [], 'ipPermissionsEgress' => [], 'tagSet' => {} }
             @response = { 'securityGroupInfo' => [] }
+            @tag = {}
           end
 
           def start_element(name, attrs = [])
@@ -23,70 +24,84 @@ module Fog
               @in_ip_permissions_egress = true
             when 'ipRanges'
               @in_ip_ranges = true
+            when 'tagSet'
+              @in_tag_set = true
             end
           end
 
           def end_element(name)
-            case name
-            when 'cidrIp'
-              @ip_range[name] = value
-            when 'fromPort', 'toPort'
-              if @in_ip_permissions_egress
-                @ip_permission_egress[name] = value.to_i
-              else
-                @ip_permission[name] = value.to_i
+            if @in_tag_set
+              case name
+                when 'item'
+                  @security_group['tagSet'][@tag['key']] = @tag['value']
+                  @tag = {}
+                when 'key', 'value'
+                  @tag[name] = value
+                when 'tagSet'
+                  @in_tag_set = false
               end
-            when 'groups'
-              @in_groups = false
-            when 'groupDescription', 'ownerId', 'vpcId'
-              @security_group[name] = value
-            when 'groupId','groupName'
-              if @in_groups
-                @group[name] = value
-              else
+            else
+              case name
+              when 'cidrIp'
+                @ip_range[name] = value
+              when 'fromPort', 'toPort'
+                if @in_ip_permissions_egress
+                  @ip_permission_egress[name] = value.to_i
+                else
+                  @ip_permission[name] = value.to_i
+                end
+              when 'groups'
+                @in_groups = false
+              when 'groupDescription', 'ownerId', 'vpcId'
                 @security_group[name] = value
-              end
-            when 'ipPermissions'
-              @in_ip_permissions = false
-            when 'ipPermissionsEgress'
-              @in_ip_permissions_egress = false
-            when 'ipProtocol'
-              if @in_ip_permissions_egress
-                @ip_permission_egress[name] = value
-              else
-                @ip_permission[name] = value
-              end
-            when 'ipRanges'
-              @in_ip_ranges = false
-            when 'item'
-              if @in_groups
-                if @in_ip_permissions_egress
-                  @ip_permission_egress['groups'] << @group
+              when 'groupId','groupName'
+                if @in_groups
+                  @group[name] = value
                 else
-                  @ip_permission['groups'] << @group
+                  @security_group[name] = value
                 end
-                @group = {}
-              elsif @in_ip_ranges
+              when 'ipPermissions'
+                @in_ip_permissions = false
+              when 'ipPermissionsEgress'
+                @in_ip_permissions_egress = false
+              when 'ipProtocol'
                 if @in_ip_permissions_egress
-                  @ip_permission_egress['ipRanges'] << @ip_range
+                  @ip_permission_egress[name] = value
                 else
-                  @ip_permission['ipRanges'] << @ip_range
+                  @ip_permission[name] = value
                 end
-                @ip_range = {}
-              elsif @in_ip_permissions
-                @security_group['ipPermissions'] << @ip_permission
-                @ip_permission = { 'groups' => [], 'ipRanges' => []}
-              elsif @in_ip_permissions_egress
-                @security_group['ipPermissionsEgress'] << @ip_permission_egress
-                @ip_permission_egress = { 'groups' => [], 'ipRanges' => []}
-              else
-                @response['securityGroupInfo'] << @security_group
-                @security_group = { 'ipPermissions' => [], 'ipPermissionsEgress' => [] }
+              when 'ipRanges'
+                @in_ip_ranges = false
+              when 'item'
+                if @in_groups
+                  if @in_ip_permissions_egress
+                    @ip_permission_egress['groups'] << @group
+                  else
+                    @ip_permission['groups'] << @group
+                  end
+                  @group = {}
+                elsif @in_ip_ranges
+                  if @in_ip_permissions_egress
+                    @ip_permission_egress['ipRanges'] << @ip_range
+                  else
+                    @ip_permission['ipRanges'] << @ip_range
+                  end
+                  @ip_range = {}
+                elsif @in_ip_permissions
+                  @security_group['ipPermissions'] << @ip_permission
+                  @ip_permission = { 'groups' => [], 'ipRanges' => []}
+                elsif @in_ip_permissions_egress
+                  @security_group['ipPermissionsEgress'] << @ip_permission_egress
+                  @ip_permission_egress = { 'groups' => [], 'ipRanges' => []}
+                else
+                  @response['securityGroupInfo'] << @security_group
+                  @security_group = { 'ipPermissions' => [], 'ipPermissionsEgress' => [], 'tagSet' => {} }
+                end
+              when 'requestId'
+                @response[name] = value
+              when 'userId'
+                @group[name] = value
               end
-            when 'requestId'
-              @response[name] = value
-            when 'userId'
-              @group[name] = value
             end
           end
         end
