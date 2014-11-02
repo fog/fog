d2a6b78ffe9d0b7371def422fac2c8f5dbedbc2a
Frederick Cheung <frederick.cheung@gmail.com>
2014-11-02 17:08:16 +0000

    [AWS|S3] Vhost buckets don't work if bucket name has a . in it
    the S3 wildcard cert is only valid for *.s3.amazonaws.com, not
    foo.*.amazonaws.com

--- lib/fog/aws/storage.rb
+++ lib/fog/aws/storage.rb
@@ -217,7 +217,9 @@ module Fog
             if !path_style && COMPLIANT_BUCKET_NAMES !~ bucket_name
               Fog::Logger.warning("fog: the specified s3 bucket name(#{bucket_name}) is not a valid dns name, which will negatively impact performance.  For details see: http://docs.amazonwebservices.com/AmazonS3/latest/dev/BucketRestrictions.html")
               path_style = true
-            end
+            elsif scheme == 'https' && bucket_name =~ /\./
+              path_style = true
+            end  
 
             if path_style
               path = bucket_to_path bucket_name, path
