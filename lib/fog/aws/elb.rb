d1997c2eccc81e692eed3ea67c0a3e1546aa6a2b
Frederick Cheung <frederick.cheung@gmail.com>
2014-11-03 11:02:53 +0000

    [AWS|ELB] Update list of regions the mocks allow

--- lib/fog/aws/elb.rb
+++ lib/fog/aws/elb.rb
@@ -88,7 +88,7 @@ module Fog
           @region = options[:region] || 'us-east-1'
           setup_credentials(options)
 
-          unless ['ap-northeast-1', 'ap-southeast-1', 'ap-southeast-2', 'eu-west-1', 'us-east-1', 'us-west-1', 'us-west-2'].include?(@region)
+          unless ['ap-northeast-1', 'ap-southeast-1', 'ap-southeast-2', 'eu-central-1', 'eu-west-1', 'us-east-1', 'us-west-1', 'us-west-2', 'sa-east-1'].include?(@region)
             raise ArgumentError, "Unknown region: #{@region.inspect}"
           end
         end
