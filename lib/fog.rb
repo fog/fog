8e2ada726f9c9236f7128281ea0f2f17e6cad258
geemus <geemus@gmail.com>
2014-11-20 08:46:23 -0600

    ensure version is required
    closes #3281

--- lib/fog.rb
+++ lib/fog.rb
@@ -3,6 +3,8 @@
 __LIB_DIR__ = File.expand_path(File.dirname(__FILE__))
 $LOAD_PATH.unshift __LIB_DIR__ unless $LOAD_PATH.include?(__LIB_DIR__)
 
+require 'fog/version'
+
 # Use core
 require 'fog/core'
 
