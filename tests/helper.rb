44dacc062d645c002f1bf2b5729237c01137c63c
Matt Darby <matt.darby@rackspace.com>
2014-11-28 11:43:55 -0500

    Moved to Fog::Core

--- tests/helper.rb
+++ tests/helper.rb
@@ -2,7 +2,7 @@ ENV['FOG_RC']         = ENV['FOG_RC'] || File.expand_path('../.fog', __FILE__)
 ENV['FOG_CREDENTIAL'] = ENV['FOG_CREDENTIAL'] || 'default'
 
 require 'fog'
-require 'fog/bin' # for available_providers and registered_providers
+require 'fog/core/bin' # for available_providers and registered_providers
 
 Excon.defaults.merge!(:debug_request => true, :debug_response => true)
 
