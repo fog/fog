3b805f6483c31a4b55e26b5458b547609af247df
Paul Thornthwaite <tokengeek@gmail.com>
2014-12-09 22:39:20 +0000

    Detailed checks for `Brightbox` class

--- spec/fog/bin/brightbox_spec.rb
+++ spec/fog/bin/brightbox_spec.rb
@@ -7,4 +7,25 @@ describe Brightbox do
   include Fog::BinSpec
 
   let(:subject) { Brightbox }
+
+  describe "#services" do
+    it "includes all services" do
+      assert_includes Brightbox.services, :compute
+      assert_includes Brightbox.services, :storage
+    end
+  end
+
+  describe "#class_for" do
+    describe "when requesting compute service" do
+      it "returns correct class" do
+        assert_equal Fog::Compute::Brightbox, Brightbox.class_for(:compute)
+      end
+    end
+
+    describe "when requesting storage service" do
+      it "returns correct class" do
+        assert_equal Fog::Storage::Brightbox, Brightbox.class_for(:storage)
+      end
+    end
+  end
 end
