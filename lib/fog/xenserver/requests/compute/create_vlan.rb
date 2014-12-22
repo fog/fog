e126477ae847ea84f18810ff996ac5c9b2313eaa
Paulo Henrique Lopes Ribeiro <plribeiro3000@gmail.com>
2014-12-21 22:14:31 -0200

    Added more deprecations

--- lib/fog/xenserver/requests/compute/create_vlan.rb
+++ lib/fog/xenserver/requests/compute/create_vlan.rb
@@ -7,14 +7,27 @@ module Fog
         #
         # @see http://docs.vmd.citrix.com/XenServer/6.0.0/1.0/en_gb/api/?c=VLAN
         #
-        def create_vlan( pif_ref, vlan_id, network_ref )
+        def create_vlan( pif_ref, vlan_id, network_ref = '' )
+          if pif_ref.is_a?(Hash)
+            config = pif_ref
+            extra_config = vlan_id
+
+            pif_ref = extra_config[:pif_ref]
+            tag = config[:tag].to_s
+            network_ref = extra_config[:network_ref]
+          else
+            Fog::Logger.deprecation(
+                'This api is deprecated. The only expected params are two hash of attributes.'
+            )
+            tag = vlan_id.to_s
+          end
           @connection.request(
             {
               :parser => Fog::Parsers::XenServer::Base.new,
               :method => 'VLAN.create'
             },
             pif_ref,
-            vlan_id.to_s,
+            tag,
             network_ref
           )
         end
