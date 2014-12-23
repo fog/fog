d910434e1a1e0a708b2145c2e97237231c4e5b4b
Ilja Bobkevic <ilja.bobkevic@unibet.com>
2014-12-23 12:55:47 +0100

    Add ovirt_ca_no_verify option for ovirt provider

--- lib/fog/ovirt/compute.rb
+++ lib/fog/ovirt/compute.rb
@@ -5,7 +5,7 @@ module Fog
     class Ovirt < Fog::Service
       requires   :ovirt_username, :ovirt_password
       recognizes :ovirt_url,      :ovirt_server,  :ovirt_port, :ovirt_api_path, :ovirt_datacenter,
-                 :ovirt_ca_cert_store, :ovirt_ca_cert_file
+                 :ovirt_ca_cert_store, :ovirt_ca_cert_file, :ovirt_ca_no_verify
 
       model_path 'fog/ovirt/models/compute'
       model      :server
@@ -113,6 +113,7 @@ module Fog
           connection_opts[:datacenter_id] = options[:ovirt_datacenter]
           connection_opts[:ca_cert_store] = options[:ovirt_ca_cert_store]
           connection_opts[:ca_cert_file]  = options[:ovirt_ca_cert_file]
+          connection_opts[:ca_no_verify]  = options[:ovirt_ca_no_verify]
 
           @client = OVIRT::Client.new(username, password, url, connection_opts)
         end
