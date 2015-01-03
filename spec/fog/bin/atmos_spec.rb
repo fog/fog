0ef2453fa0e86943995bb933caab36e7ca732d34
Paul Thornthwaite <tokengeek@gmail.com>
2015-01-03 20:51:47 +0000

    Tests top level classes interface
    Currently most providers used by the `fog` binary give themselves a top
    level class. This has become the place a lookup table between services
    and the classes which implement them.
    
    This adds compatibility tests for the existing behaviour to allow
    controlled extraction to modules.
    
    This top level class behaviour is due to be deprecated and removed in
    fog 2.0

--- spec/fog/bin/atmos_spec.rb
+++ spec/fog/bin/atmos_spec.rb
@@ -7,4 +7,28 @@ describe Atmos do
   include Fog::BinSpec
 
   let(:subject) { Atmos }
+
+  describe "#services" do
+    it "includes all services" do
+      assert_includes Atmos.services, :storage
+    end
+  end
+
+  describe "#class_for" do
+    describe "when requesting storage service" do
+      it "returns correct class" do
+        assert_equal Fog::Storage::Atmos, Atmos.class_for(:storage)
+      end
+    end
+  end
+
+  describe "#[]" do
+    describe "when requesting storage service" do
+      it "returns instance" do
+        Fog::Storage::Atmos.stub(:new, "instance") do
+          assert_equal "instance", Atmos[:storage]
+        end
+      end
+    end
+  end
 end
