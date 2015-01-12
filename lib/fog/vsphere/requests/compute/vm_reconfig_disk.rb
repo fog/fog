020e25456d90bdb06ea21aa68a3171fdaad9b5e1
Teng Li <otec.teng@gmail.com>
2015-01-12 18:42:51 +0800

    add disk if configure option key disk_uuid exist but is nil

--- lib/fog/vsphere/requests/compute/vm_reconfig_disk.rb
+++ lib/fog/vsphere/requests/compute/vm_reconfig_disk.rb
@@ -8,17 +8,37 @@ module Fog
 
           vm_disks = get_vm_ref(options['instance_uuid']).disks
           if options.has_key? 'disk_uuid'
-            virtual_disk = vm_disks.select!{|disk| disk.backing.uuid == options['disk_uuid']}
-          else
+            if options['disk_uuid']!=nil
+              virtual_disk = vm_disks.select!{|disk| disk.backing.uuid == options['disk_uuid']}
+            end
+          else            
             virtual_disk = vm_disks.first
           end
-          virtual_disk.capacityInKB = options['size'] 
-          virtual_disk.backing.thinProvisioned = options['thinProvisioned'] || false
-          
-          device_cfg = RbVmomi::VIM::VirtualDeviceConfigSpec(
-            :operation=>RbVmomi::VIM::VirtualDeviceConfigSpecOperation('edit'),
-            :device => virtual_disk
-          )
+                            
+          if virtual_disk
+            virtual_disk.capacityInKB = options['size'] 
+            virtual_disk.backing.thinProvisioned = options['thinProvisioned'] || false            
+            device_cfg = RbVmomi::VIM::VirtualDeviceConfigSpec(
+              :operation=>RbVmomi::VIM::VirtualDeviceConfigSpecOperation('edit'),
+              :device => virtual_disk
+            )
+          else
+            device_cfg = RbVmomi::VIM::VirtualDeviceConfigSpec(
+              :operation => :add,
+              :fileOperation => :create,
+              :device => RbVmomi::VIM.VirtualDisk(
+              :key => 1,
+              :backing => RbVmomi::VIM.VirtualDiskFlatVer2BackingInfo(
+                :fileName => "#{vm_disks.last.backing.fileName}.#{vm_disks.last.unitNumber + 1}",
+                :diskMode => :persistent,
+                :thinProvisioned => true
+              ),
+              :controllerKey => 1000,
+              :unitNumber => vm_disks.last.unitNumber + 1,
+              :capacityInKB => options['size']
+              )
+            )
+          end
           hardware_spec={:deviceChange => [device_cfg]}
 
           vm_reconfig_hardware('instance_uuid' => options['instance_uuid'], 'hardware_spec' => hardware_spec )
