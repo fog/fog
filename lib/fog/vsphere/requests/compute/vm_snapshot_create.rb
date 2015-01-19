07fef86fbaa2dfe63d29bc5fc421dc395e8a99ab
Teng Li <otec.teng@gmail.com>
2015-01-19 15:16:55 +0800

    fix: vsphere snapshots

--- lib/fog/vsphere/requests/compute/vm_snapshot_create.rb
+++ lib/fog/vsphere/requests/compute/vm_snapshot_create.rb
@@ -3,21 +3,23 @@ module Fog
     class Vsphere
       class Real
 
-        def vm_snapshot_revert(options = {})
+        def vm_snapshot_create(options = {})
           options = { 'force' => false }.merge(options)
           raise ArgumentError, "instance_uuid is a required parameter" unless options.has_key? 'instance_uuid'
           raise ArgumentError, "name is a required parameter" unless options.has_key? 'name'
 
           search_filter = { :uuid => options['instance_uuid'], 'vmSearch' => true, 'instanceUuid' => true }
           vm_mob_ref = @connection.searchIndex.FindAllByUuid(search_filter).first
-          raise "vm_snapshot_revert is not implemented"
+
+          task = vm_mob_ref.CreateSnapshot_Task(:name=>options['name'],:memory=>false,:quiesce=>true)
+          task.wait_for_completion
         end
 
       end
 
       class Mock
 
-        def vm_snapshot_revert(options = {})
+        def vm_snapshot_create(options = {})
           raise ArgumentError, "instance_uuid is a required parameter" unless options.has_key? 'instance_uuid'
           { 'task_state'     => "running", 'reboot_type' => options['force'] ? 'reset_power' : 'reboot_guest' }
         end
