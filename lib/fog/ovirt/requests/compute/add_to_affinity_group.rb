d49fe71463e26431aa83d5f7c6e9bf55793657e2
Pavol Dilung <pavol.dilung@gmail.com>
2015-02-24 11:00:50 +0100

    Remove dangeling references to pry debugger

--- lib/fog/ovirt/requests/compute/add_to_affinity_group.rb
+++ lib/fog/ovirt/requests/compute/add_to_affinity_group.rb
@@ -3,7 +3,6 @@ module Fog
     class Ovirt
       class Real
         def add_to_affinity_group(id, options)
-          binding.pry
           raise ArgumentError, "instance id is a required parameter" unless id
           raise ArgumentError, "affinity group id is a required parameter for add-to-affinity-group" unless options.key? :id
           client.add_vm_to_affinity_group(options[:id], id)
