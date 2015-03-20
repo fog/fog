413426cc499ad30bd91430d6ac810df7241c67d3
Darren Hague <d.hague@sap.com>
2015-03-20 12:29:57 +0000

    [openstack|identity] check_token and validate_token - make tenant_id optional
    Fixes #3493

--- lib/fog/openstack/requests/identity/check_token.rb
+++ lib/fog/openstack/requests/identity/check_token.rb
@@ -2,11 +2,11 @@ module Fog
   module Identity
     class OpenStack
       class Real
-        def check_token(token_id, tenant_id)
+        def check_token(token_id, tenant_id=nil)
           request(
             :expects  => [200, 203],
             :method   => 'HEAD',
-            :path     => "tokens/#{token_id}?belongsTo=#{tenant_id}"
+            :path     => "tokens/#{token_id}"+(tenant_id ? "?belongsTo=#{tenant_id}" : '')
           )
         end
       end
--- lib/fog/openstack/requests/identity/validate_token.rb
+++ lib/fog/openstack/requests/identity/validate_token.rb
@@ -2,11 +2,11 @@ module Fog
   module Identity
     class OpenStack
       class Real
-        def validate_token(token_id, tenant_id)
+        def validate_token(token_id, tenant_id=nil)
           request(
             :expects  => [200, 203],
             :method   => 'GET',
-            :path     => "tokens/#{token_id}?belongsTo=#{tenant_id}"
+            :path     => "tokens/#{token_id}"+(tenant_id ? "?belongsTo=#{tenant_id}" : '')
           )
         end
       end
