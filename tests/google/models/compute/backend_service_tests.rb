d08cc07f9b093735b22a4bb59e8314ab5001f9b0
Paulo Henrique Lopes Ribeiro <plribeiro3000@gmail.com>
2015-04-03 11:57:04 -0300

    Remove Google tests

--- tests/google/models/compute/backend_service_tests.rb
+++
@@ -1,6 +0,0 @@
-require 'securerandom'
-Shindo.tests("Fog::Compute[:google] | backend service model", ['google']) do
-  random_string = SecureRandom.hex
-  model_tests(Fog::Compute[:google].backend_services, {:name => "fog-test-backend-service-#{random_string}",
-      :health_checks => [@health_check]})
-end
