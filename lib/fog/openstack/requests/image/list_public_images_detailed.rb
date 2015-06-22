5857abb896575a69bdeff0a23ade0d29ef52046b
Ladislav Smola <lsmola@redhat.com>
2015-06-22 18:18:32 +0200

    OpenStack Glance and Heat list methods unified interface
    We need to move all list methods to unified interface, where
    only Hash is passed as a first argument. The hash can have
    specific fields, that will be recognized and deleted. Rest
    of the Hash goes directly to request :query. ArgumentError
    is raised when mandatory param is missing.
    
    This way we can start using the list methods the same. Which
    is very important for handling e.g. pagination, filtering,
    etc.
    
    All changes are made backwards compatible, with deprecation
    warnings, when old interface is used.

--- lib/fog/openstack/requests/image/list_public_images_detailed.rb
+++ lib/fog/openstack/requests/image/list_public_images_detailed.rb
@@ -2,10 +2,13 @@ module Fog
   module Image
     class OpenStack
       class Real
-        def list_public_images_detailed(attribute=nil, query=nil)
-          path = 'images/detail'
-          if attribute
-            query = { attribute => query }
+        def list_public_images_detailed(options = {}, query_deprecated = nil)
+          if options.is_a?(Hash)
+            query = options
+          elsif options
+            Fog::Logger.deprecation("Calling OpenStack[:glance].list_public_images_detailed(attribute, query) format"\
+                                    " is deprecated, call .list_public_images_detailed(attribute => query) instead")
+            query = { options => query_deprecated }
           else
             query = {}
           end
@@ -13,14 +16,14 @@ module Fog
           request(
             :expects => [200, 204],
             :method  => 'GET',
-            :path    => path,
+            :path    => 'images/detail',
             :query   => query
           )
         end
       end # class Real
 
       class Mock
-        def list_public_images_detailed(attribute=nil, query=nil)
+        def list_public_images_detailed(options = {}, query_deprecated = nil)
           response = Excon::Response.new
           response.status = [200, 204][rand(1)]
           response.body = {'images' => self.data[:images].values}
