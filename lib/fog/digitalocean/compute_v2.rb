92da751796c802aa78b34dec62b416c64bcb334c
bryanl <bryan@osesm.com>
2015-09-15 15:32:20 -0400

    list_servers

---
+++ lib/fog/digitalocean/compute_v2.rb
@@ -0,0 +1,71 @@
+require 'fog/digitalocean/core'
+
+module Fog
+  module Compute
+    class DigitalOceanV2 < Fog::Service
+      requires :digitalocean_token
+
+      model_path 'fog/digitalocean/models/compute_v2'
+      model :server
+      collection :servers
+
+      request_path 'fog/digitalocean/requests/compute_v2'
+      request :list_servers
+      request :get_server
+
+      class Mock
+        def self.data
+          @data ||= Hash.new do |hash, key|
+            hash[key] = {
+                :servers => [],
+                :ssh_keys => []
+            }
+          end
+        end
+
+        def initialize(options={})
+          @digitalocean_token = options[:digitalocean_token]
+        end
+
+        def data
+          self.class.data[@digitalocean_token]
+        end
+
+        def reset_data
+          self.class.data.delete(@digitalocean_token)
+        end
+      end
+
+      class Real
+        def initialize(options={})
+          digitalocean_token = options[:digitalocean_token]
+          persistent = false
+          options = {
+              headers: {
+                  'Authorization' => "Bearer #{digitalocean_token}",
+              }
+          }
+          @connection = Fog::Core::Connection.new 'https://api.digitalocean.com', persistent, options
+        end
+
+        def request(params)
+          params[:headers] ||= {}
+          begin
+            response = @connection.request(params)
+          rescue Excon::Errors::HTTPStatusError => error
+            raise case error
+                    when Excon::Errors::NotFound
+                      Fog::Compute::Bluebox::NotFound.slurp(error)
+                    else
+                      error
+                  end
+          end
+          unless response.body.empty?
+            response.body = Fog::JSON.decode(response.body)
+          end
+          response
+        end
+      end
+    end
+  end
+end
