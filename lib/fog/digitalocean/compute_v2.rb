dda7eb92f5675968ff2bc2e7c81b787aba4555aa
bryanl <bryan@osesm.com>
2015-09-17 18:15:11 -0400

    add ssh key data

--- lib/fog/digitalocean/compute_v2.rb
+++ lib/fog/digitalocean/compute_v2.rb
@@ -14,32 +14,39 @@ module Fog
       collection :regions
       model :flavor
       collection :flavors
+      model :ssh_key
+      collection :ssh_keys
 
       request_path 'fog/digitalocean/requests/compute_v2'
-      request :get_server_details
-      request :list_servers
-      request :list_images
-      request :list_regions
-      request :list_flavors
-      request :create_server
-      request :delete_server
-      request :list_droplet_actions
-      request :get_droplet_action
-      request :disable_backups
-      request :reboot_server
-      request :power_cycle
-      request :shutdown
-      request :power_on
-      request :power_off
-      request :restore
-      request :password_reset
-      request :resize
-      request :rebuild
-      request :rename
       request :change_kernel
+      request :create_server
+      request :create_ssh_key
+      request :delete_server
+      request :delete_ssh_key
+      request :disable_backups
       request :enable_ipv6
       request :enable_private_networking
+      request :get_droplet_action
+      request :get_server_details
+      request :get_ssh_key
+      request :list_droplet_actions
+      request :list_flavors
+      request :list_images
+      request :list_regions
+      request :list_servers
+      request :list_ssh_keys
+      request :password_reset
+      request :power_cycle
+      request :power_off
+      request :power_on
+      request :reboot_server
+      request :rebuild
+      request :rename
+      request :resize
+      request :restore
+      request :shutdown
       request :snapshot
+      request :update_ssh_key
       request :upgrade
 
 
@@ -47,8 +54,8 @@ module Fog
         def self.data
           @data ||= Hash.new do |hash, key|
             hash[key] = {
-                :servers => [],
-                :ssh_keys => []
+              :servers  => [],
+              :ssh_keys => []
             }
           end
         end
@@ -69,13 +76,13 @@ module Fog
       class Real
         def initialize(options={})
           digitalocean_token = options[:digitalocean_token]
-          persistent = false
-          options = {
-              headers: {
-                  'Authorization' => "Bearer #{digitalocean_token}",
-              }
+          persistent         = false
+          options            = {
+            headers: {
+              'Authorization' => "Bearer #{digitalocean_token}",
+            }
           }
-          @connection = Fog::Core::Connection.new 'https://api.digitalocean.com', persistent, options
+          @connection        = Fog::Core::Connection.new 'https://api.digitalocean.com', persistent, options
         end
 
         def request(params)
