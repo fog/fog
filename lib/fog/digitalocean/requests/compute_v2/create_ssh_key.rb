96c05748f44ac132920a03e78face6013411e562
Dave Benvenuti <davebenvenuti@gmail.com>
2015-11-02 19:55:18 -0500

    improve mocks to more closely match real ssh key endpoint functionality

--- lib/fog/digitalocean/requests/compute_v2/create_ssh_key.rb
+++ lib/fog/digitalocean/requests/compute_v2/create_ssh_key.rb
@@ -30,13 +30,15 @@ module Fog
           response        = Excon::Response.new
           response.status = 201
 
+          data[:ssh_keys] << {
+            "id" => Fog::Mock.random_numbers(6).to_i,
+            "fingerprint" => (["00"] * 16).join(':'),
+            "public_key" => public_key,
+            "name" => name
+          }
+
           response.body ={
-            'ssh_key' => {
-              'id'          => 512190,
-              'fingerprint' => "3b:16:bf:e4:8b:00:8b:b8:59:8c:a9:d3:f0:19:45:fa",
-              'public_key'  => "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAQQDDHr/jh2Jy4yALcK4JyWbVkPRaWmhck3IgCoeOO3z1e2dBowLh64QAM+Qb72pxekALga2oi4GvT+TlWNhzPH4V example",
-              'name'        => "My SSH Public Key"
-            }
+            'ssh_key' => data[:ssh_keys].last
           }
 
           response
