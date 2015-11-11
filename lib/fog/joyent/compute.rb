ebfaa0c97a387eeb18c5c797aaa33a1596e46676
geemus <geemus@gmail.com>
2015-11-11 10:40:51 -0600

    make net/ssh require optional

--- lib/fog/joyent/compute.rb
+++ lib/fog/joyent/compute.rb
@@ -1,6 +1,5 @@
 require 'fog/joyent/core'
 require 'fog/joyent/errors'
-require 'net/ssh'
 
 module Fog
   module Compute
@@ -126,6 +125,12 @@ module Fog
           end
 
           if options[:joyent_keyname]
+            begin
+              require "net/ssh"
+            rescue LoadError
+              Fog::Logger.warning("'net/ssh' missing, please install and try again.")
+              exit(1)
+            end
             @joyent_keyname = options[:joyent_keyname]
             @joyent_keyphrase = options[:joyent_keyphrase]
             @key_manager = Net::SSH::Authentication::KeyManager.new(nil, {
