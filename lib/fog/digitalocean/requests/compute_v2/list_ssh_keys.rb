771e57f0d3d10fa67bd7bd5fccf27f5410a6c857
Ben Sedat <bsedat@tinfoilsecurity.com>
2016-07-06 18:00:59 -0700

    Fix filtering for Digital Ocean list_ssh_keys request

--- lib/fog/digitalocean/requests/compute_v2/list_ssh_keys.rb
+++ lib/fog/digitalocean/requests/compute_v2/list_ssh_keys.rb
@@ -2,18 +2,18 @@ module Fog
   module Compute
     class DigitalOceanV2
       class Real
-        def list_ssh_keys
+        def list_ssh_keys(filters = {})
           request(
             :expects => [200],
             :method => 'GET',
-            :path => 'v2/account/keys',
+            :path => "v2/account/keys#{filters.to_a.map { |x| "#{x[0]}=#{x[1]}" }.join("&")}",
           )
         end
       end
 
       # noinspection RubyStringKeysInHashInspection
       class Mock
-        def list_ssh_keys
+        def list_ssh_keys(filters = {})
           response = Excon::Response.new
           response.status = 200
           response.body = {
