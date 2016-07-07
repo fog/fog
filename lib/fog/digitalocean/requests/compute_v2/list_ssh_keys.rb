14d0ebc489c5ef47d63f5488ffe8eb6b05a7de26
Ben Sedat <bsedat@tinfoilsecurity.com>
2016-07-06 18:33:39 -0700

    Add tests for Digital Ocean list_ssh_keys request
    Also fill out the mock response body with the example SSH key from the
    Digital Ocean API docs if no SSH keys have been created already.

--- lib/fog/digitalocean/requests/compute_v2/list_ssh_keys.rb
+++ lib/fog/digitalocean/requests/compute_v2/list_ssh_keys.rb
@@ -14,13 +14,22 @@ module Fog
       # noinspection RubyStringKeysInHashInspection
       class Mock
         def list_ssh_keys(filters = {})
-          response = Excon::Response.new
+          response        = Excon::Response.new
           response.status = 200
-          response.body = {
-            "ssh_keys" => data[:ssh_keys],
-            "links" => {},
+          response.body   = {
+            "ssh_keys" => data[:ssh_keys] ||
+              [
+                {
+                  "id" => 512189,
+                  "fingerprint" => "3b:16:bf:e4:8b:00:8b:b8:59:8c:a9:d3:f0:19:45:fa",
+                  "public_key" => "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAQQDDHr/jh2Jy4yALcK4JyWbVkPRaWmhck3IgCoeOO3z1e2dBowLh64QAM+Qb72pxekALga2oi4GvT+TlWNhzPH4V example",
+                  "name" => "My SSH Public Key"
+                }
+              ],
+            "links" => {
+            },
             "meta" => {
-              "total" => data[:ssh_keys].count
+              "total" => data[:ssh_keys].count || 1
             }
           }
           response
