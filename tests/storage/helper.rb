5fb2f60feb0e3562f2e18d8061027c740dd8a4aa
Vít Ondruch <vondruch@redhat.com>
2016-09-13 10:47:33 +0200

    Drop 'hp' from storage tests.

--- tests/storage/helper.rb
+++ tests/storage/helper.rb
@@ -3,9 +3,6 @@ def storage_providers
     :aws        => {
       :mocked => true
     },
-    :hp         => {
-      :mocked => true
-    },
     :internetarchive => {
       :mocked => true
     },
