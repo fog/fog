78da8fcccbddee01baae58ccafcda196b3545bfd
Vít Ondruch <vondruch@redhat.com>
2016-09-13 12:06:14 +0200

    Drop 'hp' test cases.

--- tests/hp/models/compute/key_pairs_tests.rb
+++
@@ -1,5 +0,0 @@
-Shindo.tests("Fog::Compute[:hp] | key_pairs", ['hp']) do
-
-  collection_tests(Fog::Compute[:hp].key_pairs, {:name => 'fogkeyname'}, true)
-
-end
--- tests/hp/models/network/floating_ips_tests.rb
+++
@@ -1,8 +0,0 @@
-Shindo.tests('HP::Network | networking floating ips collection', ['hp', 'networking', 'floatingip']) do
-
-  @ext_network = HP[:network].networks.all({'router:external'=>true}).first
-
-  attributes = {:floating_network_id => @ext_network.id}
-  collection_tests(HP[:network].floating_ips, attributes, true)
-
-end
