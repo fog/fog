fog/fog

commit dd9db0468363a687667dada3b9279b0b25246023

Remove digitalocean bin files

lib/fog/bin/digitalocean.rb
@@ -1,29 +1,0 @@
-class DigitalOcean < Fog::Bin
-  class << self
-    def class_for(key)
-      case key
-      when :compute
-        Fog::Compute::DigitalOceanV2
-      else
-        raise ArgumentError, "Unsupported #{self} service: #{key}"
-      end
-    end
-
-    def [](service)
-      @@connections ||= Hash.new do |hash, key|
-        hash[key] = case key
-        when :compute
-          Fog::Logger.warning("DigitalOcean[:compute] is not recommended, use Compute[:digitalocean] for portability")
-          Fog::Compute.new(:provider => 'DigitalOcean')
-        else
-          raise ArgumentError, "Unrecognized service: #{key.inspect}"
-        end
-      end
-      @@connections[service]
-    end
-
-    def services
-      Fog::DigitalOcean.services
-    end
-  end
-end

lib/fog/digitalocean.rb
@@ -1,1 +1,0 @@
-require 'fog/digitalocean/compute_v2'

commit 664c01e07e5976bbf89e9a958997baccf7e98507

Fix issue with DigitalOcean as available provider
dd9db0468363a687667dada3b9279b0b25246023 mistakenly removed the
fog/bin/digitalocean file which was need for the `available_providers`
method. This has been re-added to fix a broken test.

lib/fog/bin.rb
@@ -57,6 +57,7 @@ require 'fog/bin/bluebox'
 require 'fog/bin/brightbox'
 require 'fog/bin/cloudstack'
 require 'fog/bin/clodo'
+require 'fog/bin/digitalocean'
 require 'fog/bin/dnsimple'
 require 'fog/bin/dnsmadeeasy'
 require 'fog/bin/fogdocker'

lib/fog/bin/digitalocean.rb
@@ -1,0 +1,29 @@
+class DigitalOcean < Fog::Bin
+  class << self
+    def class_for(key)
+      case key
+      when :compute
+        Fog::Compute::DigitalOceanV2
+      else
+        raise ArgumentError, "Unsupported #{self} service: #{key}"
+      end
+    end
+
+    def [](service)
+      @@connections ||= Hash.new do |hash, key|
+        hash[key] = case key
+        when :compute
+          Fog::Logger.warning("DigitalOcean[:compute] is not recommended, use Compute[:digitalocean] for portability")
+          Fog::Compute.new(:provider => 'DigitalOcean')
+        else
+          raise ArgumentError, "Unrecognized service: #{key.inspect}"
+        end
+      end
+      @@connections[service]
+    end
+
+    def services
+      Fog::DigitalOcean.services
+    end
+  end
+end