e64ccf6c1c8f655e91297d66bcb23ddcbdf15896
Nat Welch <nat@natwelch.com>
2017-03-12 10:21:57 -0700

    Remove more internet archive stuff

--- tests/storage/helper.rb
+++ tests/storage/helper.rb
@@ -3,9 +3,6 @@ def storage_providers
     :aws        => {
       :mocked => true
     },
-    :internetarchive => {
-      :mocked => true
-    },
     :local      => {
       :mocked => false
     },
